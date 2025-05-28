09addb1e873869c13482c26571568462ba1b0d8d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-05-28 10:51:11 +0900

    [level 1] Title: 신규 아이디 추천, Time: 14.25 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,58 +1,34 @@
-class Solution {
+public class Solution {
     public String solution(String new_id) {
-        //모든 대문자를 대응되는 소문자로 치환
-        new_id = new_id.toLowerCase();
-        //알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거
-        StringBuilder sb = new StringBuilder();
-        for(char c : new_id.toCharArray()){
-            if(c >= 'a' && c <= 'z' || c >= '0' && c <= '9' || c == '-' || c =='_' || c == '.'){
-                sb.append(c);
-            }
-        }
-        new_id = sb.toString();
-        //마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
+        // 1단계: 모든 대문자를 소문자로 치환
+        String answer = new_id.toLowerCase();
         
-        sb = new StringBuilder();
-        boolean flag = false;
-        for(char c : new_id.toCharArray()){
-            if(c =='.'){
-                if(!flag){
-                    sb.append(c);
-                    flag = true;
-                }
-            }else{
-                sb.append(c);
-                flag = false;
-            }
+        // 2단계: 알파벳 소문자, 숫자, '-', '_', '.'만 남기고 모두 제거
+        answer = answer.replaceAll("[^a-z0-9-_.]", "");
+        
+        // 3단계: '.'가 2번 이상 연속된 부분 하나로 치환
+        answer = answer.replaceAll("[.]{2,}", ".");
+        
+        // 4단계: 처음이나 끝에 '.'가 있으면 제거
+        answer = answer.replaceAll("^\\.|\\.$", "");
+        
+        // 5단계: 빈 문자열이면 "a" 대입
+        if (answer.isEmpty()) {
+            answer = "a";
         }
         
-        new_id = sb.toString();
-        
-        //마침표(.)가 처음이나 끝에 위치한다면 제거
-        if (new_id.startsWith(".")) {
-            new_id = new_id.substring(1);
-        }
-        if (new_id.endsWith(".")) {
-            new_id = new_id.substring(0, new_id.length() - 1);
-        }
-        //빈 문자열이라면, new_id에 "a"를 대입
-        if (new_id.isEmpty()) {
-            new_id = "a";
-        }
-        //길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거
-        //만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거
-        if(new_id.length() >= 16){
-            new_id = new_id.substring(0,15);
-            if(new_id.endsWith(".")){
-                new_id = new_id.substring(0, new_id.length()-1);
-            }
+        // 6단계: 길이가 16자 이상이면 첫 15자만 남기고 나머지 제거
+        //       만약 제거 후 마지막 문자가 '.'면 제거
+        if (answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("\\.$", "");
         }
         
-        //길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙이기
-        while(new_id.length() <= 2){
-            new_id += new_id.charAt(new_id.length() - 1);
+        // 7단계: 길이가 2자 이하이면 마지막 문자 반복해서 3자 만들기
+        while (answer.length() < 3) {
+            answer += answer.charAt(answer.length() - 1);
         }
         
-        return new_id;
+        return answer;
     }
 }
