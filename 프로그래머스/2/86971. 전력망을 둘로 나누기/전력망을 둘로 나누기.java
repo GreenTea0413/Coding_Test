27c66cd1f0821cf0e169e08d1601f620ec276dd4
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-05-29 09:49:42 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.90 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,29 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public int bfs(boolean[] visited,List<Integer>[] graph, int start){
-        Queue<Integer> queue = new LinkedList<>();
-        visited[start]  = true;
-        queue.add(start);
-        int count = 1;
+    int answer = 101;
+    
+    int bfs(List<Integer>[] graph, boolean[] visited, int start){
+        Queue <Integer> queue = new LinkedList<Integer>();
         
+        visited[start] = true;
+        queue.add(start);
+        int num = 1;
         while(!queue.isEmpty()){
-            int num = queue.poll();
+            int s = queue.poll();
             
-            for(int next : graph[num]){
+            for(int next : graph[s]){
                 if(!visited[next]){
-                    count ++;
-                    visited[next] = true;
                     queue.add(next);
+                    visited[next] = true;
+                    num ++;
                 }
             }
         }
-        return count;
+        return num;
     }
+    
     public int solution(int n, int[][] wires) {
-        int answer = 101;
         List<Integer>[] graph = new ArrayList[n + 1];
-        for(int i = 0; i < n + 1; i ++){
+        for(int i = 0 ; i < n + 1; i ++){
             graph[i] = new ArrayList<>();
         }
         
@@ -39,11 +41,13 @@ class Solution {
             graph[a].remove(Integer.valueOf(b));
             graph[b].remove(Integer.valueOf(a));
             
+            
             boolean[] visited = new boolean[n + 1];
-            int count = bfs(visited, graph, a);
-            if(count == -1) return 0;
+            
+            int count = bfs(graph,visited,a);
             int diff = Math.abs(n - 2 * count);
-            answer = Math.min(answer , diff);
+            
+            answer = Math.min(answer, diff);
             
             graph[a].add(b);
             graph[b].add(a);
