666bb856fc5ecae45d57648bf5b1e3df7bae0f8f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-05-29 11:20:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.91 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,42 +3,41 @@ import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int answer = -1;
-        
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        
-        int[] dx = {0,0,-1,1};
+        int[] dx = {0,0,1,-1};
         int[] dy = {1,-1,0,0};
         
-        Queue<int[]> queue = new LinkedList<>();
-        visited[0][0] = true;
-        queue.add(new int[]{0,0,1});
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] v = new boolean[n][m] ;
         
-        while(!queue.isEmpty()){
-            int[] arr = queue.poll();
+        v[0][0] =true;
+        q.add(new int[]{0,0,1});
+        
+        while(!q.isEmpty()){
+            int[] arr = q.poll();
             
             int x = arr[0];
             int y = arr[1];
-            int dist = arr[2];
-        
-            if(x == n - 1 && y == m - 1){
-                return dist;
+            int d = arr[2];
+            
+            if(x == n-1 && y == m-1){
+                return d;
             }
             
-            for(int i = 0 ; i < 4; i++){
+            for(int i = 0; i < 4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
                 if(0 <= nx && nx < n && 0 <= ny && ny < m){
-                    if(!visited[nx][ny] && maps[nx][ny] == 1){
-                        visited[nx][ny] = true;
-                        queue.add(new int[]{nx,ny,dist + 1});
+                    if(!v[nx][ny] && maps[nx][ny] == 1){
+                        v[nx][ny] = true;
+                        q.add(new int[]{nx,ny,d+1});
                     }
+                    
                 }
             }
         }
-        
         return answer;
     }
 }
