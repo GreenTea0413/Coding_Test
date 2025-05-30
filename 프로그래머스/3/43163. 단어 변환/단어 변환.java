980332d70a2b5927b383bfb9e5343c9663189ca6
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-05-30 11:09:48 +0900

    [level 3] Title: 단어 변환, Time: 13.07 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,38 +1,39 @@
 class Solution {
     static boolean[] v;
     static int answer = 51;
-    public int solution(String begin, String target, String[] words) {
-        v = new boolean[words.length];
-        dfs(begin,target,words,0);
-        
-        if(answer == 51) return 0;
-        return answer;
-    }
-    void dfs(String start, String end, String[] words, int count){
-        if(start.equals(end)) {
+    
+    public void dfs(String s, String e, String[] words, int count){
+        if(s.equals(e)) {
             answer = Math.min(answer, count);
             return;
-        }
+        }   
         
         for(int i = 0; i < words.length; i++){
             String w = words[i];
             String t = "";
             int num = 0;
             
-            for(int j = 0; j < w.length(); j++){
-                if(start.charAt(j) != w.charAt(j)){
+            for(int j = 0; j < s.length(); j++){
+                if(s.charAt(j) != w.charAt(j)){
                     t += w.charAt(j);
-                    num ++;
+                    num++;
                 }
                 else{
-                    t += start.charAt(j);
+                    t += s.charAt(j);
                 }
             }
             if(num == 1 && !v[i]){
                 v[i] = true;
-                dfs(t,end,words,count+1);
+                dfs(t, e, words,count + 1);
                 v[i] = false;
             }
         }
+    }
+    public int solution(String begin, String target, String[] words) {
+        v = new boolean[words.length];
+        dfs(begin,target,words,0);
+        
+        if(answer == 51) answer = 0;
+        return answer;
     }
 }
