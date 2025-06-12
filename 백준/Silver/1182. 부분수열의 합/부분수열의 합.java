1648ed23f663edcbbd28fbf23f7069c76e50524d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-06-12 12:29:10 +0900

    [Silver II] Title: 부분수열의 합, Time: 116 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, S, answer;
+    static int[] arr;
+
+    public static void dfs(int depth, int sum) {
+        if (depth == N) {
+            if (sum == S) answer++;
+            return;
+        }
+
+        // 현재 원소 포함
+        dfs(depth + 1, sum + arr[depth]);
+
+        // 현재 원소 미포함
+        dfs(depth + 1, sum);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        answer = 0;
+        dfs(0, 0);
+
+        // 공집합 제외시키는 경우
+        if (S == 0) answer--;
+
+        System.out.println(answer);
+    }
+}
