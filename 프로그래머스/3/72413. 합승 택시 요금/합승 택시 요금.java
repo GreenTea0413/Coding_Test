b5d88d39358224b55f8e9932193d09701c36fee1
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-07-01 01:26:14 +0900

    [level 3] Title: 합승 택시 요금, Time: 31.77 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,10 +1,8 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
-        
         int[][] costs = new int[n+1][n+1];
-        
         for(int[] f : fares){
             costs[f[0]][f[1]] = f[2];
             costs[f[1]][f[0]] = f[2];
@@ -15,35 +13,34 @@ class Solution {
             Arrays.fill(d, Integer.MAX_VALUE);
         }
         
-        Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
-        final int[] start = {s, a, b};
+        Queue<int []> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
+        final int[] start = {s,a,b};
         
-        for(int j = 0; j < 3; j++){
-            int[] d = dist[j];
-            pq.add(new int[]{start[j],0});
-            d[start[j]] = 0;
+        for(int i = 0; i < 3; i++){
+            int[] d = dist[i];
+            pq.add(new int[]{start[i], 0});
+            d[start[i]] = 0;
             
             while(!pq.isEmpty()){
                 int[] cur = pq.remove();
                 
-                for(int i = 1; i <=n; i++){
-                    if(costs[cur[0]][i] == 0) continue;
-                    if(d[i] > cur[1] + costs[cur[0]][i]){
-                        d[i] = cur[1] + costs[cur[0]][i];
-                        pq.add(new int[]{i, cur[1] + costs[cur[0]][i]});
+                for(int j = 1; j <=n; j++){
+                    if(costs[cur[0]][j] == 0) continue;
+                    if(d[j] > cur[1] + costs[cur[0]][j]){
+                        d[j] = cur[1] + costs[cur[0]][j];
+                        pq.add(new int[]{j, cur[1] + costs[cur[0]][j]});
                     }
                 }
             }
         }
         int minCost = Integer.MAX_VALUE;
-        for(int i = 1; i <= n; i ++){
+        for (int i = 1; i <= n; i++) {
             int sum = 0;
-            for(int[] d : dist){
-                sum+= d[i];
+            for (int[] d : dist) {
+                sum += d[i];
             }
             minCost = Math.min(minCost, sum);
         }
-        
-        return minCost;
+        return minCost;          
     }
 }
