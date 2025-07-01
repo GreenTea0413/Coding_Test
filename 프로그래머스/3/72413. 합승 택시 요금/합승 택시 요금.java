a7d8a2e9d29bd53e23ff2d446d1fc54f105930ef
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-07-01 14:30:15 +0900

    [level 3] Title: 합승 택시 요금, Time: 28.42 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -8,32 +8,32 @@ class Solution {
             costs[f[1]][f[0]] = f[2];
         }
         
-        int[][] dist = new int [3][n+1];
-        for(int[] d : dist){
+        int[][] dist = new int[3][n+1];
+        for(int[] d: dist){
             Arrays.fill(d, Integer.MAX_VALUE);
         }
         
         Queue<int[]> pq = new PriorityQueue<>((e1, e2) -> e1[1] - e2[1]);
         int[] start = {s, a, b};
         
-        for(int i = 0; i < 3; i++){
+        for(int i = 0 ; i < 3; i++){
             int[] d = dist[i];
-            pq.add(new int[]{start[i], 0});
+            pq.add(new int[]{start[i],0});
             d[start[i]] = 0;
             
             while(!pq.isEmpty()){
                 int[] cur = pq.remove();
                 
-                for(int j =1 ; j <=n; j++){
-                    if(costs[cur[0]][j] == 0) continue;
-                    if(d[j] > cur[1] + costs[cur[0]][j]){
+                for(int j = 1; j <= n; j++){
+                    if(costs[cur[0]][j] == 0 ) continue;
+                    
+                    if(d[j] > cur[1] + costs[cur[0]][j]){   
                         d[j] = cur[1] + costs[cur[0]][j];
-                        pq.add(new int[]{j, cur[1] + costs[cur[0]][j]});
+                        pq.add(new int[] {j, cur[1] + costs[cur[0]][j]});
                     }
                 }
             }
-        }
-        
+        }                                                       
         int min = Integer.MAX_VALUE;
         for(int i = 1; i <= n; i++){
             int sum = 0;
@@ -42,7 +42,6 @@ class Solution {
             }
             min = Math.min(min, sum);
         }
-        
-        return min;
+        return min;            
     }
 }
