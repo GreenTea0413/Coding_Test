28826c145326291475a1ced5e7b0b679c5ef544e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-07-03 13:41:56 +0900

    [level 2] Title: 소수 찾기, Time: 24.10 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -6,33 +6,34 @@ class Solution {
         v = new boolean[numbers.length()];
         dfs("", numbers);
         
-        int answer = 0;
+        int sum =0;
         for(int num : nSet){
-            if(isPrime(num))
-                answer ++;
+            if(isPrime(num)){
+                sum ++;
+            }
         }
-        return answer;
+        return sum;
     }
     
     public void dfs(String cur, String numbers){
-        if(!cur.equals("")) nSet.add(Integer.parseInt(cur));
+        if(!cur.isEmpty()){
+            nSet.add(Integer.parseInt(cur));
+        }
         
         for(int i = 0; i < numbers.length(); i++){
             if(!v[i]){
-                v[i] = true;
+                v[i]= true;
                 dfs(cur + numbers.charAt(i), numbers);
-                v[i] = false;
+                v[i]= false;
             }
         }
     }
     
-    public boolean isPrime(int n){
-        if(n < 2) return false;
-        
-        for(int i = 2; i <=Math.sqrt(n); i++)
-        {
-            if(n % i == 0)  return false;
+    public boolean isPrime(int num){
+        if(num < 2) return false;
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0) return false;
         }
         return true;
-     }
+    }
 }
