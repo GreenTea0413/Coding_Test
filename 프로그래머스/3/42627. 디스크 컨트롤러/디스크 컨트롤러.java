c02213b05a40f99f291a806eb8b4d93b0735fb6d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-07-03 14:44:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 5.03 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,13 +1,14 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (j1, j2) -> j1[0] - j2[0]);
-        Queue<int[]>pq = new PriorityQueue<>((j1,j2) ->j1[1] - j2[1]);
+        Arrays.sort(jobs, (j1,j2) -> j1[0]- j2[0]);
+        Queue<int[]> pq = new PriorityQueue<>((j1,j2) -> j1[1] - j2[1]);
         
-        int cur =0;
-        int job =0;
-        int idx =0;
-        int total =0;
+        int cur = 0;
+        int job = 0;
+        int idx = 0;
+        int total = 0;
         
         while(job < jobs.length){
             while(idx < jobs.length && jobs[idx][0] <= cur){
@@ -26,6 +27,6 @@ class Solution {
             }
         }
         
-        return total/ jobs.length;
+        return total / jobs.length;
     }
 }
