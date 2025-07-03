8df52184db8af49c2eada0d677badccffa072df4
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-07-03 13:36:46 +0900

    [level 2] Title: 타겟 넘버, Time: 7.05 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int solution(int[] numbers, int target) {        
+    public int solution(int[] numbers, int target) {
         return dfs(numbers, 0, target, 0);
     }
     
-    public int dfs(int[] numbers, int count, int target, int cur){
-        if(count == numbers.length){
-            return (cur == target) ? 1 : 0;
+    int dfs(int[] numbers, int idx, int target, int cur){
+        if(numbers.length == idx){
+            return (cur == target) ?  1 : 0;
         }
         
-        int sum = 0;
-        sum += dfs(numbers,count + 1, target, cur + numbers[count]);
-        sum += dfs(numbers,count + 1, target, cur - numbers[count]);
+        int sum =0;
+        sum += dfs(numbers, idx+1, target, cur + numbers[idx]);
+        sum += dfs(numbers, idx+1, target, cur - numbers[idx]);
         
         return sum;
     }
