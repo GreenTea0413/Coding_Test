35d526d6a9ac255d6ccbc1035f2d33c7899ea692
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-07-03 23:27:08 +0900

    [level 2] Title: 소수 찾기, Time: 27.91 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,39 +1,40 @@
 import java.util.*;
 class Solution {
     Set<Integer> nSet = new HashSet<>();
-    boolean[] v;
+    boolean v[];
     public int solution(String numbers) {
         v = new boolean[numbers.length()];
+        
         dfs("", numbers);
         
-        int sum =0;
+        int sum = 0;
         for(int num : nSet){
             if(isPrime(num)){
-                sum ++;
+                sum++;
             }
         }
+        
         return sum;
     }
     
     public void dfs(String cur, String numbers){
-        if(!cur.isEmpty()){
-            nSet.add(Integer.parseInt(cur));
-        }
+        if(!cur.isEmpty()) nSet.add(Integer.parseInt(cur));
         
         for(int i = 0; i < numbers.length(); i++){
             if(!v[i]){
-                v[i]= true;
+                v[i] = true;
                 dfs(cur + numbers.charAt(i), numbers);
-                v[i]= false;
+                v[i] = false;
             }
         }
     }
     
-    public boolean isPrime(int num){
-        if(num < 2) return false;
-        for(int i = 2; i <= Math.sqrt(num); i++){
-            if(num % i == 0) return false;
+    public boolean isPrime(int n){
+        if(n < 2) return false;
+        for(int i = 2;  i<= Math.sqrt(n); i++){
+            if(n % i == 0) return false;
         }
+        
         return true;
     }
 }
