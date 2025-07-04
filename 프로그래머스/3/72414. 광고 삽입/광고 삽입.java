bdd3b843c1ebf64da4c8f25ea70de4bc06f4180d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-07-04 13:04:31 +0900

    [level 3] Title: 광고 삽입, Time: 462.36 ms, Memory: 293 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -5,56 +5,54 @@ class Solution {
         long[] delta = new long[playSec + 1];
         
         for(String log : logs){
-            String[] s = log.split("-");
-            int start = strToInt(s[0]);
-            int end = strToInt(s[1]);
+            String[] sp = log.split("-");
+            int start = strToInt(sp[0]);
+            int end = strToInt(sp[1]);
             delta[start] ++;
             delta[end] --;
         }
         
         for(int i = 1; i < delta.length; i++){
-            delta[i] = delta[i-1] + delta[i];
+            delta[i] = delta[i - 1] + delta[i];
         }
         for(int i = 1; i < delta.length; i++){
-            delta[i] = delta[i-1] + delta[i];
+            delta[i] = delta[i - 1] + delta[i];
         }
         
         int start = 0;
-        long max = delta[advSec-1];
+        long end = delta[advSec - 1];
         
-        for(int i = 0; i < playSec - advSec + 1; i++){
-            if(max < delta[i+advSec] - delta[i]){
-                max = delta[i+advSec] - delta[i];
+        for(int i = 1; i < playSec - advSec + 1; i++){
+            if(end < delta[i + advSec] - delta[i]){
+                end = delta[i + advSec] - delta[i];
                 start = i + 1;
             }
         }
-        
         return intToStr(start);
     }
     
-    
     int strToInt(String str){
-        String[] sp = str.split(":");
+        String[] sp =  str.split(":");
         
         int h = Integer.parseInt(sp[0]);
         int m = Integer.parseInt(sp[1]);
         int s = Integer.parseInt(sp[2]);
         
-        return 3600 * h + 60 * m + s;
+        return h * 3600 + 60 * m + s;
     }
     
-    String intToStr(int s){
-        String sec = String.valueOf(s % 60);
+    String intToStr(int num){
+        String sec = String.valueOf(num % 60);
         if(sec.length() == 1) sec = "0" + sec;
-        s = s /60;
+        num = num / 60;
         
-        String min = String.valueOf(s % 60);
+        String min = String.valueOf(num % 60);
         if(min.length() == 1) min = "0" + min;
-        s = s /60;
+        num = num / 60;
         
-        String hour = String.valueOf(s);
+        String hour = String.valueOf(num);
         if(hour.length() == 1) hour = "0" + hour;
-        
+    
         return hour + ":" + min + ":" + sec;
     }
 }
