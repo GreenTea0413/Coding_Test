9a2455e3f0ffaf5f01f31ec42856ccc470de4266
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-08-22 19:22:20 +0900

    [level 2] Title: 타겟 넘버, Time: 35.43 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,14 +1,14 @@
-function dfs(index, sum, numbers, target){
-    if(index == numbers.length){
-        if(sum == target) return 1;
-        else return 0;
-    }
-    return dfs(index + 1, sum + numbers[index], numbers, target) + dfs(index + 1, sum - numbers[index], numbers, target)
-}
-
 function solution(numbers, target) {
     var answer = 0;
     
+     return dfs(numbers, 0, 0, target)
+}
+
+function dfs (numbers, index, sum, target){
+    if(index === numbers.length){
+        if(sum === target) return 1;
+        else return 0
+    }
     
-    return dfs(0,0,numbers, target);
+    return dfs(numbers, index + 1, sum + numbers[index], target) + dfs(numbers, index + 1, sum - numbers[index], target)
 }
