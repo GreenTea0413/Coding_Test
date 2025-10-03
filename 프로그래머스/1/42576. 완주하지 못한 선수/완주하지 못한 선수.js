c48033e4f04ecea174a33f12447ec60413d565e7
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-10-03 16:32:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.53 ms, Memory: 66.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,19 @@
+function solution(participant, completion) {
+    var answer = "";
+    const map = new Map();
+    
+    for (const name of participant){
+        map.set(name, (map.get(name) || 0) + 1);
+    }
+    
+    for (const name of completion){
+        map.set(name, (map.get(name) - 1));
+    }
+    
+    for(const [name, count] of map.entries()){
+        if(count > 0){
+            answer = name;
+        }
+    }
+    return answer;
+}
