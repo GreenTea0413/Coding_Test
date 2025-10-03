e3bc0d1fc38751266ae5037a2460a55b5831cb87
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-10-03 17:32:57 +0900

    [level 2] Title: 올바른 괄호, Time: 31.85 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 38.5 MB, 시간: 33.83 ms
+메모리: 38.1 MB, 시간: 31.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 09일 10:48:56
+2025년 10월 03일 17:32:56
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,21 +1,16 @@
 function solution(s){
     var answer = true;
-
     const stack = [];
-
-    // 일단 첫번째꺼 넣기
-    for (c of s){
+    
+    for(c of s){
         if(c === '('){
-            stack.push(c);
+            stack.push(c)
         }
         else if(c === ')'){
-            if(stack.length === 0){
-                return false;
-            }
-            else
-                stack.pop();
+            if(stack.length === 0 ) return false 
+            else stack.pop()
         }
     }
-    if(stack.length > 0) answer = false;
+    if(stack.length > 0) return false
     return answer;
 }
