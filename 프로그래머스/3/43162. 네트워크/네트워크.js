db3354f43e0d19f6a4a471d820e0de68b9676077
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-10-05 13:52:00 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,22 +1,28 @@
 function solution(n, computers) {
     var answer = 0;
-    const visited = Array(n).fill(false);
     
+    const visited = Array(n).fill(false)
+    
+    
+    // 110
+    // 110
+    // 001
     for(let i = 0; i < n; i++){
         if(visited[i]) continue
         dfs(n, computers, visited, i)
-        answer ++;
-    }
+        answer++
+    }    
     
     return answer;
 }
 
 function dfs(n, computers, visited, cur){
     visited[cur] = true
+    
     for(let i = 0; i < n; i++){
-        if(!visited[i] && computers[cur][i] == 1){
+        if(computers[i][cur] == 1 && !visited[i]){
             dfs(n, computers, visited, i)
         }
     }
+    
 }
-
