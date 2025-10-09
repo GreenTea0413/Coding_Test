1ebe52f9e970cdb91899c3be52a63f46430d6a18
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-10-09 16:48:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.31 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 55.8 MB, 시간: 18.75 ms
+메모리: 38.3 MB, 시간: 14.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 30일 13:35:19
+2025년 10월 09일 16:48:53
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,30 +1,32 @@
 function solution(maps) {
-    const n = maps.length;
-    const m = maps[0].length; 
     
-    const dx = [0, 0, 1, -1];
-    const dy = [1, -1, 0, 0];
-
-    const visited = Array.from(Array(n),() => Array(m).fill(false));
-    const queue = [[0, 0, 1]];
-    visited[0][0] = true;
+    const dx = [0,0,-1,1]
+    const dy = [1,-1,0,0]
     
-    while (queue.length > 0){
-        const [x, y, dist] = queue.shift();
-        if(x == n - 1 && y == m - 1) return dist;
+    const visited = Array.from({length:maps.length}, () => Array.from({length:maps[0].length}, () => false))
+    
+    const queue = [[0,0,1]]
+    visited[0][0] = true
+    
+    while(queue.length > 0){
+        const [x,y,dist] = queue.shift()
+        
+        if(x === maps.length-1 && y === maps[0].length-1){
+            return dist
+        }
         
         for(let i = 0; i < 4; i++){
-            const nx = x + dx[i];
-            const ny = y + dy[i];
+            const nx = dx[i] + x
+            const ny = dy[i] + y
             
-            if(0 <= nx && nx < n && 0 <= ny && ny < m){
-                if(!visited[nx][ny] && maps[nx][ny] != 0){
-                    visited[nx][ny] = true;
-                    queue.push([nx,ny,dist+1]);
+            if(0 <= nx && nx < maps.length && 0 <= ny && ny < maps[0].length){
+                if(!visited[nx][ny] && maps[nx][ny]){
+                    visited[nx][ny] = true
+                    queue.push([nx, ny, dist + 1])
+                    
                 }
             }
         }
     }
-    
     return -1;
 }
