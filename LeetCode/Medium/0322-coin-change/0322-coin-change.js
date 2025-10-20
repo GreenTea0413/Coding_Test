faaa260e4b9bcd20f65e20c05acc107c08ad078e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-10-20 18:01:48 +0900

    Time: 107 ms (20.94%), Space: 62.7 MB (24.2%) - LeetHub

--- LeetCode/Medium/0322-coin-change/0322-coin-change.js
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.js
@@ -4,26 +4,24 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    if(amount === 0) return 0
-    
-    const len = coins.length;
+    const len = coins.length
+    const visited = Array.from({length : amount + 1}, () => false)
     const queue = [[amount, 0]]
-    const visited = Array.from({length: amount + 1}, () => false)
-    visited[amount] = true
 
-    let head = 0
+    let idx = 0
 
-    while (head < queue.length) {
-        const [remainAmount, count] = queue[head++];
-        if(remainAmount == 0) return count
+    while(idx < queue.length){
+        const [remain, count] = queue[idx++]
+        if(remain === 0) return count
 
         for(const coin of coins){
-            const next = remainAmount - coin
-            if(next >= 0 && !visited[next]){
-                queue.push([next, count + 1])
+            let next = remain - coin
+            if(!visited[next] && next >= 0){
                 visited[next] = true
-            } 
+                queue.push([next, count + 1])
+            }
         }
     }
+
     return -1
 };
