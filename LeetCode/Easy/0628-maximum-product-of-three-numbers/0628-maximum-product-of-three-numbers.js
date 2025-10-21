fc4ed6632e8cefaf2b7101864e56d07b49ab5ca6
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-10-21 16:21:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    if(nums.length === 3) return nums[0] * nums[1] * nums[2]
+    const arr = nums.sort((a,b) => a - b)
+    const len = nums.length
+    let answer = 0
+    console.log(arr)
+    if(arr[0] < 0 && arr[1] < 0 && arr[len-1] >= 0){
+        answer = Math.max(arr[0] * arr[1] * arr[len-1], arr[len-1] * arr[len-2] * arr[len-3])
+    }
+    else if(arr[0] >= 0) {
+        answer = arr[len-1] * arr[len-2] * arr[len - 3]
+    }
+
+    return answer
+};
