0dbadde6438efb5aa84b97c50d3e4484e7e3c0a4
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-10-31 00:03:45 +0900

    [level 3] Title: 등굣길, Time: 1.49 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,20 +1,20 @@
 function solution(m, n, puddles) {
-    const num = 1_000_000_007;
-    const dp = Array.from({ length: n + 1 }, () => Array(m + 1).fill(0));
-
-    for(const[x, y] of puddles){
+    var num = 1_000_000_007
+    const dp = Array.from({length : n + 1}, () => Array(m + 1).fill(0))
+    
+    for(const [x, y] of puddles){
         dp[y][x] = -1
     }
     dp[1][1] = 1
-    for (let y = 1; y <= n; y++) {
-        for (let x = 1; x <= m; x++) {
-            if (dp[y][x] === -1) {
-                dp[y][x] = 0;
-                continue;
+    for(let i = 1; i < n + 1; i++){
+        for(let j = 1; j < m + 1; j++){
+            if(dp[i][j] === -1){
+                dp[i][j] = 0
+                continue
             }
-            if (y > 1) dp[y][x] += dp[y - 1][x] % num;
-            if (x > 1) dp[y][x] += dp[y][x - 1] % num;
-            dp[y][x] %= num;
+            if( i > 1) dp[i][j] += dp[i-1][j] % num
+            if( j > 1) dp[i][j] += dp[i][j-1] % num
+            dp[i][j] %= num
         }
     }
     return dp[n][m];
