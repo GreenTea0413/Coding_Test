8933892684d4db7757ecdf47e26787456c449c05
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-13 00:00:21 +0900

    [level 2] Title: 주식가격, Time: 175.72 ms, Memory: 18.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    l = len(prices)
+    answer = [0] * l
+    
+    for i in range(l) :
+        for j in range(i + 1, l) :
+            if prices[i] <= prices[j] :
+                answer[i] += 1
+            else :
+                answer[i] += 1
+                break
+    return answer
