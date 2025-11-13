882adce3351967a1275ff668df028ff0e1eeae29
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-13 23:35:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.70 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,32 @@
 from collections import deque
 
 def solution(maps):
-    n, m = len(maps), len(maps[0])  # 맵 크기 (행: n, 열: m)
-    dx = [1, -1, 0, 0]  # 상하좌우 이동
-    dy = [0, 0, 1, -1]
+    x = len(maps)
+    y = len(maps[0])
+    answer = -1
 
-    queue = deque([(0, 0)])  # (x, y) 위치를 큐에 삽입
-
-    while queue:
-        cx, cy = queue.popleft()  # 현재 위치 꺼내기
+    queue = deque()
+    queue.append([0,0,1])
+    visited = [[False] * y for _ in range(x)]
+    
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    
+    while queue :
+        a, b, c = queue.popleft()
         
-        # 목표 지점 도달하면 거리 반환
-        if cx == n - 1 and cy == m - 1:
-            return maps[cx][cy]
-
-        # 상하좌우 탐색
-        for i in range(4):
-            nx, ny = cx + dx[i], cy + dy[i]
+        if(a == x-1 and b == y-1) :
+            return c
+        
+        visited[a][b] = True
+        
+        for i in range(4) :
+            nx = dx[i] + a
+            ny = dy[i] + b
             
-            # 맵 범위 안에 있고, 벽이 아니며 방문하지 않은 경우 이동 가능
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1:
-                maps[nx][ny] = maps[cx][cy] + 1  # 거리 갱신
-                queue.append((nx, ny))  # 큐에 추가하여 탐색 계속 진행
-                
-    return -1  # 도착할 수 없는 경우
+            if nx >= 0 and nx < x and ny >= 0 and ny < y :
+                if visited[nx][ny] == False and maps[nx][ny] == 1 :
+                    visited[nx][ny] = True
+                    queue.append([nx,ny,c+1])
+        
+    return answer
