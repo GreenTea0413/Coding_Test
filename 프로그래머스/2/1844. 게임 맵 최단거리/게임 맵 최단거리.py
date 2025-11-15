7d3ea586f0ea6abf119c8c530ff8a9b79e6df6b7
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-15 16:39:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.45 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,32 +1,29 @@
 from collections import deque
 
 def solution(maps):
-    x = len(maps)
-    y = len(maps[0])
     answer = -1
-
+    m = len(maps)       # 세로
+    n = len(maps[0])    # 가로
+    
+    visited = [[False] * n for _ in range(m)]
     queue = deque()
     queue.append([0,0,1])
-    visited = [[False] * y for _ in range(x)]
     
     dx = [0,0,1,-1]
     dy = [1,-1,0,0]
     
     while queue :
-        a, b, c = queue.popleft()
-        
-        if(a == x-1 and b == y-1) :
+        x,y,c = queue.popleft()
+        if x == m - 1 and y == n - 1 :
             return c
         
-        visited[a][b] = True
-        
         for i in range(4) :
-            nx = dx[i] + a
-            ny = dy[i] + b
-            
-            if nx >= 0 and nx < x and ny >= 0 and ny < y :
-                if visited[nx][ny] == False and maps[nx][ny] == 1 :
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx >= 0 and nx < m and ny >= 0 and ny < n :
+                if visited[nx][ny] == False and maps[nx][ny] == 1:
                     visited[nx][ny] = True
-                    queue.append([nx,ny,c+1])
-        
+                    queue.append([nx,ny,c + 1])
+                
     return answer
