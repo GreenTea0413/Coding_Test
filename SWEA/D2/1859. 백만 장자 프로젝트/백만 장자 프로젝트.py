40dd5687b50e12222d6b55c7ee4850c67dd25391
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-18 20:38:10 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 767 ms, Memory: 181,436 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,15 +1,15 @@
 n1 = int(input())
 
-for i in range(1, n1+1) :
-    anw = 0
+for i in range(n1) :
     n2 = int(input())
-    arr = list(map(int, input().split()))
-
+    line = list(map(int,input().split()))
+    
     max = 0
-    for j in range(n2-1, -1, -1):
-        if max < arr[j]:
-            max = arr[j]
+    answer = 0
+    for j in range(n2-1, -1, -1) :
+        if line[j] > max :
+            max = line[j]
         else :
-            anw += max - arr[j]
-
-    print(f"#{i} {anw}")
+            answer += max - line[j]
+            
+    print(f"#{i + 1} {answer}")
