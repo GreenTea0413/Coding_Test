f18dceaeaf92fec614c9f32dd569d78ad9dab898
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-19 18:55:20 +0900

    [D2] Title: 간단한 369게임, Time: 61 ms, Memory: 51,072 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,28 +1,12 @@
-t = int(input())
+n = int(input())
+arr = []
 
-arr = [str(i) for i in range(1, t+1)]
-
-result = [0 for k in range(len(arr))]
-
-for j in range(1, len(arr)+1) :
-    chr = ''
-    count = 0
-    count += arr[j-1].count('3')
-    count += arr[j-1].count('6')
-    count += arr[j-1].count('9')
-
-    if(count == 1) :
-        chr = '-'
-    elif (count == 2) :
-        chr ='--'
-    elif (count == 3) :
-        chr = '---'
-    else : 
-         chr = j
-
-    result[j - 1] = chr
-
-for k in range(len(arr)) :
-    print(result[k], end = " ")
+for i in range(1, n+1) :
+    s = str(i)
+    clap = s.count('3') + s.count('6') + s.count('9')
     
-    
+    if clap == 0 :
+        arr.append(s)
+    else :
+        arr.append('-' * clap)
+print(f"{' '.join(arr)}")
