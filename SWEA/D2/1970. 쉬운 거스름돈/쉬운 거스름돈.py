154855b9f0400b014ef8975f2f55cc9e21307fbc
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-20 16:02:00 +0900

    [D2] Title: 쉬운 거스름돈, Time: 60 ms, Memory: 52,736 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
@@ -1,17 +1,11 @@
-t = int(input())
+test = int(input())
 
-for test in range(1, t + 1) :
-   
-    num = int(input())
+for t in range(1, 1 + test) :
+    n = int(input())
+    nums = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
     
-    arr = [50000, 10000, 5000, 1000,500, 100, 50, 10]
-
-    i = 0
-    print(f"#{test}")
-    while i < 8 :
-        p = int(num / arr[i])
-        num = int(num % arr[i])
-        
-        i += 1
-        print(p, end=" ")
+    print(f"#{t}")
+    for num in nums :
+        print(n // num, end = " ")
+        n = n % num
     print()
