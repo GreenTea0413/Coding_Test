bc636f4b3929eb653b2b905724b1f04fe3361701
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-21 00:17:54 +0900

    [D2] Title: 시각 덧셈, Time: 59 ms, Memory: 52,352 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -0,0 +1,17 @@
+test = int(input())
+
+for t in range(1, test+1) :
+    h1, m1, h2, m2 = map(int, input().split())
+    
+    sum_h = h1 + h2
+    sum_m = m1 + m2
+    
+    if sum_m >= 60 :
+        sum_m = sum_m % 60
+        sum_h += 1
+    
+    if sum_h >= 13 :
+        sum_h = sum_h -12
+
+    print(f"#{t} {sum_h} {sum_m}")
+        
