430e3c11b57664b1b9f145e6b3654b09cd992d8f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-11-21 13:36:56 +0900

    [D3] Title: 농작물 수확하기, Time: 112 ms, Memory: 62,080 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -0,0 +1,32 @@
+test = int(input())
+
+for tc in range(1, test + 1 ) :
+    N = int(input())
+    arr = [[0] * N  for _ in range(N)]
+    for i in range(N) :
+        line = input().strip()
+        for j in range(N) :
+            arr[i][j] = int(line[j])
+    answer = 0
+    mid = int(N // 2)
+    dy = [1,-1]
+    # N // 2 -> 5 // 2 = 2,0
+    # 윗부분 + 중간부분 + 아랫부분
+    # 윗부분
+    for i in range(mid) :
+        answer += arr[i][mid]
+        for j in range(1, i + 1) :
+            for y in dy :
+                ny = y * j + mid
+                answer += arr[i][ny]
+    # 중간
+    for i in range(N) :
+        answer += arr[mid][i]
+    # 아랫 부분 4, 3
+    for i in range(N - 1, mid, -1) :
+        answer += arr[i][mid]
+        for j in range(1, N - i) :
+            for y in dy :
+                ny = y * j + mid
+                answer += arr[i][ny]
+    print(f"#{tc} {answer}")   
