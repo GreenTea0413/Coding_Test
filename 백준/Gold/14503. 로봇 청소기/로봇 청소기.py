11bf9447b716a93c896a0b247815ecb68e31344b
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-12-03 16:22:19 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -2,54 +2,45 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-r, c, d = map(int, input().split())  # 시작 위치 + 방향
-room = [list(map(int, input().split())) for _ in range(N)]
+r,c,a = map(int, input().split())
+arr = [[0]*M for _ in range(N)]
 
-# 방향: 북(0) 동(1) 남(2) 서(3)
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+for i in range(N) :
+    arr[i] = list(map(int,input().split()))
 
-cleaned = 0  # 청소한 칸 수
+answer = 0
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
-while True:
-    # 1. 현재 칸 청소
-    if room[r][c] == 0:
-        room[r][c] = 2  # 청소 완료 표시
-        cleaned += 1
+while True :
+    if arr[r][c] == 0 :
+        arr[r][c] = 2
+        answer += 1
     
-    # 2. 주변 4칸 확인
-    all_cleaned = True
-    for i in range(4):
-        nr = r + dx[i]
-        nc = c + dy[i]
-        if 0 <= nr < N and 0 <= nc < M and room[nr][nc] == 0:
-            all_cleaned = False
+    all_clean = True
+    for i in range(4) :
+        nx = r + dx[i]
+        ny = c + dy[i]
+
+        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0 :
+            all_clean = False
             break
     
-    if all_cleaned:
-        # 3-1. 주변 4칸이 모두 청소되었거나 벽
-        # 후진
-        back_dir = (d + 2) % 4
-        nr = r + dx[back_dir]
-        nc = c + dy[back_dir]
-        
-        if 0 <= nr < N and 0 <= nc < M and room[nr][nc] != 1:
-            # 후진 가능
-            r, c = nr, nc
-        else:
-            # 후진 불가 (벽) → 종료
-            break
-    else:
-        # 3-2. 청소되지 않은 빈 칸이 있음
-        # 반시계 방향으로 90도 회전
-        d = (d + 3) % 4
-        
-        # 앞쪽 칸 확인
-        nr = r + dx[d]
-        nc = c + dy[d]
-        
-        if 0 <= nr < N and 0 <= nc < M and room[nr][nc] == 0:
-            # 전진
-            r, c = nr, nc
+    if all_clean :
+        ba = (a + 2) % 4
+        nx = r + dx[ba]
+        ny = c + dy[ba]
 
-print(cleaned)
+        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] != 1:
+            r = nx
+            c = ny
+        else :
+            break
+    else :
+        a = (a + 3) % 4
+        nx = r + dx[a]
+        ny = c + dy[a]
+        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0:
+            r = nx
+            c = ny
+print(answer)
