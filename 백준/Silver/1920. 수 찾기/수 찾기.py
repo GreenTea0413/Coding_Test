f67009bb99508c4d94c55c23dd4319a4bb84bc7a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-12-04 15:32:50 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,28 +2,10 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr1 = list(map(int, input().split()))
-arr1.sort()
+arr1 = set(map(int, input().split()))
 
 M = int(input())
 arr2 = list(map(int, input().split()))
 
-for a2 in arr2 :
-    left = 0
-    right = N - 1
-    find = 0
-    while left <= right :
-        mid = (left + right) // 2
-        if arr1[mid] > a2 :
-            right = mid - 1
-        elif arr1[mid] < a2 :
-            left = mid + 1
-        else :
-            find = 1
-            break
-    
-    if find == 1 :
-        print(1)
-    else :
-        print(0)
-        
+for num in arr2:
+    print(1 if num in arr1 else 0)
