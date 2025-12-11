c483605d6e1d8e8a23f3f4e4a1dc9fbb3c05d10c
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-12-11 16:38:56 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,16 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-A = list(map(str, input().strip().split('-')))
+s = input().split('-')
 
 answer = 0
-N = A[0].split('+')
-for n in N :
-    answer += int(n)
+first = s[0].split('+')
+for f in first :
+    answer += int(f)
 
-for i in range(1, len(A)) :
-    nums = A[i].split('+')
-    for num in nums :
-        answer -= int(num)
+for i in range(1, len(s)) :
+    part = s[i].split('+')
+    for p in part :
+        answer -= int(p)
 
 print(answer)
