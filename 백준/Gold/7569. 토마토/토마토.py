e443e2f8c6a9000ad86966b9d395ba32f71d02aa
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2025-12-15 00:32:08 +0900

    [Gold V] Title: 토마토, Time: 616 ms, Memory: 216572 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,46 +1,38 @@
 from collections import deque
+import sys
+input=sys.stdin.readline
 
-# 입력 및 초기화
-L, N, M = map(int, input().split())
-toma = [[list(map(int, input().split())) for _ in range(N)] for _ in range(M)]
-queue = deque()
+N,M,H = map(int, input().split())
 
-# 초기 익은 토마토 위치 큐에 추가
-for i in range(M):
-    for j in range(N):
-        for k in range(L):
-            if toma[i][j][k] == 1:
-                queue.append((i, j, k))  # (z, y, x) 형태로 추가
+A = [[list(map(int,input().split())) for _ in range(M)] for _ in range(H)]
+q = deque([])
+for i in range(H) :
+    for j in range(M) :
+        for k in range(N):
+            if A[i][j][k] == 1 :
+                q.append((i,j,k))
 
-# BFS 함수 정의
-def BFS():
-    dx = [-1, 1, 0, 0, 0, 0]
-    dy = [0, 0, -1, 1, 0, 0]
-    dz = [0, 0, 0, 0, -1, 1]
-    while queue:
-        z, y, x = queue.popleft()
-        
-        for i in range(6):
-            nz = z + dz[i]
-            ny = y + dy[i]
-            nx = x + dx[i]
-            
-            if 0 <= nz < M and 0 <= ny < N and 0 <= nx < L:
-                if toma[nz][ny][nx] == 0:
-                    toma[nz][ny][nx] = toma[z][y][x] + 1
-                    queue.append((nz, ny, nx))
+dx = [1,-1,0,0,0,0]
+dy = [0,0,1,-1,0,0]
+dz = [0,0,0,0,-1,1]
+answer = 0
 
-# BFS 실행
-BFS()
+while q :
+    z, y, x = q.popleft()
 
-# 결과 계산
-max_days = 0
-for layer in toma:
-    for row in layer:
-        for tomato in row:
-            if tomato == 0:  # 익지 않은 토마토가 남아 있는 경우
+    for i in range(6) :
+        nx,ny,nz = x +dx[i], y+dy[i], z+dz[i]
+
+        if  0<=nz<H and  0<=ny<M and 0<=nx<N and A[nz][ny][nx] == 0 :
+            q.append((nz,ny,nx))
+            A[nz][ny][nx] = A[z][y][x] + 1
+
+for i in range(H) :
+    for j in range(M) :
+        for k in range(N):
+            if A[i][j][k] == 0 :
                 print(-1)
                 exit()
-            max_days = max(max_days, tomato)
-
-print(max_days - 1)
+            else :
+                answer = max(A[i][j][k], answer)
+print(answer - 1)
