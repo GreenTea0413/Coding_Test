b7364d3616ea3babf49d5f57a8a7915b3789617e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-01-05 15:09:50 +0900

    [Gold IV] Title: 강의실 배정, Time: 304 ms, Memory: 63236 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,16 +1,23 @@
+# 백준 11000 - 강의실 배정
+# https://www.acmicpc.net/problem/11000
+# 그리디 - 우선순위 큐
+
 import sys
 import heapq
 input = sys.stdin.readline
 
 N = int(input())
-A = [list(map(int, input().split())) for _ in range(N)]
-A = sorted(A, key=lambda x : x[0])
-heap = []
-heapq.heappush(heap, A[0][1])
-# 일단 순서대로 넣어야함
-# 1,3 2,4 -> 1,3 끝나고 내보낸다음 3,5를 넣으면 되는데
-for s, e in A[1:] :
-    if heap[0] <= s :
-        heapq.heappop(heap)
-    heapq.heappush(heap, e)
-print(len(heap))
+A = []
+for _ in range(N) :
+    a, b = map(int, input().split())
+    A.append((a,b))
+A.sort(key= lambda x : x[0])
+
+
+hq = []
+for start, end in A :
+    if hq and hq[0] <= start :
+        heapq.heappop(hq)
+    heapq.heappush(hq, end)
+
+print(len(hq))
