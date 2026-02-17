c9c2cb40792f00c96d54c25647d143e02b7e9de5
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-02-17 23:03:18 +0900

    [level 2] Title: 타겟 넘버, Time: 5.80 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 9.26 MB, 시간: 569.12 ms
+메모리: 73.1 MB, 시간: 5.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 13일 23:13:23
+2026년 02월 17일 23:03:16
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, 0, target, 0);
+        int answer = 0;
+        return dfs(numbers, target, 0, 0);
     }
     
-    int dfs(int[] numbers, int idx, int target, int cur){
-        if(numbers.length == idx){
-            return (cur == target) ?  1 : 0;
+    public int dfs(int[] numbers, int target, int count, int index){
+        if (index == numbers.length){
+            if (target == count){
+                return 1;
+            }
+            else return 0;
         }
         
-        int sum =0;
-        sum += dfs(numbers, idx+1, target, cur + numbers[idx]);
-        sum += dfs(numbers, idx+1, target, cur - numbers[idx]);
-        
-        return sum;
+        return dfs(numbers, target, count - numbers[index], index + 1) + dfs(numbers, target, count + numbers[index], index + 1);
     }
 }
