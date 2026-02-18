708317927a2eb4b4651013a468e4d449a966fd42
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-02-18 15:37:40 +0900

    [level 2] Title: 소수 찾기, Time: 23.68 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,40 +1,45 @@
 import java.util.*;
+
 class Solution {
-    Set<Integer> nSet = new HashSet<>();
-    boolean[] v;
+    Set<Integer> set = new HashSet<>();
+    
     public int solution(String numbers) {
-        v = new boolean[numbers.length()];
-        
-        dfs("", numbers);
-        
         int answer = 0;
-        for(int num : nSet){
-            if(isPrime(num)){
-                answer ++;
+        int len = numbers.length();
+        boolean[] v = new boolean[len];
+        permutation("", v, numbers);
+        
+        for (int num : set){
+            if(prime(num)){
+                answer++;
             }
         }
         
         return answer;
     }
     
-    void dfs(String cur, String numbers){
-        if(!cur.equals("")) nSet.add(Integer.parseInt(cur));
-        
-        for(int i =0 ; i < numbers.length(); i++){
-            if(!v[i]){
-                v[i] =true;
-                dfs(cur + numbers.charAt(i) , numbers);
-                v[i] =false;
+    public void permutation(String current, boolean[] visited, String numbers){
+        if (!current.equals("")){
+            set.add(Integer.parseInt(current));
+        }
+        int len = numbers.length();
+        for(int i = 0; i < len; i ++){
+            if (!visited[i]){
+                visited[i] = true;
+                permutation(current + numbers.charAt(i), visited, numbers);
+                visited[i] = false;
             }
         }
     }
     
-    boolean isPrime(int n){
-        if(n < 2) return false;
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0) return false;
-        }
+    public boolean prime(int num){
+        if (num < 2) return false;
         
+        for (int i = 2; i <= Math.sqrt(num); i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
         return true;
     }
 }
