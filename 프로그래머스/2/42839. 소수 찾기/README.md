8ce22813c946756af794f007da28aaf31b90b5fa
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-02-19 09:39:28 +0900

    [level 2] Title: 소수 찾기, Time: 30.36 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 96.6 MB, 시간: 23.68 ms
+메모리: 81.7 MB, 시간: 30.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 18일 15:37:38
+2026년 02월 19일 09:39:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     Set<Integer> set = new HashSet<>();
     
@@ -7,38 +6,37 @@ class Solution {
         int answer = 0;
         int len = numbers.length();
         boolean[] v = new boolean[len];
-        permutation("", v, numbers);
         
-        for (int num : set){
-            if(prime(num)){
+        permute("",v,numbers);
+        
+        for(int num : set){
+            if (prime(num)){
                 answer++;
             }
         }
-        
         return answer;
     }
     
-    public void permutation(String current, boolean[] visited, String numbers){
+    public void permute(String current, boolean[] visited, String numbers){
         if (!current.equals("")){
             set.add(Integer.parseInt(current));
         }
-        int len = numbers.length();
-        for(int i = 0; i < len; i ++){
-            if (!visited[i]){
+        
+        for (int i = 0; i < numbers.length(); i++){
+            if(!visited[i]){
                 visited[i] = true;
-                permutation(current + numbers.charAt(i), visited, numbers);
+                permute(current + numbers.charAt(i), visited, numbers);
+                
                 visited[i] = false;
             }
         }
     }
     
-    public boolean prime(int num){
-        if (num < 2) return false;
+    public boolean prime(int number){
+        if (number < 2) return false;
         
-        for (int i = 2; i <= Math.sqrt(num); i++){
-            if (num % i == 0){
-                return false;
-            }
+        for (int i = 2; i <= Math.sqrt(number); i++){
+            if (number % i == 0) return false;
         }
         return true;
     }
