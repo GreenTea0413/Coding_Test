6c9aacebf8c29d26a6cce842eefc269f0cb659bb
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-02-24 12:46:48 +0900

    [level 3] Title: 단속카메라, Time: 9.13 ms, Memory: 58.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1]));
+        int camera = Integer.MIN_VALUE;
+        
+        for(int[] r : routes){
+            if (r[0] > camera){
+                camera= r[1];
+                answer++;
+            }    
+        }
+        
+        return answer;
+    }
+}
