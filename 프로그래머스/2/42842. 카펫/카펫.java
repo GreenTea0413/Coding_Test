2726f176d46f54c0630e9a79fbb284878da56e8e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-17 00:27:56 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int sq = brown + yellow;
-        // 너비를 딱 나누는 가로 s
-        // 가로 s * 높이 h 
-        for(int s = 1; s < sq; s++){
-            if(sq % s == 0){
-                int h = sq / s;
-                if(s >= h && (s-2) * (h-2) == yellow){
-                    answer = new int[]{s, h};
+        int sum = brown + yellow;
+        
+        for(int h = 1; h < sum; h++){
+            if(sum % h == 0){
+                int w = sum / h;
+                
+                if(w >= h && (w-2) * (h-2) == yellow) {
+                    return new int[]{w, h};
                 }
             }
+            
         }
-        
-        
         return answer;
     }
 }
