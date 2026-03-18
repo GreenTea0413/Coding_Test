e3f9d3b4e2ab659032e599ff204cebacb3603d8c
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-18 17:21:51 +0900

    [level 2] Title: 할인 행사, Time: 59.03 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
 
 ### 성능 요약
 
-메모리: 124 MB, 시간: 58.18 ms
+메모리: 126 MB, 시간: 59.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 03일 14:33:22
+2026년 03월 18일 17:21:46
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,30 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        // 14개면 0 ~ 9, 1 ~ 10, 2 ~ 11, 3 ~ 12, 4 ~ 13
         for(int i = 0; i < discount.length - 9; i++){
             int[] copy = number.clone();
             for(int j = i; j < i + 10; j++){
                 for(int l = 0; l < want.length; l++){
-                    if (want[l].equals(discount[j])){
-                        copy[l] -= 1;
-                    }
+                    if(want[l].equals(discount[j])) copy[l] -= 1;
                 }
+                
+                boolean check = true;
+                for(int k = 0; k < copy.length; k++){
+                    if (copy[k] < 0) {check = false; break;}
+                    if (copy[k] > 0) {check = false; break;}
+                }
+                
+                if(check) answer++;
             }
-            
-            boolean check = true;
-            for(int k = 0; k < number.length; k++){
-                if (copy[k] < 0) {check = false;}
-                else if(copy[k] >= 1) {check = false;}
-            }
-            
-            if (check) {answer ++;}
         }
-        
-        
         return answer;
     }
 }
