27da6cb93dce138ee8a4560d5bd0bebfa2f56dae
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-18 17:08:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 266.65 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=java) 
 
 ### 성능 요약
 
-메모리: 119 MB, 시간: 349.66 ms
+메모리: 134 MB, 시간: 266.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 03일 14:11:08
+2026년 03월 18일 17:08:29
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,29 +1,24 @@
 import java.util.*;
-
 class Solution {
-    
     public int solution(int[] elements) {
-        // 7 9 1 1 4
-        // i는 부분 순열 길이 1 ~ 5
-        int n = elements.length;
-        int[] arr = new int[n * 2];
-        
-        // 배열 2배로 만들기
-        for (int i = 0; i < n * 2; i++) {
-            arr[i] = elements[i % n];
+        int len = elements.length;
+        int[] arr = new int[len * 2];
+        for(int i = 0; i < len * 2; i ++){
+            arr[i] = elements[i % len];
         }
         
-        Set<Integer> set = new HashSet<>();
-        for (int len = 1; len <= n; len++){
-            for(int start = 0; start < n; start++){
-                // 0부터 i만큼의 길이의 합을 구해서 set에 넣기
+        Set<Integer> answer = new HashSet<>();
+        for(int i = 1; i <= len; i++){
+            for(int j = 0; j < len; j++){
                 int sum = 0;
-                for (int k = 0; k < len; k++) {
-                    sum += arr[start + k];
+                for(int start = j; start < j + i; start++){
+                    sum += arr[start];
                 }
-                set.add(sum);
+                answer.add(sum);
             }
         }
-        return set.size();
+        
+        
+        return answer.size();
     }
 }
