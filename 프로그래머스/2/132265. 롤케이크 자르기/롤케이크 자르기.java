c99c73af2c7a6572ff0518d9e6844c867bd38779
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-22 15:10:31 +0900

    [level 2] Title: 롤케이크 자르기, Time: 273.91 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,22 +1,24 @@
 import java.util.*;
-class Solution {    
+
+class Solution {
+    
     public int solution(int[] topping) {
         int answer = 0;
-        // 토빙 번호, 갯수
-        Map<Integer, Integer> left = new HashMap<>();
-        Map<Integer, Integer> right = new HashMap<>();
+        int len = topping.length;
+        HashMap<Integer, Integer> left = new HashMap<>();
+        HashMap<Integer, Integer> right = new HashMap<>();
         
-        left.put(topping[0], left.getOrDefault(topping[0], 0) + 1);
-        for(int i = 1; i < topping.length; i++){
+        left.put(topping[0], 1);
+        for(int i = 1; i < len; i ++){
             right.put(topping[i], right.getOrDefault(topping[i], 0) + 1);
         }
         
-        for(int i = 1; i < topping.length - 1; i++){
-            left.put(topping[i], left.getOrDefault(topping[i], 0) + 1);
+        for(int i = 1; i < len-1; i++){
+            left.put(topping[i], left.getOrDefault(topping[i] ,0) + 1);
             right.put(topping[i], right.get(topping[i]) - 1);
-            if (right.get(topping[i]) == 0) {right.remove(topping[i]);}
             
-            if(left.size() == right.size()) {answer += 1;}
+            if(right.get(topping[i]) == 0) right.remove(topping[i]);
+            if(left.size() == right.size()) answer += 1;
         }
         return answer;
     }
