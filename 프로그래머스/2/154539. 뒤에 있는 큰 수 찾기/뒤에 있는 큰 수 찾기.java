7c534d85d9de11c6d95f741f4662fcaae5352e6b
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-22 16:11:32 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 109.61 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,19 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
         int len = numbers.length;
         int[] answer = new int[len];
-        Deque<Integer> stack = new ArrayDeque<>();
+        Stack<Integer> stack = new Stack<>();
+        // 2 3 3 5 -> 3 5 5 -1
+        // len-2 부터 시작
         
-        for(int i = 0; i < len; i ++){
-            while(!stack.isEmpty() && numbers[stack.peek()] < numbers[i]){
-                answer[stack.pop()] = numbers[i];
+        for(int i = len - 1; i >= 0; i--){
+            // 현재 값
+            int cur = numbers[i];
+            // q에서 하나씩 꺼내서 이거보다 큰게 있는지 확인
+            // q는 배열 뒤에서 부터 담기로 했음
+            while(!stack.isEmpty() && numbers[stack.peek()] <= cur){      
+                stack.pop();
             }
+            
+            answer[i] = stack.isEmpty() ? -1 : numbers[stack.peek()];
+            
             stack.push(i);
-        }
-        
-        while(!stack.isEmpty()){
-            answer[stack.pop()] = -1;
         }
         return answer;
     }
