78aa14968a0bcfed53376759539ee053ff0aad14
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-22 13:26:54 +0900

    [level 2] Title: H-Index, Time: 0.88 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,14 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations); 
-        int n = citations.length;
-        for (int i = 0; i < n; i++) {
-            int h = n - i;
-            if (citations[i] >= h) {
+        int answer = 0;
+        int len = citations.length;
+        Arrays.sort(citations);
+        
+        for(int i = 0; i < len; i++){
+            int h = len - i;
+            if(citations[i] >= h){
                 return h;
             }
         }
-        return 0;
+        return answer;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.92 ms
+메모리: 81 MB, 시간: 0.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 18일 13:21:34
+2026년 03월 22일 13:26:52
 
 ### 문제 설명
 
