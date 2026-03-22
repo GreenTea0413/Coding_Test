a3c9b775a038e34d0daa3048b5fb36de21fecc13
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-22 14:28:03 +0900

    [level 2] Title: 행렬의 곱셈, Time: 11.07 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
+        // 배열 2개가 주어지면
+        // 1의 전체길이, 2[0]의 길이, 1[0]의 길이
+        // i -> n, j -> m , t -> k
+        // answer[i][j] = 1[i][t] + 2[t][j];
         int n = arr1.length;
         int m = arr2[0].length;
         int k = arr1[0].length;
