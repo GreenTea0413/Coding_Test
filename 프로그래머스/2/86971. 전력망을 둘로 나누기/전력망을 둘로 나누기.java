14fbfa5d676e4e04d6b275d188f640cd8ec1af3f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-27 14:05:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.84 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,58 +1,51 @@
 import java.util.*;
-
 class Solution {
-    public int bfs(List<Integer>[] graph, boolean[] v, int start){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        v[start] = true;
+    public int solution(int n, int[][] wires) {
+        int answer = n;
+        // 트리를 끊고 확인해야함
+        // 그래프 만들어서 양방향 연결
+        // 다 연결한 후에 wires에서 하나씩 꺼내서 제거하고 다시 붙이고 해야할듯
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i <= n; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] wire : wires){
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+        }
         
+        for(int[] wire : wires){
+            int x = wire[0];
+            int y = wire[1];
+            // 여기서 삭제하고
+            graph.get(wire[0]).remove(Integer.valueOf(wire[1]));
+            graph.get(wire[1]).remove(Integer.valueOf(wire[0]));
+            // n - 그래프 돌고 온거 확인 갯수
+            // 그래서 2개 비교하고 answer랑 비교해서 최솟값
+            // dfs에 그러면 삭제한 그래프를 넣고 어떻게 비교하지?
+            // 어디서 부터 시작을 돌려야할까..
+            boolean[] v = new boolean[n + 1];
+            int graph1 = dfs(graph, v, x); 
+            int graph2 = n - graph1;
+            int gap = Math.abs(graph1 - graph2);
+            
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+            
+            answer = Math.min(answer, gap);
+        }
+        return answer;
+    }
+    
+    public int dfs(List<List<Integer>> graph, boolean[] v, int start){
+        v[start] = true;
         int count = 1;
         
-        while(!q.isEmpty()){
-            int n = q.poll();
-            
-            for(int next : graph[n]){
-                if(!v[next]){
-                    v[next] =true;
-                    count++;
-                    q.add(next);
-                }
+        for(int next : graph.get(start)){
+            if(!v[next]){
+                count += dfs(graph, v, next);
             }
         }
         return count;
-        
-    }
-    
-    public int solution(int n, int[][] wires) {
-        int answer = 101;
-        List<Integer>[] graph = new ArrayList[n+1];
-        for(int i = 0; i < n + 1; i ++){
-            graph[i] = new ArrayList<>();
-        }
-        
-        for(int[] w : wires){
-            graph[w[0]].add(w[1]);
-            graph[w[1]].add(w[0]);
-        }
-        
-        for(int[] w : wires){
-            int a = w[0];
-            int b = w[1];
-            
-            graph[a].remove(Integer.valueOf(b));
-            graph[b].remove(Integer.valueOf(a));
-            
-            boolean[] v = new boolean[n+1];
-            int count = bfs(graph,v,a);
-            int diff = Math.abs(n - 2 * count);
-            
-            answer = Math.min(answer ,diff);
-            
-            graph[a].add(b);
-            graph[b].add(a);
-        }
-        
-        if(answer == 101) return 0;
-        return answer;
     }
 }
