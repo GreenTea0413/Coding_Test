dda897596a53f2596b1da8c89b9a0494c88a2986
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-27 12:36:29 +0900

    [level 2] Title: 삼각 달팽이, Time: 62.07 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,36 +1,36 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int n) {
-        int[][] dp = new int[n+1][n+1];
-        List<Integer> answer = new ArrayList<>();
-        int[][] d = {{1, 0}, {0, 1}, {-1, -1}};
-        int len = n;
-        int dir = 0;
-        int count = 0;
-        int num = 1;
-        int x = 0;
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        int[][] result = new int[n][n];
+        int[][] d = new int[][]{{1,0}, {0,1}, {-1, -1}};
+        // 이게 뭐람
+        // 2차원 배열을 만들어서 방향 바꾸면서 만들기?
+        // 처음 n 만큼씩 이동하고 결국 n-1, n-2 이렇게만 움직이면됨
+        // n번 반복하는 동안
+        int x = -1;
         int y = 0;
-        
-        while(n > 0){
-            x = x + d[dir % 3][0];
-            y = y + d[dir % 3][1];
-            dp[x][y] = num;
-            num++;
-            count++;
-            if(count == n){
-                n--;
-                count = 0;
-                dir += 1;
+        int dir = 0;
+        int num = 1;
+        for(int i = n; i > 0; i--){
+            int count = i;
+            while (count > 0){
+                int nx = x + d[dir % 3][0];
+                int ny = y + d[dir % 3][1];
+                
+                result[nx][ny] = num;
+                num ++;
+                count--;
+                x = nx;
+                y = ny;
             }
+            dir += 1;
         }
-        
-        for(int i = 1; i < len + 1; i++){
-            for(int j = 0; j < len; j++){
-                if (dp[i][j] != 0){
-                    answer.add(dp[i][j]);
-                }
-            }
+        for(int[] res : result){
+            for(int r : res){
+                if(r != 0) {answer.add(r);}
+            } 
         }
         return answer.stream().mapToInt(i -> i).toArray();
     }
