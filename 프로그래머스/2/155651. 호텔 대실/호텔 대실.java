daaeb8ef9eb9440a557f1197ee5e977000d1ea7d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-28 14:27:08 +0900

    [level 2] Title: 호텔 대실, Time: 9.00 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,24 +1,31 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
+        int answer = 0;
+        Arrays.sort(book_time, (a, b) -> a[0].compareTo(b[0]));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Arrays.sort(book_time, (a, b) -> {
-            return a[0].compareTo(b[0]);
-        });
         
-        for(String[] b : book_time){
-            int start = toNum(b[0]);
-            int end = toNum(b[1]) + 10;
+        // 일단 끝나는 시간 + 10
+        for(String[] book : book_time){
+            // 시작 시간을 구하고
+            int start = toNum(book[0]);
             
-            if(!pq.isEmpty() && pq.peek() <= start){
+            // 현재 방 끝나는 시간이 담겨 있는 큐에서 지금 시작보다 작으면 다 버려야함
+            while(!pq.isEmpty() && pq.peek() <= start){
                 pq.poll();
             }
-            pq.offer(end);
+            
+            // 그 다음 여기가 문제네..
+            // 일단 청소해야하니까 일단 넣는게 맞는데 answer에 값을 어떻게 넣을 것인가..
+            // 
+            pq.offer(toNum(book[1]) + 10);
+            answer = Math.max(answer, pq.size());
         }
-        return pq.size();
+        return answer;
     }
-    public int toNum(String str){
-        String[] s= str.split(":");
-        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
+    
+    public int toNum(String time){
+        String[] min = time.split(":");
+        return Integer.parseInt(min[0]) * 60 + Integer.parseInt(min[1]);
     }
 }
