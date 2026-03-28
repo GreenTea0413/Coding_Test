30ab6f65d6f845b40413eac4b0854e65f86fd9e4
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-28 14:49:54 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 26.26 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,29 +1,31 @@
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        int len = arrayA.length;
+        // 음 그러니까 arrayA의 최소공배수와 arrayB의 최소공배수를 구하라?
+        // 구하고나면 이걸로 최소 공배수가 다른 배열의 값을 나눌 수 있는지 확인해라?
         int gcdA = arrayA[0];
         int gcdB = arrayB[0];
         
         for(int a : arrayA) gcdA = gcd(gcdA, a);
         for(int b : arrayB) gcdB = gcd(gcdB, b);
         
-        int answer = 0;
-        if(divide(arrayA, gcdB)) answer = Math.max(answer, gcdB);
-        if(divide(arrayB, gcdA)) answer = Math.max(answer, gcdA);
+        if(isDivide(arrayA, gcdB)) answer = Math.max(answer, gcdB);
+        if(isDivide(arrayB, gcdA)) answer = Math.max(answer, gcdA);
         
         return answer;
     }
-    public int gcd (int a, int b){
+    
+    public int gcd(int a, int b){
         if(b == 0) return a;
         return gcd(b, a % b);
     }
     
-    public boolean divide(int[] arr, int n){
+    public boolean isDivide(int[] arr, int n){
         for(int a : arr){
-            if(a % n == 0){
-                return false;
-            }
+            if(a % n == 0) return false;
         }
-        
         return true;
     }
+    
 }
