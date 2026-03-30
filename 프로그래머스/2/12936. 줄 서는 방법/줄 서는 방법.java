6ff49e451ef9208c7998e9bdeb86feae4351d91d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-30 18:47:23 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -4,21 +4,19 @@ class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
         
-        // 팩토리얼 미리 계산
         long[] factorial = new long[n + 1];
         factorial[0] = 1;
-        for(int i = 1; i <= n; i++) factorial[i] = factorial[i-1] * i;
+        for(int i = 1; i <= n; i++){factorial[i] = factorial[i - 1] * i;}
         
-        // 사용 가능한 숫자 목록
         List<Integer> nums = new ArrayList<>();
         for(int i = 1; i <= n; i++) nums.add(i);
         
-        k--; // 0-indexed로 변환
+        k--;
         
         for(int i = n; i >= 1; i--){
-            long fact = factorial[i - 1];
-            int idx = (int)(k / fact); // 몇 번째 숫자 선택
-            answer[n - i] = nums.get(idx);
+            long fact = factorial[i-1];
+            int idx = (int) (k / fact);
+            answer[n-i] = nums.get(idx);
             nums.remove(idx);
             k %= fact;
         }
