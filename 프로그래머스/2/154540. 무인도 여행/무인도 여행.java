331e230f26de537bcc17b575aafff6f1e2222c60
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-30 16:42:39 +0900

    [level 2] Title: 무인도 여행, Time: 10.08 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,68 +1,43 @@
 import java.util.*;
-
 class Solution {
-    int[][] d = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-    int x_len;
-    int y_len;
     public int[] solution(String[] maps) {
-        ArrayList<Integer> island = new ArrayList<>();
-        x_len = maps.length;
-        y_len = maps[0].length();
-        
-        int[][] arr = new int[x_len][y_len];
-        boolean[][] v = new boolean[x_len][y_len];
-        
-        // map 만들기
-        int i = 0;
-        for(String s : maps){
-            for(int j = 0; j < y_len; j++){
-                char temp = s.charAt(j);
-                arr[i][j] = (temp == 'X') ? -1 : temp - '0';
-            }
-            i++;
-        }
-                
-        // X가 아니고 방문안했다면?
-        for(int k = 0; k < x_len; k++){
-            for(int l = 0; l < y_len; l++){
-                if(!v[k][l] && arr[k][l] != -1){
-                    island.add(bfs(arr, k, l, v));
-                }
-            }
-        }
-        
-        
-        if (island.size() == 0 ) return new int[]{-1};
-        
-        int[] answer = island.stream().mapToInt(n -> n).toArray();
-        Arrays.sort(answer);
-        return answer;
-    }
-    
-    public int bfs(int[][] arr, int x, int y, boolean[][] v){
-        int food = 0;
-        v[x][y] = true;
-        
+        int lenX = maps.length;
+        int lenY = maps[0].length();
+        List<Integer> answer = new ArrayList<>();
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y});
+        boolean[][] v = new boolean[lenX][lenY];
+        int[][] d = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            int cx = now[0];
-            int cy = now[1];
-            food += arr[cx][cy];
-            
-            for(int i =0; i < 4; i++){
-                int nx = cx + d[i][0];
-                int ny = cy + d[i][1];
-                
-                if(0 <= nx && nx < x_len && 0 <= ny && ny < y_len && !v[nx][ny] && arr[nx][ny] != -1){
-                    v[nx][ny] = true;
-                    q.offer(new int[]{nx, ny});
+        
+        for(int i = 0; i < lenX; i++){
+            for(int j = 0; j < lenY; j++){
+                if (maps[i].charAt(j) != 'X' && !v[i][j]){
+                    q.offer(new int[]{i, j});
+                    v[i][j] = true;
+                    int sum = 0;
+                    while(!q.isEmpty()){
+                        int[] now = q.poll();
+                        int x = now[0];
+                        int y = now[1];
+                        sum += maps[x].charAt(y) - '0';
+                        
+                        for(int k = 0; k < 4; k ++){
+                            int nx = x + d[k][0];
+                            int ny = y + d[k][1];
+                            
+                            if(0<= nx && nx< lenX && 0 <= ny && ny < lenY && !v[nx][ny] && maps[nx].charAt(ny) != 'X'){
+                                v[nx][ny] = true;
+                                q.offer(new int[]{nx, ny});
+                            }
+                        }
+                    }
+                    answer.add(sum);
                 }
             }
         }
         
-        return food;   
+        if (answer.size() == 0) return new int[]{-1};
+        Collections.sort(answer);
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
