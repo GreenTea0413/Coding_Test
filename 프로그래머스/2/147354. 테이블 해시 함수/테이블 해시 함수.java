6ff93f664a393148cf3a37b68ff45366aa1bae69
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-30 16:11:37 +0900

    [level 2] Title: 테이블 해시 함수, Time: 11.13 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -2,20 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
-        // 테이블의 튜플을 col번째 컬럼의 값을 기준으로 오름차순 정렬을 하되, 만약 그 값이 동일하면 기본키인 첫 번째 컬럼의 값을 기준으로 내림차순 정렬
+        // 열은 컬럼, 행은 튜플
+        // col번째 기준으로 오름차순
+        // 값 같으면 col번째 값이 같다면 data[i][0] 값을 기준으로 내림차순
+        // 이게 무슨 소리일까..
+        // 정렬 다 된거에다가 꺼내서 i 번째 튜플의 date[i] 값들을 i로 나눈 나머지로 합을 정의
+        // i의 범위는 row_begin과 row_end 사이
+        // 그리고 그게 bitwise XOR -> ^ 한 값을 해시 값으로서 반환
+        
         Arrays.sort(data, (a, b) -> {
-            if(a[col - 1] == b[col - 1]) return b[0] - a[0];
+            if (b[col - 1] == a[col - 1]) return b[0] - a[0];
             return a[col - 1] - b[col - 1];
         });
-        // row_begin ≤ i ≤ row_end 인 모든 S_i를 누적하여 bitwise XOR 한 값을 해시 값으로서 반환
-        for (int i = row_begin; i <= row_end; i++){
-            int s = 0;
-            for(int d : data[i-1]){
-                s += d % i;
+        
+
+        for(int i = row_begin; i <= row_end; i++){
+            int sum = 0;
+            for (int d : data[i - 1]){
+                sum += d % i;
             }
-            
-            answer ^= s;
+            answer ^= sum;
         }
+        
         return answer;
     }
 }
