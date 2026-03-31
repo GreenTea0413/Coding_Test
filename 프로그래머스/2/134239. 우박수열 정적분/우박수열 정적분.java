6be423513d72473dcbdb34b5e2772e962d0852b7
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-03-31 15:36:07 +0900

    [level 2] Title: 우박수열 정적분, Time: 1.07 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,37 +1,39 @@
 import java.util.*;
 class Solution {
     public double[] solution(int k, int[][] ranges) {
-        // 우박 수열
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(k);
+        // (0, 5), (1, 16), (2, 8), (3, 4), (4, 2), (5, 1)
+        List<Integer> list = new ArrayList<>();
+        list.add(k);
         while(k > 1){
-            if (k % 2 == 0) k /= 2;
-            else k = k * 3 + 1;
-            arr.add(k);
+            if (k % 2 == 0) { k /= 2;}
+            else {k = k * 3 + 1;}
+            list.add(k);
         }
         
-        // 넓이구하고
-        int n = arr.size() - 1;
+        // 미리 모든 한칸한칸 구간의 넓이를 구하기
+        // 0 ~ 1, 1 ~ 2, 2 ~ 3, 3 ~ 4, 4 ~ 5
+        int n = list.size() - 1;
         double[] sqr = new double[n];
-        for(int i = 0; i < n; i ++){
-            sqr[i] = (arr.get(i) + arr.get(i+1)) / 2.0;
+        for(int i = 0; i < n; i++){
+            sqr[i] = (list.get(i)+ list.get(i + 1)) / 2.0;
         }
         
-        // 각 범위별로 0,0 ~~ 현재 위치에 대한 총 넓이
+        // 그 다음 범위에 대한 넓이 구하기
+        // 1 -> 0 ~ 1 2 -> 0 ~ 2...
         double[] total_sqr = new double[n + 1];
         for(int i = 0; i < n; i++){
             total_sqr[i + 1] = total_sqr[i] + sqr[i];
         }
         
+        // x = a, x = n - b, y = 0
         double[] answer = new double[ranges.length];
         for(int i = 0; i < ranges.length; i++){
             int a = ranges[i][0];
             int b = n + ranges[i][1];
             
-            if (a > b) answer[i] = -1;
+            if(a > b) answer[i] = -1;
             else {answer[i] = total_sqr[b] - total_sqr[a];}
         }
-        
         
         return answer;
     }
