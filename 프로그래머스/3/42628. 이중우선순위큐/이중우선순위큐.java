27e40657dab7783f134b39fc009a9e16c90424a1
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-04-07 00:05:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 75.10 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,41 +1,39 @@
 import java.util.*;
 
 class Solution {
+    // 최대큐 최소큐 2개 만들어서 작성
     public int[] solution(String[] operations) {
-
-        PriorityQueue<Integer> min_pq = new PriorityQueue<>();
-        PriorityQueue<Integer> max_pq =
-                new PriorityQueue<>(Collections.reverseOrder());
-
-        for (String s : operations) {
-
-            String[] c = s.split(" ");
-
-            if (c[0].equals("I")) {
-                int num = Integer.parseInt(c[1]);
-                min_pq.offer(num);
-                max_pq.offer(num);
+        PriorityQueue<Integer> minQ = new PriorityQueue<>();
+        PriorityQueue<Integer> maxQ = new PriorityQueue<>((a, b) -> {
+            return b - a;
+        });
+        
+        for(String op : operations){
+            String[] str = op.split(" ");
+            
+            if(str[0].equals("I")){
+                int num = Integer.parseInt(str[1]);
+                maxQ.offer(num);
+                minQ.offer(num);
             }
-
-            else if (c[0].equals("D")) {
-
-                if (min_pq.isEmpty()) continue;
-
-                if (c[1].equals("1")) {        // 최댓값 삭제
-                    int max = max_pq.poll();
-                    min_pq.remove(max);       // 동기화
+            
+            else{
+                // 16 -5643
+                // -5643 16
+                if(maxQ.isEmpty() || minQ.isEmpty()) continue;
+                
+                if (str[1].equals("1")){
+                    int max = maxQ.poll();
+                    minQ.remove(max);
                 }
-
-                else {                        // 최솟값 삭제
-                    int min = min_pq.poll();
-                    max_pq.remove(min);       // 동기화
+                else{
+                    int min = minQ.poll();
+                    maxQ.remove(min);
                 }
             }
         }
-
-        if (min_pq.isEmpty())
-            return new int[]{0, 0};
-
-        return new int[]{max_pq.peek(), min_pq.peek()};
+        
+        if (maxQ.isEmpty()) return new int[]{0,0};
+        return new int[] {maxQ.peek(), minQ.peek()};
     }
 }
