59226c97c07bf599ab66f8ecaf1b1e95807bd24e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-04-07 11:28:46 +0900

    [level 3] Title: 숫자 게임, Time: 69.32 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/README.md
+++ 프로그래머스/3/12987. 숫자 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 숫자 게임 - 12987 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987#) 
 
 ### 성능 요약
 
-메모리: 66.5 MB, 시간: 108.62 ms
+메모리: 71.1 MB, 시간: 69.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 24일 12:23:28
+2026년 04월 07일 11:28:43
 
 ### 문제 설명
 
--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,16 +1,21 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] A, int[] B) {
+        int answer = 0;
+        // N명씩 두팀
+        // 둘다 정렬하고 하면 되는거 아닌가?
         Arrays.sort(A);
         Arrays.sort(B);
-        
         int i = 0;
         int j = 0;
-        int answer = 0;
         
-        while (i < A.length && j < B.length){
-            if (A[i] < B[j]){
+        // 만약에 B의 값이 더 크다면?
+        // 그 때는 answer += 1 left += 1해서 다음꺼로 가면됨
+        
+        // 만약에 A의 값이 더 크다면?
+        // 그러면 left += 1만 해서 A의 다음 숫자와 B첫번째 숫자로 해서 비교해야할듯?
+        while(i < A.length && j < B.length){
+            if(A[i] < B[j]){
                 i++;
                 j++;
                 answer++;
@@ -18,8 +23,7 @@ class Solution {
             else{
                 j++;
             }
-        }
-        
+        }      
         
         return answer;
     }
