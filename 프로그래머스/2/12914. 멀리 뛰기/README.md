e9f809a37e20505a7e3c79e832734371aa8ec37c
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-04-16 15:13:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 0.20 ms
+메모리: 88.4 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 18일 16:52:00
+2026년 04월 16일 15:13:11
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,13 +1,14 @@
 class Solution {
     public long solution(int n) {
-        long MOD = 1234567;
+        long answer = 0;
         long[] dp = new long[n + 1];
-        if(n <= 2) return n;
+        if (n == 1) return 1;
         
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i < n+1; i++){
-            dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
         return dp[n];
     }
