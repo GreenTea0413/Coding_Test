8a955d78538205385d8ee5484a7bd60267fc1ef6
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-04-23 14:51:47 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 8.78 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,36 +1,33 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
-        // 작업의 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것 순으로 우선순위
-        // 시간 같으면 요청 시각
-        // 요청 시각 같으면 번호 작은거
-        int time = 0;
-        int idx = 0;
-        int count = 0;
         int answer = 0;
+        int time = 0;
         int len = jobs.length;
+        int count = 0;
+        int idx = 0;
+        //도착하는 순서대로 정렬
         Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            return a[1] - b[1];
-        });
+        // PQ는 도착 순서 전에 실행 가는 한 것들을 기준으로 실행시간 빠른거 먼저
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
         
-        while (count < len){
-            while (idx < len && jobs[idx][0] <= time){
-                pq.offer(jobs[idx]);
-                idx ++;
+        while(count < len){
+            //도착한 애들 넣기 
+            while(idx < len && jobs[idx][0] <= time){
+                pq.offer(new int[]{jobs[idx][0], jobs[idx][1]});
+                idx++;
             }
             
-            
-            if (!pq.isEmpty()) {
+            if(!pq.isEmpty()){
                 int[] now = pq.poll();
-                time += now[1];              
-                answer += time - now[0];        
+                time += now[1];
+                answer += time - now[0];
                 count++;
-            } else {
+            }
+            else{
                 time = jobs[idx][0];
             }
         }
-        
         return answer / len;
     }
 }
