30df1752bff18e9353d3009995c59c3b77c4026e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-02 16:24:36 +0900

    [level 2] Title: 삼각 달팽이, Time: 46.61 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,21 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        
+        List<Integer> answer = new ArrayList<>();
+        int[][] d = {{1,0}, {0,1}, {-1,-1}};
         int[][] result = new int[n][n];
-        int[][] d = new int[][]{{1,0}, {0,1}, {-1, -1}};
-        // 이게 뭐람
-        // 2차원 배열을 만들어서 방향 바꾸면서 만들기?
-        // 처음 n 만큼씩 이동하고 결국 n-1, n-2 이렇게만 움직이면됨
-        // n번 반복하는 동안
         int x = -1;
         int y = 0;
         int dir = 0;
         int num = 1;
+        
         for(int i = n; i > 0; i--){
             int count = i;
-            while (count > 0){
+            
+            while(count > 0){
                 int nx = x + d[dir % 3][0];
                 int ny = y + d[dir % 3][1];
                 
@@ -25,13 +23,16 @@ class Solution {
                 x = nx;
                 y = ny;
             }
-            dir += 1;
+            dir ++;
         }
+        
         for(int[] res : result){
             for(int r : res){
                 if(r != 0) {answer.add(r);}
             } 
         }
+        
+        
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
