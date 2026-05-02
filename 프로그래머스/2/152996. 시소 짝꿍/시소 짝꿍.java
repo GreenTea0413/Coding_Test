0986f5e81f8ad01e8a4e12417593804d00551dfe
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-02 16:07:30 +0900

    [level 2] Title: 시소 짝꿍, Time: 102.38 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -5,21 +5,18 @@ class Solution {
         long answer = 0;
         Map<Double, Long> map = new HashMap<>();
         
-        double[][] ratios = {{1,1}, {2,3}, {3,2}, {1,2}, {2,1}, {3,4}, {4,3}};
+        double[][] ratio = {{1,1}, {2,3}, {1,2}, {3,2}, {3,4}, {2,1}, {4,3}};
         
         for(int w : weights){
-            for(double [] r : ratios){
-                double key = w * r[0] / r[1];
-                if(map.containsKey(key)){
-                    answer += map.get(key);
-                }
+            // 그 다음 w를 또 꺼내서 비율 만큼 곱해서 값이 있는지 확인 있으면 answer에 그 만큼 더해주기
+            for(double[] r : ratio){
+                double temp = (double) w * (r[0] / r[1]);
+                if(map.containsKey(temp)) {answer += map.get(temp);}
             }
-            if(map.containsKey((double) w)){
-                map.put((double) w, map.get((double) w) + 1);
-            }
-            else{
-                map.put((double) w, 1L);
-            }
+            // map에 있으면 있는 값에다가 + 1
+            // 없으면 새로 put 1개 있다고 해주기
+            if(map.containsKey((double) w)){ map.put((double) w, map.get((double) w) + 1);}
+            else{map.put((double) w, 1L);}
         }
         
         return answer;
