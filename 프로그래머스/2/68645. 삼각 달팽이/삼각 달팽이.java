bfb8226b507c309339eb0759682c33ec517d6290
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-03 10:37:54 +0900

    [level 2] Title: 삼각 달팽이, Time: 35.79 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -4,35 +4,27 @@ class Solution {
     public int[] solution(int n) {
         List<Integer> answer = new ArrayList<>();
         int[][] d = {{1,0}, {0,1}, {-1,-1}};
-        int[][] result = new int[n][n];
+        int[][] maps = new int[n][n];
         int x = -1;
         int y = 0;
-        int dir = 0;
         int num = 1;
+        int dir = 0;
         
         for(int i = n; i > 0; i--){
-            int count = i;
-            
-            while(count > 0){
-                int nx = x + d[dir % 3][0];
-                int ny = y + d[dir % 3][1];
-                
-                result[nx][ny] = num;
-                num ++;
-                count--;
-                x = nx;
-                y = ny;
+            for(int j = 0; j < i; j++){
+                x += d[dir][0];
+                y += d[dir][1];
+                maps[x][y] = num++;
             }
-            dir ++;
-        }
-        
-        for(int[] res : result){
-            for(int r : res){
-                if(r != 0) {answer.add(r);}
-            } 
+            dir = (dir + 1) % 3;
         }
         
         
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                answer.add(maps[i][j]);
+            }
+        }
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
