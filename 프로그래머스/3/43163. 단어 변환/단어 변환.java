fff74638b60688332b76db2e5abd74aab6ca61bd
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-13 22:05:19 +0900

    [level 3] Title: 단어 변환, Time: 0.07 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,37 +1,37 @@
 import java.util.*;
+
 class Solution {
+    int answer = Integer.MAX_VALUE;
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        boolean[] v = new boolean[words.length];
-        Queue<String> q = new LinkedList<>();
-        q.offer(begin);
+        dfs(begin, target, words, new boolean[words.length], 0);
         
-        while(!q.isEmpty()){
-            int size = q.size();
-            
-            for(int i = 0; i < size; i++){
-                String cur = q.poll();
-                
-                if(cur.equals(target)) return answer;
-                
-                for(int j = 0; j < words.length; j++){
-                    if(!v[j] && isDiff(cur, words[j]) == 1){
-                        v[j] = true;
-                        q.offer(words[j]);
-                    }
-                }
-            }
-            answer ++;
-        }
-        return 0;
+        if(answer == Integer.MAX_VALUE) return 0;
+        return answer;
     }
     
-    public int isDiff(String s1, String s2){
-        int gap = 0;
-        for(int i = 0; i < s1.length(); i++){
-            if(s1.charAt(i) != s2.charAt(i)) gap++;
+    public void dfs(String begin, String target, String[] words, boolean[] v, int depth){
+        if(begin.equals(target)){
+            answer = Math.min(answer, depth);
+            return;
         }
         
-        return gap;
+        for(int i = 0; i < words.length; i++){
+            if(!v[i] && check(begin, words[i])){
+                v[i] = true;
+                dfs(words[i], target, words, v, depth + 1);
+                v[i] = false;
+            }
+        }
     }
+    
+    public boolean check(String s1, String s2){
+        int count = 0;
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                count++;
+            }
+            if (count >= 2) return false;
+        }
+        return true;
+    } 
 }
