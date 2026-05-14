85077a79dda686ddde05df30f961fc6a112cbf18
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-14 15:03:57 +0900

    [level 3] Title: 여행경로, Time: 1.45 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,15 +1,15 @@
 import java.util.*;
+
 class Solution {
     String[] answer;
-    int len;
+    int count;
     boolean found = false;
-
     public String[] solution(String[][] tickets) {
-        len = tickets.length;
+        count = tickets.length;
+        answer = new String[count + 1];
         
-        // ICN이 가는 idx 저장해놓기
         Map<String, List<Integer>> map = new HashMap<>();
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < tickets.length; i++){
             String start = tickets[i][0];
             map.putIfAbsent(start, new ArrayList<>());
             map.get(start).add(i);
@@ -19,28 +19,33 @@ class Solution {
             Collections.sort(list, (a, b) -> tickets[a][1].compareTo(tickets[b][1]));
         }
         
-        String[] path = new String[len + 1];
+        String[] path = new String[count + 1];
+        boolean[] v = new boolean[count + 1];
         path[0] = "ICN";
-        dfs(tickets, map, new boolean[len], "ICN", 0, path);
+ 
+        
+        dfs(map, tickets, path, v, 0, "ICN");
         
         return answer;
     }
     
-    public void dfs(String[][] tickets, Map<String, List<Integer>> map, boolean[] v, String now, int depth, String[] path){
-        if(depth == len){
-            answer = path.clone();
+    public void dfs(Map<String, List<Integer>> map, String[][] tickets, String[] path, boolean[] v, int depth, String now){
+        if(count == depth){
             found = true;
+            answer = path.clone();
             return;
         }
         
         if(!map.containsKey(now)) return;
         
-        for(int idx : map.get(now)){
-            if(!v[idx] && !found){
-                v[idx] = true;
-                path[depth + 1] = tickets[idx][1];
-                dfs(tickets, map, v, tickets[idx][1], depth + 1, path);
-                v[idx] = false;
+        for(int next : map.get(now)){
+            if(!v[next] && !found){
+                v[next] = true;
+                path[depth + 1] = tickets[next][1];
+                
+                dfs(map, tickets, path, v, depth + 1, tickets[next][1]);
+                
+                v[next] = false;
             }
         }
     }
