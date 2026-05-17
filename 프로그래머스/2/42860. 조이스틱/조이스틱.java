6a1c6aae293079a868a4bb35b297bf71a317c4e3
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-17 20:53:22 +0900

    [level 2] Title: 조이스틱, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,38 +1,26 @@
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int len = name.length();
-
-        // 이거는 위아래 방향만 더하기
-        for(int i = 0; i < len; i++){
-            // A에서 Z갔다가 돌아서 가는거
-            int z_gap = 'Z' - name.charAt(i) + 1;
-            // A에서 출발 했을 때
-            int a_gap = name.charAt(i) - 'A';
+        
+        // 글자 위아래로 움직이기
+        for(char c : name.toCharArray()){
+            int zGap = 'Z' - c + 1;
+            int aGap = c - 'A';
             
-            if(z_gap >= a_gap){
-                answer += a_gap;
-            }
-            else{
-                answer += z_gap;
-            }
+            if(zGap >= aGap){answer += aGap;}
+            else{answer+= zGap;}
         }
         
-        // ZAAAAJAA
-        // 01234567
-        // 최대로 움직이게 되는 move
+        int len = name.length();
         int move = len - 1;
-        for (int i = 0; i < len; i++){
+        for(int i = 0; i < len; i++){
             int next = i + 1;
-            // 여기서 i = 0 일 때 next는 J 위치임 len - 1까지 가게됨
-            while (next < len && name.charAt(next) == 'A') {
-                next++;
+            while(next < len && name.charAt(next) == 'A'){
+                next ++;
             }
             
-            // next = 8
-            move = Math.min(move, (len - next) + i * 2);
+            move = Math.min(move, i * 2 + (len - next));
             move = Math.min(move, i + (len - next) * 2);
-            
         }
         
         return answer + move;
