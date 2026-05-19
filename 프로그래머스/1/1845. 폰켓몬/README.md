18221145fb7d330ee335a985ddf5f14d465b369a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-19 10:35:08 +0900

    [level 1] Title: 폰켓몬, Time: 3.85 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 97.8 MB, 시간: 4.99 ms
+메모리: 66.7 MB, 시간: 3.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 17일 22:30:11
+2026년 05월 19일 10:35:06
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,17 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int count = nums.length;
-        HashMap<Integer, Integer> hash = new HashMap<>();
-        for(int i : nums){
-            hash.put(i, hash.getOrDefault(i, 0) + 1);
+        int n = nums.length / 2;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
         
-        int s = hash.size();
-        if (count / 2 < s){
-            answer = count / 2;
-        }else {
-            answer = s;
-        }
-        
-        
-        return answer;
+        if(map.size() > n){return n;}
+        else {return map.size();}
     }
 }
