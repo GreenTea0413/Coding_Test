67a11e6eddefe6116f22c890826e4a72459abf3c
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-20 12:22:22 +0900

    [level 2] Title: 프로세스, Time: 1.23 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,36 +1,36 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        
-        // priorities 0부터 시작하는 idx ,우선순위 적혀있는 value
+        int len = priorities.length;
         Queue<int[]> q = new LinkedList<>();
-        for(int i =0; i < priorities.length; i++){
+        for(int i = 0; i < len; i++){
             q.offer(new int[]{i, priorities[i]});
-        } 
-    
+        }
+        
+        
         while(!q.isEmpty()){
             int[] next = q.poll();
-            int time = next[1];
+            int p = next[1];
             
-            boolean isHigher = false;
-            for(int[] arr : q){
-                if(arr[1] > next[1]){
-                    isHigher = true;
+            boolean higher = false;
+            for(int[] a : q){
+                if(a[1] > p){
+                    higher = true;
                     break;
                 }
             }
             
-            if(isHigher) q.offer(next);
+            if(higher) q.offer(next);
             else{
                 answer ++;
                 if(next[0] == location){
                     return answer;
                 }
             }
+            
         }
-        
-        
         return answer;
     }
 }
