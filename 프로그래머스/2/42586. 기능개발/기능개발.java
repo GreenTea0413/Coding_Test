adcd44295baef427c2233dff4a6c2723fbf731dd
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-20 12:03:02 +0900

    [level 2] Title: 기능개발, Time: 3.56 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,26 +1,34 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         int len = speeds.length;
-        // 100 - 진행률 / 속도
-        // 7, 3, 9
-        // 그러면 첫번째 진행하는 동안 숫자가 이거보다 작으면 계속해서 빼서 없애기
         Queue<Integer> q = new LinkedList<>();
+        
         for(int i = 0; i < len; i++){
-            int time = (100 - progresses[i]) / speeds[i];
-            if((100 - progresses[i]) % speeds[i] > 0) time += 1;
-            q.offer(time);
+            int p = progresses[i];
+            int s = speeds[i];
+            
+            int temp = 100 - p;
+            int rest = temp / s;
+            if(temp % s > 0){rest += 1;}
+            q.offer(rest);
         }
+        
+        // [7, 4, 9]
         while(!q.isEmpty()){
-            int next = q.poll();
             int count = 1;
-            while(!q.isEmpty() && next >= q.peek()){
+            int now = q.poll();
+            
+            while(!q.isEmpty() && q.peek() <= now){
+                count++;
                 q.poll();
-                count += 1;
             }
             answer.add(count);
         }
-        return answer.stream().mapToInt(i->i).toArray();
+        
+        
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
