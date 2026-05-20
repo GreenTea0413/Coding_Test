b0eedc6682015e18092e3dd8240f631beeea612d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-20 11:34:10 +0900

    [level 3] Title: 베스트앨범, Time: 4.48 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,11 +3,10 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int len = genres.length;
-        
-        // total
+        // 전체
+        // int[] -> 고유번호, 플레이 횟수
         Map<String, Integer> total = new HashMap<>();
-        // 장르, [고유번호, 횟수]
-        Map<String , List<int[]>> play = new HashMap<>();
+        Map<String, List<int[]>> play = new HashMap<>();
         
         for(int i = 0; i < len; i++){
             String g = genres[i];
@@ -19,14 +18,13 @@ class Solution {
             play.get(g).add(new int[]{i, p});
         }
         
-        // 키들을 list로 keySet을 통해 가져오고 높은 순서대로 정렬
         List<String> genKey = new ArrayList<>(total.keySet());
-        genKey.sort((a,b) -> {return total.get(b) - total.get(a);});
+        genKey.sort((a,b) -> total.get(b) - total.get(a));
         
         List<Integer> answer = new ArrayList<>();
         
-        for(String key : genKey){
-            List<int[]> list = play.get(key);
+        for(String gen : genKey){
+            List<int[]> list = play.get(gen);
             list.sort((a,b) -> {
                 if(a[1] == b[1]) return a[0] - b[0];
                 return b[1] - a[1];
@@ -37,10 +35,12 @@ class Solution {
                 answer.add(l[0]);
                 count++;
                 
-                if(count == 2) break;
+                if(count == 2){
+                    break;
+                }
             }
         }
         
-        return answer.stream().mapToInt(i->i).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
