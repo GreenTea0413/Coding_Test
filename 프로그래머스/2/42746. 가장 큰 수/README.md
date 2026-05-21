c09b625e418917885ec6f71f125d447e4b7c38cc
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-21 11:47:29 +0900

    [level 2] Title: 가장 큰 수, Time: 109.66 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 137 MB, 시간: 183.03 ms
+메모리: 104 MB, 시간: 109.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 25일 12:02:43
+2026년 05월 21일 11:47:26
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,21 +1,22 @@
 import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
-        // int[] → String[]
-        String[] strs = new String[numbers.length];
-        for(int i = 0; i < numbers.length; i++){
-            strs[i] = String.valueOf(numbers[i]);
+        int len = numbers.length;
+        
+        String[] str = new String[len];
+        for(int i = 0; i < len; i++){
+            str[i] = String.valueOf(numbers[i]);
         }
-
-        // 핵심: a+b vs b+a 비교로 정렬
-        Arrays.sort(strs, (a, b) -> (b + a).compareTo(a + b));
-
-        // 전부 0인 경우 예외처리 ex) [0, 0, 0]
-        if(strs[0].equals("0")) return "0";
-
+        Arrays.sort(str, (a, b) -> (b + a).compareTo(a + b));
+        
         StringBuilder sb = new StringBuilder();
-        for(String s : strs) sb.append(s);
-
+        
+        for(int i = 0; i < len; i++){
+            sb.append(str[i]);
+        }
+        if(sb.charAt(0) == '0') return "0";
+        
         return sb.toString();
     }
 }
