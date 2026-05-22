74158faa43b840b85fe9969462183023101e5737
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-22 15:07:37 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int sum = brown + yellow;
+        int total = brown + yellow;
         
-        for(int h = 1; h < sum; h++){
-            if(sum % h == 0){
-                int w = sum / h;
-                
-                if(w >= h && (w-2) * (h-2) == yellow) {
-                    return new int[]{w, h};
+        for(int h = 1 ; h < total; h++){
+            if(total % h == 0){
+                int w = total / h;
+                if(w >= h && (h - 2) * (w - 2) == yellow){
+                    return new int[]{w,h};
                 }
             }
-            
         }
         return answer;
     }
