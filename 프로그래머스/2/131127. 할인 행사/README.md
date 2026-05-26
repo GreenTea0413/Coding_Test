7488af5460c244860a71a2af53f438fda2291281
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-26 21:48:53 +0900

    [level 2] Title: 할인 행사, Time: 35.91 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 126 MB, 시간: 59.03 ms
+메모리: 87.8 MB, 시간: 35.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 18일 17:21:46
+2026년 05월 26일 21:48:46
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,23 +1,29 @@
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        int len = discount.length;
         
-        for(int i = 0; i < discount.length - 9; i++){
+        for(int i = 0 ; i < len - 9; i++){
             int[] copy = number.clone();
+            //10일 연속으로 가야하니까 이렇게 끊고
             for(int j = i; j < i + 10; j++){
-                for(int l = 0; l < want.length; l++){
-                    if(want[l].equals(discount[j])) copy[l] -= 1;
+                for(int k = 0; k < want.length; k++){
+                    if(discount[j].equals(want[k])){
+                        copy[k] -= 1;
+                    }
                 }
-                
-                boolean check = true;
-                for(int k = 0; k < copy.length; k++){
-                    if (copy[k] < 0) {check = false; break;}
-                    if (copy[k] > 0) {check = false; break;}
-                }
-                
-                if(check) answer++;
             }
+            
+            boolean check = true;
+            
+            for(int j = 0; j < copy.length; j++){
+                if(copy[j] > 0){check = false; break;}
+                if(copy[j] < 0){check = false; break;}
+            }
+            
+            if(check) answer++;
         }
+        
         return answer;
     }
 }
