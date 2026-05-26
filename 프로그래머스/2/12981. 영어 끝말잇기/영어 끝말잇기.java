7d22fa56b615b646797e77dab0024226be146fed
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-26 21:11:09 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,32 +1,25 @@
 import java.util.*;
+
 class Solution {
+    Set<String> set = new HashSet<>();
+    
     public int[] solution(int n, String[] words) {
-        // 단어 기록
-        Map<String, Integer> map = new HashMap<>();
-        // 끝 단어 기록
-        String lastWord = String.valueOf(words[0].charAt(words[0].length() - 1));
-        map.put(words[0], 1);
-        int count = 1;
+        int[] answer = new int[2];
+        String last = words[0];
+        set.add(words[0]);
         
         for(int i = 1; i < words.length; i++){
-            // 다음 시작언어
-            String startWord = String.valueOf(words[i].charAt(0));
-            
-            // 같지 않음!!
-            if(!lastWord.equals(startWord)){return new int[]{count % n + 1, count / n + 1};}
-            // 같을 때
-            else if(lastWord.equals(startWord)){
-                // 맵에도 없으면 횟수 추가에 map에 넣기, 끝말 단어 바꿔주기
-                if(!map.containsKey(words[i])){
-                    count++;
-                    map.put(words[i], 1);
-                    lastWord = String.valueOf(words[i].charAt(words[i].length() - 1));
-                }
-                else{
-                    return new int[]{count % n + 1, count / n + 1};
-                }
+            String w = words[i];
+            if(set.contains(w) || (last.charAt(last.length() - 1) != w.charAt(0))){
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                
+                return answer;
             }
+            last = w;
+            set.add(w);
         }
-        return new int[]{0,0};
+        
+        return answer;
     }
 }
