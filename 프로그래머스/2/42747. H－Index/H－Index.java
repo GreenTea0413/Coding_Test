0d5ba9a1fcedaa466c44aa7096458e7e198b25f6
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-27 11:38:38 +0900

    [level 2] Title: H-Index, Time: 0.62 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,19 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int len = citations.length;
         Arrays.sort(citations);
+        int len = citations.length;
         
-        // 0 1 3 5 6
-        // i=0일 떄 0 이상 인용된 나머지 논문이 5개이상이고 이하인가? x
-        // i=1 1번 이상 인용된게 4개, 이하 2개
-        // i=2 3번 이상 인용된게 3개, 이하 3개
-        for(int i = 0 ; i < len; i++){
+        for(int i = 0; i < len; i++){
             int h = len - i;
-            if(citations[i] >= h){
+            if(h <= citations[i]){
                 return h;
             }
         }
+        
         return answer;
     }
 }
