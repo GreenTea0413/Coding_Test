9cad99920cd7476edb681568bf827375a81a3bad
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-28 14:23:43 +0900

    [level 2] Title: 피로도, Time: 1.94 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 83.5 MB, 시간: 3.35 ms
+메모리: 62.6 MB, 시간: 1.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 22일 13:54:24
+2026년 05월 28일 14:23:41
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,21 +1,38 @@
+import java.util.*;
+
 class Solution {
     int answer = 0;
-    int len;
+    int len = 0;
     public int solution(int k, int[][] dungeons) {
         len = dungeons.length;
-        boolean [] v = new boolean[len];
-        dfs(v, k, dungeons, 0);
+        // 최소 피로도 내림차순
+        // 최소 피로도가 같으면 소모 피로다가 작은 순서대로
+        Arrays.sort(dungeons, (a, b) -> {
+            if(a[0] == b[0]) return a[1] - b[1];
+            return b[0] - a[0];
+        });
+        // 시작을 0,1,2으로 선택 후 시작
+        for(int i = 0; i < len; i++){
+            if(k >= dungeons[i][0]){
+                boolean[] v = new boolean[len];
+                v[i] = true;
+                dfs(v, k - dungeons[i][1], dungeons, 1);    
+            }
+        }
+        
         return answer;
     }
     
     public void dfs(boolean[] v, int k, int[][] dungeons, int count){
-        answer = Math.max(count, answer);
+        answer = Math.max(answer, count);
         
         for(int i = 0; i < len; i++){
-            if(!v[i] && k >= dungeons[i][0] ){
-                v[i] = true;
-                dfs(v, k - dungeons[i][1], dungeons, count + 1);
-                v[i] = false;
+            if(!v[i]){
+                if(k >= dungeons[i][0]){
+                    v[i] = true;
+                    dfs(v, k - dungeons[i][1], dungeons, count + 1);
+                    v[i] = false;
+                }
             }
         }
     }
