d264e706af15a94871d9f54215ceb3384a7946b5
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-05-28 16:50:25 +0900

    [level 2] Title: [1차] 캐시, Time: 23.69 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,28 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        // 캐시 LRU 교체
-        // queue의 크기가 cacheSize보다 작으면 그냥 확인
-        // 이미 있다면 +1, 없으면 +5 후 queue에 넣기 
-            // 하지만 cacheSize를 넘어가려고 하면 queue에서 가장 앞에꺼 빼고 넣기
         if (cacheSize == 0) {return cities.length * 5;}
+        
         Queue<String> q = new LinkedList<>();
-        for(String c : cities){
-            String city = c.toUpperCase();
+        
+        for(String city : cities){
+            String temp = city.toLowerCase();
             
-            if (q.contains(city)){
-                answer += 1;
-                q.remove(city);
-                q.offer(city);
+            
+            // q안에 도시가 있다면?
+            if(q.contains(temp)){
+                answer+=1;
+                q.remove(temp);
+                q.offer(temp);
             }
             else{
                 answer += 5;
-                q.offer(city);
+                q.offer(temp);
                 if(q.size() > cacheSize){
                     q.poll();
                 }
             }
-        }       
+        }
+        
         
         return answer;
     }
