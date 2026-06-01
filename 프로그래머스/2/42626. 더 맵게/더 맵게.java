eb610b96af8f3087836b2076d0903def45fad9ca
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-01 23:21:02 +0900

    [level 2] Title: 더 맵게, Time: 647.02 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,15 +1,15 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
         for(int s : scoville){
-            pq.offer(s);
+            pq.add(s);
         }
         
         while(pq.peek() < K){
-            if (pq.size() < 2) return -1;
+            if(pq.size() < 2) return -1;
             int num1 = pq.poll();
             int num2 = pq.poll();
             
