4ac84191862171029280e31ff439ee97a928b24f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-01 23:44:39 +0900

    [level 2] Title: 주식가격, Time: 15.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,25 +4,22 @@ class Solution {
     public int[] solution(int[] prices) {
         int len = prices.length;
         int[] answer = new int[len];
-        Stack<Integer> q = new Stack<>();
-        // 값 : 1 2 3 2 3
-        // 인 : 0 1 2 3 4
-        // 이러면 1,2,3 들어가고 2 들어갈 때(i가 3일때) 3보다 작기때문에 한번 q.poll발생
-        // 3 - 2는 1인데..?
-        // answer = [0,0,1,0,0]이 되어버림
-        // 그리고 q는 이제 [0,1,3,4]가 되어버림
+        Stack<Integer> s = new Stack<>();
+        
+        // 1 2 3 -> 2 들어갈때 3 > 2 되니까 3을 비교해야하네
+        // [0,0,1,0,0];
+        // 0, 1, 3 -> 4 들어갈 때 가격 안떨어지니까 그대로 [0,1,3,4]
         for(int i = 0; i < len; i++){
-            int p = prices[i];
-            while(!q.isEmpty() && p < prices[q.peek()]){
-                int idx = q.pop();
-                answer[idx] = i - idx;  
+            while(!s.isEmpty() && prices[s.peek()] > prices[i]){
+                int idx = s.pop();
+                answer[idx] = i - idx;
             }
-            q.push(i);
+            s.push(i);
         }
         
-        while(!q.isEmpty()){
-            int n = q.pop();
-            answer[n] = len - n - 1;
+        while(!s.isEmpty()){
+            int idx = s.pop();
+            answer[idx] = len - idx - 1;
         }
         return answer;
     }
