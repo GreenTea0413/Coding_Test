f2056b164f8efec5756ac54cae9c4f78922ad8d9
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-02 12:24:59 +0900

    [level 2] Title: [3차] n진수 게임, Time: 270.79 ms, Memory: 364 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,33 +2,31 @@ class Solution {
     public String solution(int n, int t, int m, int p) {
         String answer = "";
         String number = "";
+        int len = t * m;
         
         int num = 0;
-        // m * t 글자 이상 만들기
-        while (number.length() < m * t) {
-            number += toN(num, n);
+        while(number.length() < len){
+            number += toNum(num, n);
             num++;
         }
         
-        // p번째 사람 위치: 인덱스 p-1부터 m 간격으로 t개
-        for (int i = p - 1; i < m * t; i += m) {
+        for(int i = p - 1; i < len; i += m){
             answer += number.charAt(i);
         }
         
-        return answer.toUpperCase();
+        return answer;
     }
     
-    public String toN(int num, int n) {
+    public String toNum(int num, int n){
         if (num == 0) return "0";
-
-        String res = "";
-        while (num > 0) {
-            int r = num % n;
-            // 둘 다 char나 String으로 통일
-            char c = r >= 10 ? (char)('A' + r - 10) : (char)('0' + r);
-            res = c + res;
+        
+        String str = "";
+        while(num > 0){
+            int rest = num % n;
+            char c = (rest >= 10) ? (char)('A' + rest - 10) : (char)('0' + rest);
+            str = c + str;
             num /= n;
         }
-        return res;
+        return str;
     }
 }
