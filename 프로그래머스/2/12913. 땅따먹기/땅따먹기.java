597c9f303853341bbd8808e606c80f7937b0d573
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-02 12:57:09 +0900

    [level 2] Title: 땅따먹기, Time: 13.17 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,26 +1,26 @@
 class Solution {
+    int answer = 0;
     int solution(int[][] land) {
-        int answer = 0;
         int len = land.length;
         int[][] dp = new int[len][4];
-        // 첫줄 이렇게 하고
+        
         for(int i = 0; i < 4; i++){
             dp[0][i] = land[0][i];
         }
         
-        for (int i = 1; i < len; i++){
-            // 본인 위치 제외하고 나머지 3개 더하기랑 그냥 dp[i][j]위치
+        for(int i = 1; i < len; i++){
             for(int j = 0; j < 4; j++){
-                dp[i][j] = Math.max(land[i][j] + Math.max(Math.max(dp[i-1][(j + 1)% 4], dp[i-1][(j + 2)% 4]), dp[i-1][(j + 3)% 4]), dp[i][j]);
+                dp[i][j] = Math.max(land[i][j] + 
+Math.max(
+    Math.max(dp[i-1][(j + 1)% 4], dp[i-1][(j + 2)% 4]), 
+            dp[i-1][(j + 3)% 4]), 
+            dp[i][j]);
             }
         }
-        
+    
         for(int i = 0; i < 4; i++){
             answer = Math.max(answer, dp[len-1][i]);
         }
-        
-        
-        
         return answer;
     }
 }
