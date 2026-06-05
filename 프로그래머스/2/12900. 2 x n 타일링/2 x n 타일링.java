9bb86574e7c8f2a110221c746674cd5533874143
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-05 13:14:55 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.62 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,16 +1,15 @@
 class Solution {
     public int solution(int n) {
+        int answer = 0;
         int[] dp = new int[n + 1];
-        int MOD = 1000000007;
+        if(n <= 2) return n;
+        dp[0] = 0;
         dp[1] = 1;
-        if(n >= 2){
-            dp[2] = 2;
-            for(int i = 3; i <= n; i++){
-                dp[i] = (dp[i-2] + dp[i-1]) % MOD;
-            }
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
         }
-        // 3 -> 111 1= =1 3가지
-        // 4 -> 1111 == 11= =11 1=1
+        
         return dp[n];
     }
 }
