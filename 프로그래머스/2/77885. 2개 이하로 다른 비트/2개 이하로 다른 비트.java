b99a46f9e9f694f698c232a3d7161f9db9960040
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-06 16:18:11 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 206.68 ms, Memory: 294 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -2,28 +2,28 @@ class Solution {
     public long[] solution(long[] numbers) {
         int len = numbers.length;
         long[] answer = new long[len];
-
-        for(int i = 0 ; i < len; i++){
-            long num = numbers[i];
+        
+        for(int i = 0; i < len; i++){
+            String st = "";
+            long n = numbers[i];
             
-            if(num % 2 == 0){
-                answer[i] = num + 1;
-            }
+            if(n % 2 == 0) answer[i] = n + 1;
             else{
-                String n = "";
-                while(num > 0){
-                    long rest = num % 2;
-                    n = rest + n;
-                    num /= 2;
+                while(n > 0){
+                    long rest = n % 2;
+                    st = String.valueOf(rest) + st;
+                    n /= 2;
                 }
-                n = "0" + n;
-                
-                int idx = n.lastIndexOf("01");
-                
-                String result = n.substring(0, idx) + "10" + n.substring(idx + 2);
-                answer[i] = Long.parseLong(result, 2);
+
+                st = "0" + st;
+
+                int idx = st.lastIndexOf("01");
+                String temp = st.substring(0, idx) + "10" + st.substring(idx + 2);
+                answer[i] = Long.parseLong(temp, 2);
             }
         }
+        
+        
         
         return answer;
     }
