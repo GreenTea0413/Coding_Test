36e5f6cb2cae7da35f0c8788ddde9f002bebbc15
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-06 16:30:21 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 84.61 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,14 +1,15 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+        long answer = 0;
         int len = queue1.length;
-        long total = 0;
         long sum1 = 0;
         long sum2 = 0;
-        long answer = 0;
-        Queue<Integer>q1 = new LinkedList<>();
-        Queue<Integer>q2 = new LinkedList<>();
+        long total = 0;
         
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
         for(int i = 0; i < len; i++){
             q1.offer(queue1[i]);
             q2.offer(queue2[i]);
@@ -17,29 +18,28 @@ class Solution {
             total += queue1[i] + queue2[i];
         }
         
-        if(total % 2 == 1){return -1;}
-        if(sum1 == sum2) {return 0;}
+        if(total % 2 != 0) return -1;
+        if(sum1 == sum2) return 0;
         
         while(answer < len * 2){
             while(!q1.isEmpty() && sum1 > sum2){
                 int n = q1.poll();
-                q2.offer(n);
                 sum1 -= n;
                 sum2 += n;
+                q2.offer(n);
                 answer++;
             }
-            while(!q2.isEmpty() && sum2 > sum1){
-                int m = q2.poll();
-                q1.offer(m);
-                sum1 += m;
-                sum2 -= m;
+            while(!q2.isEmpty() && sum1 < sum2){
+                int n = q2.poll();
+                sum1 += n;
+                sum2 -= n;
+                q1.offer(n);
                 answer++;
             }
-            if (sum1 == sum2){
-                return (int) answer;
-            }
+            
+            if(sum1 == sum2) {return (int) answer;}
+            
         }
-        
         return -1;
     }
 }
