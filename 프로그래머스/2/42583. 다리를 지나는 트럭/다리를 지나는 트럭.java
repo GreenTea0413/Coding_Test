8cefbed46476f1750f803834c7f1e8c8f536a5ee
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-07 22:48:06 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 14.98 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,32 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < bridge_length; i++){q.offer(0);}
+        int answer = 0;
+        
+        // 다리 완성
+        int cur_weight = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+        for(int i = 0; i < bridge_length; i++){bridge.offer(0);}
         
         int idx = 0;
-        int cur_weight = 0;
-        int answer = 0;
-        // 끝 시작
-        // 0  0 -> 0  7
-        // 0  7
-        // 7  0
-        // 1초마다 한칸씩 앞으로 가서 처리한다고 보면됨
         while(idx < truck_weights.length){
-            cur_weight -= q.poll();
+            cur_weight -= bridge.poll();
             int t = truck_weights[idx];
-            
+            // 트럭에서 제일 위에 있는 값 + 현재 다리 값 < weight보다 작으면 추가
             if(cur_weight + t <= weight){
                 cur_weight += t;
-                q.offer(t);
-                idx++;
+                bridge.offer(t);
+                idx ++;
             }
             else{
-                q.offer(0);
+                bridge.offer(0);
             }
             answer++;
         }
-        // 우리는 한칸씩 앞으로 가면서 값을 계산했고 이제 다 빠져나왔을 경우에 다리길이 더해주면 끝
+        
+        
+        
         return answer + bridge_length;
     }
 }
