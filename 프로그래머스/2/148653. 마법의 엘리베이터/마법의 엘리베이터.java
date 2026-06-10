e17533f7b44574cb590bc120436ac429c30fad61
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-10 12:16:57 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.05 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,34 +1,32 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-        int temp = storey;
-        int len = String.valueOf(storey).length();
         
-        while(temp > 0){
-            int rest = temp % 10;
-            // 6이상이면
-            if (10 - rest < rest) {
-                temp = temp / 10 + 1;
+        while(storey > 0){
+            int rest = storey % 10;
+            
+            if(rest > 5){
                 answer += 10 - rest;
+                storey = (storey / 10) + 1;
             }
-            // 4이하면
-            else if(10 - rest > rest){
-                temp = temp / 10;
+            else if(rest < 5){
                 answer += rest;
-            } 
-            // 5일떄는 앞에 숫자 봐가면서 해야함
-            else {
-                int next = (temp / 10) % 10;
-                if (next >= 5) {
-                    temp = temp / 10 + 1;
+                storey = (storey / 10);
+            }
+            // 5일때는 앞자리 보고 내리던지 올리던지 해야함
+            else{
+                int front = (storey / 10) % 10;
+                if(front >= 5){
                     answer += 5;
+                    storey = (storey / 10) + 1;
                 }
-                else {
-                    temp = temp / 10;
+                else{
                     answer += 5;
-                } 
+                    storey = storey / 10;
+                }
             }
         }
+         
         return answer;
     }
 }
