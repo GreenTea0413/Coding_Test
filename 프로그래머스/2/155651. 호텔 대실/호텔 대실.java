a9f6fe3d4a5f78ba5895f316b042a6d1f463efda
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-11 16:38:39 +0900

    [level 2] Title: 호텔 대실, Time: 7.01 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -3,26 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        // 끝나는 시간을 넣어서 앞에 시작시간들이랑 비교 하기
+        Arrays.sort(book_time, (a, b) -> {return a[0].compareTo(b[0]);});
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        Arrays.sort(book_time, (a, b) -> a[0].compareTo(b[0]));
         
         for(String[] book : book_time){
             int start = toMin(book[0]);
             int end = toMin(book[1]);
-            // 기존 방의 종료시간 보다 시작시간이 더 뒤에 있으면 방빼!
-            while(!pq.isEmpty() && pq.peek() + 10 <= start){
-                pq.poll();   
+            
+            while(!pq.isEmpty() && pq.peek() <= start){
+                pq.poll();
             }
             
-            pq.add(end);
-            answer = Math.max(pq.size(), answer);
-        }       
+            pq.offer(end + 10);
+            answer = Math.max(answer, pq.size());
+        }
+        
         return answer;
     }
     
-    public int toMin(String time){
-        String[] s = time.split(":");
-        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]) ;
+    public int toMin(String s){
+        String[] arr= s.split(":");
+        
+        return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
     }
 }
