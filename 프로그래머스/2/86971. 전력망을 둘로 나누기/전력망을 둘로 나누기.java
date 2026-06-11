7d1ad921181144ef37edeee15532d5e4eacc64f7
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-11 16:31:15 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.19 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,52 +2,55 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer = n;
+        int answer = Integer.MAX_VALUE;
         List<List<Integer>> graph = new ArrayList<>();
         
+        // 그래프 만들기
         for(int i = 0; i <= n; i++){graph.add(new ArrayList<>());}
-        for(int[] wire : wires){
-            graph.get(wire[0]).add(wire[1]);
-            graph.get(wire[1]).add(wire[0]);
+        for(int[] w : wires){
+            graph.get(w[0]).add(w[1]);
+            graph.get(w[1]).add(w[0]);
         }
         
-        // 이제 wire를 하나씩 자르면서 비교
-        for(int[] wire : wires){
-            int a = wire[0];
-            int b = wire[1];
+        // 그래프 선 하나씩 끊기
+        for(int[] w : wires){
+            int a = w[0];
+            int b = w[1];
             
             graph.get(a).remove(Integer.valueOf(b));
             graph.get(b).remove(Integer.valueOf(a));
             
-            int count1 = bfs(a, graph, new boolean[n + 1]);
-            int count2 = bfs(b, graph, new boolean[n + 1]);
+            int countA = bfs(graph, a, new boolean[n + 1]);
+            int countB = bfs(graph, b, new boolean[n + 1]);
+            int gap = Math.abs(countA - countB);
             
             graph.get(a).add(b);
             graph.get(b).add(a);
-            
-            int gap = Math.abs(count2 - count1); 
-            answer = Math.min(gap, answer);
+               
+            answer = Math.min(answer, gap);
         }
+        
         return answer;
     }
     
-    public int bfs(int start, List<List<Integer>> graph, boolean[] v){
-        int count = 0;
+    public int bfs(List<List<Integer>> graph, int start, boolean[] v){
         Queue<Integer> q = new LinkedList<>();
-        v[start] = true;
         q.offer(start);
+        v[start] = true;
+        int count = 1;
         
         while(!q.isEmpty()){
             int now = q.poll();
             
             for(int next : graph.get(now)){
                 if(!v[next]){
-                    q.offer(next);
                     v[next] = true;
                     count++;
+                    q.offer(next);
                 }
             }
         }
+        
         return count;
     }
 }
