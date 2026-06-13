0c1a06968101f5ac910fe08b7c0c5e60cd3e0aa7
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-13 11:55:45 +0900

    [level 3] Title: 베스트앨범, Time: 4.67 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,42 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int len = genres.length;
-        // 전체
-        // int[] -> 고유번호, 플레이 횟수
         Map<String, Integer> total = new HashMap<>();
+        // [고유번호, 횟수]
         Map<String, List<int[]>> play = new HashMap<>();
+        int len = plays.length;
         
         for(int i = 0; i < len; i++){
             String g = genres[i];
             int p = plays[i];
             
             total.put(g, total.getOrDefault(g, 0) + p);
-            
             play.putIfAbsent(g, new ArrayList<>());
             play.get(g).add(new int[]{i, p});
         }
         
-        List<String> genKey = new ArrayList<>(total.keySet());
-        genKey.sort((a,b) -> total.get(b) - total.get(a));
+        List<String> genre = new ArrayList<>(total.keySet());
+        genre.sort((a, b) -> {return total.get(b) - total.get(a);});
         
         List<Integer> answer = new ArrayList<>();
-        
-        for(String gen : genKey){
-            List<int[]> list = play.get(gen);
-            list.sort((a,b) -> {
-                if(a[1] == b[1]) return a[0] - b[0];
-                return b[1] - a[1];
-            });
-            
+        for(String g : genre){
             int count = 0;
-            for(int[] l : list){
-                answer.add(l[0]);
+            
+            List<int[]> list = new ArrayList<>(play.get(g));
+            list.sort((a, b) -> {return b[1] - a[1];});
+            
+            for(int[] arr : list){
+                answer.add(arr[0]);
                 count++;
-                
-                if(count == 2){
-                    break;
-                }
+                if(count == 2){break;}
             }
         }
         
