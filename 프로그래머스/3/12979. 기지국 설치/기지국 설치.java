a8978054a0d96b87c147956129f51ff0c774ad36
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-13 11:23:12 +0900

    [level 3] Title: 기지국 설치, Time: 0.36 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,29 +1,20 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        // 범위를 구해서 이 범위 안에 기지국을 몇개 설치할지를 정해야함
-        // 기지국 포함 좌우 w
-        int range = w * 2 + 1;
         int start = 1;
-        
-        for(int station : stations){
-            int left = station - w;
+        int range = w * 2 + 1;
+        for(int s : stations){
+            int left = s - w;
             
-            // 기지국 left 기준으로 start보다 크다면 그 앞에는 기지국이 없다는 뜻
-            if (start < left){
-                int gap = left - start;
-                answer += (gap + range - 1) / range;
+            if(start < left){
+                answer += (left - start + range - 1) / range;
             }
-            
-            start = station + w + 1;
+            start = s + w + 1;
         }
         
         if(start <= n){
-            int gap = n - start + 1;
-            answer += (gap + range - 1) / range;
+            answer += (n - start + range) / range;
         }
-        
-    
         return answer;
     }
 }
