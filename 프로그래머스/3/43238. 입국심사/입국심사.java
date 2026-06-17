93be786cce6b7d504a7d43dcad5fd179b70c398c
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-17 10:36:08 +0900

    [level 3] Title: 입국심사, Time: 17.15 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,7 +1,5 @@
-import java.util.*;
-
 class Solution {
-    public long solution(int n, int[] times) {        
+    public long solution(int n, int[] times) {
         long left = 0;
         long right = 0;
         for(int t : times){
@@ -12,19 +10,18 @@ class Solution {
         long answer = right;
         
         while(left <= right){
-            long mid = (left + right) / 2;
-        
-            long total = 0;
+            long mid = (left + right) /  2;
+            long total = 0; // 나눈 수 30 / 7 -> 4 + 30 / 10 -> 3 가져가는 느낌
             for(int t : times){
-                total += mid / t;        
+                total += mid / t;
             }
             
-            if (total >= n){
-                answer = mid;
-                right = mid - 1;
-            }
-            else {
+            if(total < n){
                 left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+                answer = mid;
             }
         }
         
