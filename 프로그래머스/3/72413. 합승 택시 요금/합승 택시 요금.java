acb153e5a596cc19f382a958cd33db2c14ef325e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-24 11:27:31 +0900

    [level 3] Title: 합승 택시 요금, Time: 17.83 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,47 +1,62 @@
 import java.util.*;
+
 class Solution {
+    static class Node{
+        int to;
+        int cost;
+        
+        Node(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+    
+    List<Node>[] graph;
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int[][] costs = new int[n+1][n+1];
-        for(int[] f : fares){
-            costs[f[0]][f[1]] = f[2];
-            costs[f[1]][f[0]] = f[2];
+        graph = new ArrayList[n + 1];
+        
+        for(int i = 0; i <= n; i++){graph[i] = new ArrayList<>();}
+        
+        for(int[] fare : fares){
+            graph[fare[0]].add(new Node(fare[1], fare[2]));
+            graph[fare[1]].add(new Node(fare[0], fare[2]));
         }
         
-        int[][] dist = new int[3][n+1];
-        for(int[] d : dist){
-            Arrays.fill(d, Integer.MAX_VALUE);
+        int[] distS = dijkstra(n, s);
+        int[] distA = dijkstra(n, a);
+        int[] distB = dijkstra(n, b);
+        
+        int answer = Integer.MAX_VALUE;
+        for (int k = 1; k <= n; k++) {
+            answer = Math.min(answer, distS[k] + distA[k] + distB[k]);
         }
         
-        Queue<int[]> pq = new PriorityQueue<>((e1,e2)-> e1[1] - e2[1]);
-        int[] start = {s,a,b};
+        return answer;
+    }
+    
+    public int[] dijkstra(int n, int start){
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
         
-        for(int i =0 ; i < 3; i++){
-            int[] d = dist[i];
-            pq.add(new int[]{start[i], 0});
-            d[start[i]] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {return a.cost - b.cost;});
+        dist[start] = 0;
+        pq.offer(new Node(start, 0));        
         
-            while(!pq.isEmpty()){
-                int[] cur = pq.remove();
+        while(!pq.isEmpty()){
+            Node now = pq.poll();
+            
+            if(now.cost > dist[now.to]){continue;}
+            
+            for(Node next : graph[now.to]){
+                int nextCost = now.cost + next.cost;
                 
-                for(int j = 1; j<=n; j++){
-                    if(costs[cur[0]][j] == 0) continue;
-                    if(d[j] > cur[1] + costs[cur[0]][j]){
-                        d[j] =cur[1] + costs[cur[0]][j];
-                        pq.add(new int[]{j, cur[1] + costs[cur[0]][j]});
-                    }
+                if(nextCost < dist[next.to]){
+                    dist[next.to] = nextCost;
+                    pq.offer(new Node(next.to, nextCost));
                 }
             }
         }
-        int min = Integer.MAX_VALUE;
-        for(int i = 1; i <= n; i++){
-            int sum = 0;
-            for(int[] d : dist){
-                sum += d[i];
-            }
-            min = Math.min(min, sum);
-        }
         
-        return min;
-        
+        return dist;
     }
 }
