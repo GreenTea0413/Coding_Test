f10376aec1b116e41c2f08724464e3acab403c97
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-06-24 11:36:50 +0900

    [level 2] Title: 배달, Time: 8.31 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,42 +1,50 @@
 import java.util.*;
 
 class Solution {
+    static class Node{
+        int to;
+        int cost;
+        
+        Node(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+    
+    List<Node>[] graph;
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
-
-        List<List<int[]>> graph = new ArrayList<>();
-        for(int i = 0; i <= N ; i++){
-            graph.add(new ArrayList<>());
-        }
+        graph= new ArrayList[N + 1];
         
+        for(int i = 0; i <= N; i++){graph[i]= new ArrayList<>();}
         for(int[] r : road){
-            graph.get(r[0]).add(new int[]{r[1], r[2]});
-            graph.get(r[1]).add(new int[]{r[0], r[2]});
+            graph[r[0]].add(new Node(r[1], r[2]));
+            graph[r[1]].add(new Node(r[0], r[2]));
         }
         
-        // 1번 마을에서 시작해서 배달이 가능한 마을의 개수
         int[] dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {return a.cost - b.cost;});
+        pq.offer(new Node(1, 0));
         dist[1] = 0;
         
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{1, 0});
-        
-        while(!q.isEmpty()){
-            // 현재 위치랑, 현재까지 걸어온 거리
-            int[] now = q.poll();
+        while(!pq.isEmpty()){
+            Node now = pq.poll();
             
-            for(int[] next : graph.get(now[0])){
-                // next[0]이 다음, next[1]이 거리
-                if(dist[next[0]] > now[1] + next[1] && dist[next[0]] != 0){
-                    dist[next[0]] = now[1] + next[1];
-                    q.offer(new int[]{next[0], now[1] + next[1]});
-                }                 
+            if(now.cost > dist[now.to]){continue;}
+            
+            for(Node next : graph[now.to]){
+                int nextCost = now.cost + next.cost;
+                
+                if(nextCost < dist[next.to]){
+                    dist[next.to] = nextCost;
+                    pq.offer(new Node(next.to, nextCost));
+                }
             }
+            
         }
-
-        for(int d : dist){
-            if(d <= K) answer++;
+        int answer = 0;
+        for(int i = 1; i <= N; i++){
+            if(dist[i] <= K){answer++;}
         }
         return answer;
     }
