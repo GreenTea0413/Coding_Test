118f82c44807c7c5d5273608af7ef2e330193ac2
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-03 13:22:55 +0900

    [level 3] Title: N으로 표현, Time: 17.30 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,39 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if (N == number){
-            return 1;
-        }
-        // dp[1] = 5
-        // dp[2] = 5 + 5, 5 * 5, 5 - 5, 5 / 5, 55
-        // dp[3] = 5 + 55, 5 * 55, 5 / 55
-        // dp[3] = dp[1] + dp[2] or dp[2] + dp[1]
-        // dp[i] = dp[i-j] + dp[j] 
-        List<Set<Integer>> dp = new ArrayList<>();
+        if(N == number){return 1;}
         
-        for(int i = 0; i <= 8; i++){
-            dp.add(new HashSet<>());
-        }
+        List<Set<Integer>> set = new ArrayList<>();
+        for(int i = 0; i <= 8; i++){set.add(new HashSet<>());}
         
         for(int i = 1; i <= 8; i++){
             int num = Integer.parseInt(String.valueOf(N).repeat(i));
-            dp.get(i).add(num);
+            set.get(i).add(num);
             
-            for (int j = 1; j < i; j++){
-                for(int n1 : dp.get(j)){
-                    for (int n2 : dp.get(i-j)){
-                        dp.get(i).add(n1 + n2);
-                        dp.get(i).add(n1 - n2);
-                        dp.get(i).add(n1 * n2);
-                        if (n2 != 0){
-                            dp.get(i).add(n1 / n2);
-                        }
+            for(int j = 1; j < i; j++){
+                for(int n1 : set.get(j)){
+                    for(int n2 : set.get(i - j)){
+                        set.get(i).add(n1 + n2);
+                        set.get(i).add(n1 - n2);
+                        set.get(i).add(n1 * n2);
+                        if(n2 != 0) set.get(i).add(n1 / n2);
                     }
                 }
             }
-            if (dp.get(i).contains(number))
-                return i;
+            if(set.get(i).contains(number)) return i;
         }
+        
+        
+        
+        
         return -1;
     }
 }
