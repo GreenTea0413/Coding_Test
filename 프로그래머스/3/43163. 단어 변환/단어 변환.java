d6428ac996a44fe1829964dc648987fd65b1bd24
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-06 15:00:15 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,41 +1,32 @@
 import java.util.*;
 
 class Solution {
-    int answer = Integer.MAX_VALUE;
+    int answer = 0;
     public int solution(String begin, String target, String[] words) {
-        dfs(begin, target, words, 0, new boolean[words.length]);
-        
-        if(answer == Integer.MAX_VALUE) return 0;
+        dfs(begin, target, words, new boolean[words.length], 0);
         return answer;
     }
     
-    public void dfs(String begin, String target, String[] words, int depth, boolean[] v){
-        if(depth > answer) return;
-        
-        if(begin.equals(target)){
-            answer = Math.min(answer, depth);
-        }
+    public void dfs(String cur, String target, String[] words, boolean[] v, int count){
+        if(cur.equals(target)){
+            answer = count;
+            return;
+        }       
         
         for(int i = 0; i < words.length; i++){
-            if(!v[i]){
-                if(isCheck(begin, words[i])){
-                    v[i] = true;
-                    dfs(words[i], target, words, depth + 1, v);
-                    v[i] = false;
-                }
+            if(!v[i] && check(cur, words[i])){
+                v[i] = true;
+                dfs(words[i], target, words, v, count + 1);
+                v[i] = false;
             }
         }
     }
-    
-    public boolean isCheck(String a, String b){
-        int gap = 0;
-        int len = a.length();
-        for(int i = 0; i < len; i++){
-            if(a.charAt(i) != b.charAt(i)) gap++;
-            if(gap > 1) return false;
+    public boolean check(String a, String b){
+        int count = 0;
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)) count++;
+            if(count > 1) return false;
         }
-        
         return true;
-        
     }
 }
