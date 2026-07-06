dcdcc809891cf4053527fb0b6b67188f47aa53fd
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-06 14:03:07 +0900

    [level 3] Title: 야근 지수, Time: 148.37 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,22 +3,20 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        long total = 0;
-        int len = works.length;
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {return b - a;});
-        for(int w : works){
-            total += w; 
+        int total = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+        for(int w : works) {
             pq.offer(w);
+            total += w;
         }
         
-        if(total < n) return 0;
+        if(n >= total) {return 0;}
         
         while(n > 0){
             int num = pq.poll();
-            num -= 1;
+            num--;
+            n--;
             pq.offer(num);
-            n --;
         }
         
         for(int p : pq){
