4a40a84f5a84cc28bf294a196b1af2dcf03fac99
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-06 13:31:00 +0900

    [level 3] Title: 네트워크, Time: 4.68 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,34 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < n; i++){graph.add(new ArrayList<>());}
-        
+        List<Integer>[] graph = new ArrayList[n];
+        for(int i = 0; i < n; i++){
+            graph[i] = new ArrayList<>();
+        }
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(i != j && computers[i][j] == 1){
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
-                } 
+                    graph[i].add(j);
+                    graph[j].add(i);
+                }
             }
         }
         
         boolean[] v = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
-        
         for(int i = 0; i < n; i++){
             if(!v[i]){
                 answer++;
+                v[i] = true;
+                Queue<Integer> q = new LinkedList<>();
                 q.offer(i);
+
                 while(!q.isEmpty()){
                     int now = q.poll();
                     v[now] = true;
-                    
-                    for(int next : graph.get(now)){
-                        if(!v[next]){
-                            v[next] = true;
-                            q.offer(next);
-                        }
+
+                    for(int next : graph[now]){
+                        if(!v[next]){q.offer(next);}
                     }
                 }
             }
