a264b6866a149159fd709bd671456b8d68d09794
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-07 11:43:44 +0900

    [level 3] Title: 최고의 집합, Time: 0.19 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[] solution(int n, int s) {
         if(n > s) return new int[]{-1};
+        
         int[] answer = new int[n];
-        
-        int div = s / n;
-        int rest = s % n;
-        
+        // 곱이 최대로 되기 위해서는 여기서 s / n 만큼 하고 거기서 부족한 만큼 더해주면 끝?
+        int mid = s / n;
+        int total = mid * n;
+        int gap = s - total;
         for(int i = 0; i < n; i++){
-            answer[i] = div;
+            answer[i] = mid;
         }
         
-        for(int i = n - 1; i >= n - rest; i--){
-            answer[i] += 1;
+        if(gap > 0){
+            int last = n - 1;
+            for(int i = 0; i < gap; i++){
+                answer[last] += 1;
+                last--;
+            }
         }
         return answer;
     }
