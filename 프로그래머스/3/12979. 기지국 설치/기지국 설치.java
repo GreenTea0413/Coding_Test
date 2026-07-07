b951efb7765bf800f1a33bb8323569f24bf26c36
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-07 11:18:12 +0900

    [level 3] Title: 기지국 설치, Time: 1.66 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,20 +1,30 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int start = 1;
+        // stations을 기준으로 1부터 시작했을 때 그 갭 차이 만큼 들어갈 수 있는지 확인
+        // 4 -> 3, 5 1부터 시작하면 gap == 2 1개만 들어가면 됨
+        // 11 -> 10, 12여서 아까 구헌거 idx == 6으로 시작해서 10 - 6 4칸인데 w 생각하면 2개 가능
+        // 여기서 1개를 그냥 1칸말고 w * 2 + 1을 범위로 지정
+        // 범위보다 -1 한거 만큼 더해주고 범위 만큼 나누면 +1한 것 보다 더 정교하게 구간 나누기 가능
+        int idx = 1;
         int range = w * 2 + 1;
         for(int s : stations){
-            int left = s - w;
+            int start = s - w;
+            int end = s + w;
             
-            if(start < left){
-                answer += (left - start + range - 1) / range;
-            }
-            start = s + w + 1;
+            int gap = start - idx;
+            int count = (gap + range - 1) / range;
+            
+            answer += count;
+            idx = end + 1;
         }
         
-        if(start <= n){
-            answer += (n - start + range) / range;
+        if(idx <= n){
+            int gap = n - idx;
+            int count = (gap + range) / range;
+            answer += count;
         }
+
         return answer;
     }
 }
