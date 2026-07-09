8ec77b95c195e86b850c026a335f8212a8c461b0
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-09 12:09:32 +0900

    [level 3] Title: 불량 사용자, Time: 33.72 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,46 +1,45 @@
 import java.util.*;
 
 class Solution {
-    Set<Set<String>> ban = new HashSet<>();
+    Set<Set<String>> answer = new HashSet<>();
+    int len = 0;
     public int solution(String[] user_id, String[] banned_id) {
-        int len = user_id.length;
+        len = user_id.length;
         boolean[] v = new boolean[len];
         
         dfs(v, user_id, banned_id, 0, new HashSet<>());
-        return ban.size();
+        
+        return answer.size();
     }
     
     public void dfs(boolean[] v, String[] user_id, String[] banned_id, int depth, Set<String> cur){
-        if(depth == banned_id.length) {
-            ban.add(new HashSet<>(cur));
+        if(depth == banned_id.length){
+            answer.add(new HashSet<>(cur));
             return;
         }
         
-        
-        for(int j = 0; j < user_id.length; j++){
-            if(!v[j]){
-                if(isCheck(user_id[j], banned_id[depth])){
-                    v[j] = true;    
-                    cur.add(user_id[j]);
+        for(int i = 0; i < len; i++){
+            if(!v[i]){
+                if(check(user_id[i], banned_id[depth])){
+                    v[i] = true;
+                    cur.add(user_id[i]);
+                    
                     dfs(v, user_id, banned_id, depth + 1, cur);
-
-                    v[j] = false;
-                    cur.remove(user_id[j]);
+                    
+                    cur.remove(user_id[i]);
+                    v[i] = false;
                 }
             }
         }
-        
     }
     
-    public boolean isCheck(String a, String b){
-        if(a.length() != b.length()) return false;
-        for(int i = 0; i < a.length(); i++){
-            if(b.charAt(i) == '*') continue;
-            if(a.charAt(i) != b.charAt(i)){
-                return false;
-            }
+    public boolean check(String user, String ban){
+        if(user.length() != ban.length()) return false;
+        for(int i = 0; i < ban.length(); i++){
+            if(ban.charAt(i) == '*'){continue;}
+            if(ban.charAt(i) != user.charAt(i)){return false;}
         }
-        
+    
         return true;
     }
 }
