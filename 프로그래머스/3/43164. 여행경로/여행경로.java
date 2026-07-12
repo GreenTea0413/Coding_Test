ba16b60bde61f50f1f94a52aea006ecd08b35c32
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-12 11:49:42 +0900

    [level 3] Title: 여행경로, Time: 1.23 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,34 +1,38 @@
 import java.util.*;
 
 class Solution {
-    String[] answer;
     int len;
+    String[] answer;
     boolean found = false;
+    
     public String[] solution(String[][] tickets) {
         len = tickets.length;
         answer = new String[len + 1];
-        
+        // 경로대 경로로 못 이으니까 경로가 가는 index들 저장하기
+        // INC -> 0
+        // HND -> 1
+        // JFK -> 2
         Map<String, List<Integer>> map = new HashMap<>();
-        for(int i = 0; i < tickets.length; i++){
+        for(int i = 0; i < len; i++){
             String start = tickets[i][0];
             map.putIfAbsent(start, new ArrayList<>());
             map.get(start).add(i);
         }
         
+        
+        // 그리고 이제 값들 불러온거에서 각각 출발지로부터 알파벳 순으로 정렬
         for(List<Integer> list : map.values()){
             Collections.sort(list, (a, b) -> tickets[a][1].compareTo(tickets[b][1]));
         }
         
         String[] path = new String[len + 1];
-        boolean[] v = new boolean[len + 1];
         path[0] = "ICN";
-
-        dfs(map, tickets, path, v, 0, "ICN");
-        
+        dfs(tickets, map, new boolean[len], "ICN", 0, path);
+          
         return answer;
     }
     
-    public void dfs(Map<String, List<Integer>> map, String[][] tickets, String[] path, boolean[] v, int depth, String now){
+    public void dfs(String[][] tickets, Map<String, List<Integer>> map, boolean[] v, String now, int depth, String[] path){
         if(depth == len){
             answer = path.clone();
             found = true;
@@ -42,7 +46,7 @@ class Solution {
                 v[next] = true;
                 path[depth + 1] = tickets[next][1];
                 
-                dfs(map, tickets, path, v, depth + 1, tickets[next][1]);
+                dfs(tickets, map, v, tickets[next][1], depth + 1, path);
                 
                 v[next] = false;
             }
