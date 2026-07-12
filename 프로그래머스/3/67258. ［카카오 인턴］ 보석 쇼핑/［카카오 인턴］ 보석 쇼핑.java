ed7ba343728054978a488f0fda249c97fa66cbef
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-12 10:54:00 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 30.21 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -2,28 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] gems) {
-        Set<String> set = new HashSet<>();
-        for(String g : gems){set.add(g);}
-        int count = set.size();
+        // 일단 보석 종류 갯수 구하기
+        Set<String> gem = new HashSet<>();
+        for(String g : gems){gem.add(g);}
+        int count = gem.size();
         if(count == 1) return new int[]{1,1};
         
-        int len = gems.length;
-        int[] answer = new int[]{0, len};
         int left = 0;
         int right = 0;
+        int len = gems.length;
+        int[] answer = new int[]{0, len};
+        
+        // 보석 갯수 하나씩 채워넣기
         Map<String, Integer> map = new HashMap<>();
         
         while(right < len){
-            String g = gems[right];
-            map.put(g, map.getOrDefault(g, 0) + 1);
+            String gr = gems[right];
+            
+            map.put(gr, map.getOrDefault(gr, 0) + 1);
             
             while(map.size() == count){
+                String gl = gems[left];
                 if(answer[1] - answer[0] > right - left){
                     answer[0] = left + 1;
                     answer[1] = right + 1;
                 }
-                map.put(gems[left], map.get(gems[left]) - 1);
-                if(map.get(gems[left]) == 0) map.remove(gems[left]);
+                map.put(gl, map.get(gl) - 1);
+                if(map.get(gl) == 0) map.remove(gl);
                 left++;
             }
             right++;
