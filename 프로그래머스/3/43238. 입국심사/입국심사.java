03848c1fc0223bb40c1572c1a6a2d6f1086b7d70
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-14 13:50:39 +0900

    [level 3] Title: 입국심사, Time: 20.17 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,25 @@
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
+        long max = 0;
+        for(int t : times){max = Math.max(t, max);}
+        
+        long right = n * max;
+        if(n == 1) return right;
+        
+        // 이런문제는 이분탐색으로 풀어야함
         long left = 0;
-        long right = 0;
-        for(int t : times){
-            right = Math.max(t, right);
-        }
-        right *= n;
-        
-        long answer = right;
-        
         while(left <= right){
-            long mid = (left + right) /  2;
-            long total = 0; // 나눈 수 30 / 7 -> 4 + 30 / 10 -> 3 가져가는 느낌
+            long mid = (left + right) / 2;
+            long rest = 0;
             for(int t : times){
-                total += mid / t;
+                rest += mid / t;
             }
-            
-            if(total < n){
-                left = mid + 1;
-            }
-            else{
-                right = mid - 1;
-                answer = mid;
-            }
+            // 처음에 mid가 30, 7과 10으로 나누면 3 + 3 6이 나옴
+            // 하지만 이것보다 더 줄일 수 있으니까
+            if(rest >= n){right = mid - 1;}
+            else{left = mid + 1; answer = mid + 1;}
         }
-        
         return answer;
     }
 }
