ed72d67ac1971a918a9e789b42c79d24b7000bae
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-15 14:26:34 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 21.95 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -5,36 +5,33 @@ class Solution {
         int n = enroll.length;
         int[] answer = new int[n];
         
-        Map<String, String> parentMap = new HashMap<>();
-        Map<String, Integer> indexMap = new HashMap<>();
-        // young -> edward
-        // tod -> jaimie
-        // jaimie -> mary 
-        // emily -> mary
-        // sam -> edward
-        // edward -> mary
-        // mary -> -
-        // jonh -> -
+        // 그래프 만들기
+        // r에 따라 e 넣어주기
+        // 그래프 반대로 만들어줘야지 타고올라가는거 처럼 만들기 가능
+        Map<String, String> pMap = new HashMap<>();
+        Map<String, Integer> iMap = new HashMap<>();
         for(int i = 0; i < n; i++){
-            String parent = referral[i];
-            String child = enroll[i];
+            String e = enroll[i];
+            String r = referral[i];
             
-            parentMap.put(child, parent);
-            indexMap.put(child, i);
+            pMap.put(e, r);
+            iMap.put(e, i);
         }
-        
+                
+        // 그 다음 seller를 하나씩 꺼내서 이름 비교해서 pMap에 다음 값 찾고 -이면 중단
+        // 그러면서 iMap에서는 idx를 꺼내서 answer에 바로 바로 값 더해주기
         for(int i = 0; i < seller.length; i++){
+            int total = amount[i] * 100;
             String now = seller[i];
-            int money = amount[i] * 100;
             
-            while(!now.equals("-") && money > 0){
-                int give = money / 10;
-                int keep = money - give;
+            while(!now.equals("-") && total > 0){
+                int rest = total / 10;
+                int num = total - rest;
                 
-                answer[indexMap.get(now)] += keep;
-                now = parentMap.get(now);
+                answer[iMap.get(now)] += num;
+                now = pMap.get(now);
                 
-                money = give;
+                total = rest;
             }
         }
         
