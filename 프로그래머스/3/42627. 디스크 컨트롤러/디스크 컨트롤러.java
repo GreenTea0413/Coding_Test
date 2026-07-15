abba44e0e590edb5b0f72f00fb0ae5c46fc686b2
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-15 14:57:44 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.55 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,24 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
+        // 이러면 일단 요청 시간이 빠른 순으로 넣기
+        Arrays.sort(jobs, (a, b) -> (a[0] - b[0]));
+        
+        // 우선순위 -> 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if(a[2] == b[2]){
+                if(a[1] == b[1]) return a[0] - b[0];
+                else return a[1] - b[1];
+            }
+            return a[2] - b[2];
+        });
+        
+        int time = 0;
         int answer = 0;
+        int len = jobs.length;
         int idx = 0;
         int count = 0;
-        int len = jobs.length;
-        int time = 0;
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {return a[1] - b[1];});
-        
+        // 작업의 번호, 요청시간, 소요시간
         while(count < len){
+            // 일단 바로 넣고 여기서 time이랑 현재 들어온 값의 요청시간 call과 time이 맞는지 비교
+            // 비교해서 바로 넣기
             while(idx < len && jobs[idx][0] <= time){
-                pq.offer(new int[]{jobs[idx][0], jobs[idx][1]});
+                pq.offer(new int[]{idx, jobs[idx][0], jobs[idx][1]});
                 idx++;
             }
             
+            // 그럼 현재 시간에 맞게 들어온 것들이 있을거임
+            // 0,0,3
+            // 2 3 5
+            // 1 1 9
             if(!pq.isEmpty()){
                 int[] now = pq.poll();
-                time += now[1];
-                answer += time - now[0];
+                time += now[2];
+                answer += time - now[1];
                 count++;
             }
             else{
