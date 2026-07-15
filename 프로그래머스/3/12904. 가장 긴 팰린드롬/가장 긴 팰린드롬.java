503cb8d69f27ae9c20e82e235ea251c0f1ad8723
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-15 15:09:43 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 18.03 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,22 +1,24 @@
 class Solution
 {
-    public int solution(String s)
-    {
+    public int solution(String s){
         int answer = 0;
-        
+        // 우선 짝수와 홀수의 경우가 있음
         for(int i = 0; i < s.length(); i++){
-            answer = Math.max(answer, palindrome(s, i, i));
-            answer = Math.max(answer, palindrome(s, i, i + 1));
+            answer = Math.max(answer, palindrome(s,i,i));
+            answer = Math.max(answer, palindrome(s,i,i + 1));
         }
+        
         return answer;
     }
     
-    public int palindrome(String s, int left, int right){
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            left --;
-            right ++;
+    // 이걸로 처음에 0, 1이 오게되면 left와 right를 늘려가면서 비교
+    // 사실상 i가 0이면 0, 1이 끝
+    // i가 1이면 0,1,2 또는 0, 1, 2, 3이끝
+    int palindrome(String s, int left, int right){
+        while(right < s.length() && left >= 0 && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
         }
-        
         return right - left - 1;
     }
 }
