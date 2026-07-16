32ea6653534a8de0a954665ba84b43d0227ff835
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-16 11:23:40 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 12.93 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -2,46 +2,49 @@ import java.util.*;
 
 class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
-        int people = timetable.length;
+        // 일단 버스보다 사람 적으면 바로 반환
+        int len = timetable.length;
         
-        int[] time = new int[people];
-        for(int i = 0; i < people; i++){time[i] = toNum(timetable[i]);}
-        
-        Arrays.sort(time);
+        String answer = "";
+        int[] arr = new int[len];
+        for(int i = 0; i < len; i++){
+            arr[i] = toMin(timetable[i]);
+        }
+        Arrays.sort(arr);
         
         int idx = 0;
         int last = -1;
-        
-        for(int bus = 0; bus < n; bus++){
-            int busTime = 540 + bus * t;
+        // 버스는 n개만 옴 순환 되는 구조가 아님
+        for(int i = 0; i < n; i++){
+            // 그러면 이게 이제 배차 버스 시간
+            // 처음에는 09:00이니까 540
+            int time = i * t + 540;
             int count = 0;
-            while(idx < people && count < m && time[idx] <= busTime){
-                last = time[idx];
-                idx ++;
+            while(idx < len && count < m &&  arr[idx] <= time){
+                last = arr[idx];
                 count++;
+                idx++;
             }
             
-            if(bus == n - 1){
-                if (count < m) {
-                    return toString(busTime);
-
-                }
-                return toString(last - 1);
+            if(i == n - 1){
+                if(count < m){return toStr(time);}
+                else{return toStr(last - 1);}
             }
         }
-
-        return "";
-    }
-    
-    public int toNum(String s){
-        String[] arr = s.split(":");
-        return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
-    }
-    
-    public String toString(int n){
-        int h = n / 60;
-        int m = n % 60;
         
-        return String.format("%02d:%02d", h, m);
+        
+        return answer;
+    }
+    
+    public int toMin(String str){
+        String[] s = str.split(":");
+        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
+    }
+    
+    public String toStr(int num){
+        int m = num % 60;
+        int h = num / 60;
+        
+        return String.format("%02d:%02d", h,m);
     }
 }
