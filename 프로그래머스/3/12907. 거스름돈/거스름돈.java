cd46a5081399974e23cdcb23d5fcda42f973973d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-16 10:56:09 +0900

    [level 3] Title: 거스름돈, Time: 12.13 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n, int[] money) {
         int answer = 0;
+        // money에 있는걸로 채워야함
         int[] dp = new int[n + 1];
         dp[0] = 1;
         
@@ -9,6 +10,7 @@ class Solution {
                 dp[i] = (dp[i] + dp[i - m]) % 1000000007;
             }
         }
+        
         
         return dp[n];
     }
