e56755d3deab6ef3db97b1d2d579e6c665acddbf
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-21 15:01:03 +0900

    [level 3] Title: 양과 늑대, Time: 4.56 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,33 +1,40 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>>graph;
-    int[] info;
+    List<List<Integer>> graph;
+    int[] arr;
     int answer = 0;
     public int solution(int[] info, int[][] edges) {
+        int n = info.length;
+        
+        // 그래프 만들기
         graph = new ArrayList<>();
-        this.info = info;
-        for(int i = 0; i < info.length; i++){graph.add(new ArrayList<>());}
+        arr = info.clone();
+        for(int i = 0; i <= n; i++){graph.add(new ArrayList<>());}
         for(int[] e : edges){graph.get(e[0]).add(e[1]);}
         
-        List<Integer> list = new ArrayList<>();
-        for(int next : graph.get(0)){list.add(next);}
-        // 이걸 처음부터 시작해서 양의 갯수가 잘 맞는지 확인하면서 이동해야함
-        dfs(1, 0, list);
+        // 양늑은 처음에 무조건 양으로 시작하기에
+        // 경로들을 다음 목적지들을 담아서 dfs 시키기
+        
+        List<Integer> path = new ArrayList<>();
+        for(int next : graph.get(0)){path.add(next);}
+        
+        dfs(1, 0, path);
         return answer;
     }
     
-    public void dfs(int sheep, int wolf, List<Integer> list){
-        if(wolf >= sheep) return;
-        answer = Math.max(answer, sheep);
+    public void dfs(int sheep, int wolf, List<Integer> path){
+        if(sheep <= wolf) return;
+        else{answer = Math.max(answer, sheep);}
         
-        for(int next : list){
-            List<Integer> nextList = new ArrayList<>(list);
-            nextList.remove(Integer.valueOf(next));
-            nextList.addAll(graph.get(next));
-
-            if(info[next] == 0){dfs(sheep + 1, wolf, nextList);}
-            else{dfs(sheep, wolf + 1, nextList);}
+        // 현재 경로안에서 다음으로 갈 수 있는 경우들
+        for(int p : path){
+            List<Integer> pClone = new ArrayList<>(path);
+            pClone.remove(Integer.valueOf(p));  // 지금 값 바로 삭제
+            pClone.addAll(graph.get(p));        // 현재 다음 값들 이동하는거
+            
+            if(arr[p] == 0){dfs(sheep + 1, wolf, pClone);}
+            else{dfs(sheep, wolf + 1, pClone);}
         }
     }
 }
