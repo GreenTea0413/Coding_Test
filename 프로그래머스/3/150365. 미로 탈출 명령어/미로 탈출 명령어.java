520876f05fb2392080da42a57e09c83a5a94b265
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-21 17:05:29 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.55 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,36 +1,34 @@
 import java.util.*;
 
 class Solution {
-    int[][] d = {{1,0}, {0,-1},{0,1}, {-1,0}};
-    String[] dir = {"d", "l", "r", "u"};
+    int[][] dir = {{1, 0}, {0, -1}, {0, 1}, {-1, 0}};
+    String[] d = {"d", "l", "r", "u"};
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         StringBuilder answer = new StringBuilder();
-        int dist = Math.abs(x - r) + Math.abs(y - c);
+        // 일단 바로 갔을 때의 거리
+        int dist = Math.abs(r - x) + Math.abs(c - y);
+        if(dist > k) return "impossible"; // 거리가 k보다 길면 못가니까
+        if((k - dist) % 2 == 1) return "impossible"; // 이게 거리가 아무리 된다고 해도 홀수면 못감
         
-        if(dist > k) return "impossible";
-        if ((k - dist) % 2 == 1) return "impossible";
-        
-        // 이제 여기서 어떻게 갈지 계산
         while(k > 0){
             for(int i = 0; i < 4; i++){
-                int nx = x + d[i][0];
-                int ny = y + d[i][1];
-
+                int nx = x + dir[i][0];
+                int ny = y + dir[i][1];
+                
                 if(nx < 1 || nx > n || ny < 1 || ny > m) continue;
                 int remain = k - 1;
-                int nextDist = Math.abs(nx - r) + Math.abs(ny - c);
-
-                if(remain >= nextDist && (remain - nextDist) % 2 == 0){
+                int nextDist = Math.abs(r - nx) + Math.abs(c - ny);
+                
+                if(nextDist <= remain && (remain - nextDist) % 2 == 0){
                     x = nx;
                     y = ny;
-                    k --;
-                    answer.append(dir[i]);
+                    k--;
+                    answer.append(d[i]);
                     
                     break;
                 }
             }
         }
-        
         return answer.toString();
     }
 }
