e0e341ff42a145a312ad74bb976aa9ec3e8ce3f1
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-22 16:43:38 +0900

    [level 3] Title: 길 찾기 게임, Time: 21.51 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,89 +1,85 @@
 import java.util.*;
 
 class Solution {
-
-    static class Node {
-        int x, y, num;
-        Node left, right;
-
-        Node(int x, int y, int num) {
+    class Node{
+        int x;
+        int y;
+        int idx;
+        
+        Node left;
+        Node right;
+        
+        Node(int x, int y, int idx){
             this.x = x;
             this.y = y;
-            this.num = num;
+            this.idx = idx;
         }
     }
-
+    
     List<Integer> pre = new ArrayList<>();
     List<Integer> post = new ArrayList<>();
-
     public int[][] solution(int[][] nodeinfo) {
+        int n = nodeinfo.length;
+        int[][] answer = new int[2][n];
+        
         List<Node> nodes = new ArrayList<>();
-
-        for (int i = 0; i < nodeinfo.length; i++) {
-            nodes.add(new Node(
-                    nodeinfo[i][0],
-                    nodeinfo[i][1],
-                    i + 1
-            ));
+        
+        // node객체를 만들어서 해당 값을 넣어주기
+        for(int i = 0 ; i < n; i++){
+            nodes.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
         }
-
-        // y 내림차순으로 하는 이유가 높이가 높은 것이 부모이기 때문에
-        // 최상단 부모 선택
-        nodes.sort((a, b) -> b.y - a.y);
+        
+        // y 좌표들을 기준으로 해서 높은 것을 부모로
+        nodes.sort((a,b) -> (b.y - a.y));
         Node root = nodes.get(0);
         
-        // node 하나씩 꺼내서 규칙에 맡게 트리 생성
-        for (int i = 1; i < nodes.size(); i++) {
+        for(int i = 1; i < n; i++){
             insert(root, nodes.get(i));
         }
-
+        
         preorder(root);
         postorder(root);
-
-        int[][] answer = new int[2][nodeinfo.length];
-
-        for (int i = 0; i < pre.size(); i++) {
-            answer[0][i] = pre.get(i);
-        }
-
-        for (int i = 0; i < post.size(); i++) {
-            answer[1][i] = post.get(i);
-        }
-
+        
+        for(int i = 0; i < n; i++){answer[0][i] = pre.get(i);}
+        for(int i = 0; i < n; i++){answer[1][i] = post.get(i);}
         return answer;
     }
-
-    private void insert(Node parent, Node child) {
-        if (child.x < parent.x) {
-            if (parent.left == null) {
-                parent.left = child;
-            } else {
+    
+    public void insert(Node parent, Node child){
+        if(parent.x > child.x){
+            if(parent.left != null){
                 insert(parent.left, child);
             }
-        } else {
-            if (parent.right == null) {
-                parent.right = child;
-            } else {
-                insert(parent.right, child);
+            else{
+                parent.left = child;
+            }
+        }
+        else{
+            if(parent.x < child.x){
+                if(parent.right != null){
+                    insert(parent.right, child);
+                }
+                else{
+                    parent.right = child;
+                }
             }
         }
     }
-
-    private void preorder(Node node) {
-        if (node == null) return;
-
-        pre.add(node.num);
-
-        preorder(node.left);
-        preorder(node.right);
+    
+    public void preorder(Node root){
+        if(root != null){
+            pre.add(root.idx);
+            
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
-
-    private void postorder(Node node) {
-        if (node == null) return;
-
-        postorder(node.left);
-        postorder(node.right);
-
-        post.add(node.num);
+    
+    public void postorder(Node root){
+        if(root != null){
+            postorder(root.left);
+            postorder(root.right);
+            post.add(root.idx);
+        }
     }
 }
