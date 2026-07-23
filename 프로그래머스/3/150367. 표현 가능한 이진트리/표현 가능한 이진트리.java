3cdd96854a3a6d01d06f3a17c90c2b2ee8b8f568
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-23 12:20:18 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 19.54 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] solution(long[] numbers) {
-        int len = numbers.length;
+        int len =numbers.length;
         int[] answer = new int[len];
         
-        for(int i = 0 ; i < len; i++){
+        // 이진트리 꽉찬 이진트리 사이즈는 1 3 7 ...
+        for(int i = 0; i < len; i++){
             String bin = Long.toBinaryString(numbers[i]);
             
-            // 트리는 꽉 채우는걸로 해야하고 차피 비어있으면 그만큼 + 0 해주면 됨
             int size = 1;
             while(size < bin.length()){
                 size = size * 2 + 1;
@@ -16,21 +16,20 @@ class Solution {
                 bin = "0" + bin;
             }
             
-            // 이러면 원래 7이면 111이니까 size 가 3일 때 안돌아감
-            // 0 추가되는거 없이
-            answer[i] = check(bin, 0, size - 1) ? 1 : 0;
+            answer[i] = check(bin, 0, size - 1) ? 1 : 0 ;
         }
         return answer;
     }
     
-    boolean check(String bin, int left, int right){
+    boolean check(String bin, int left ,int right){
         if(left == right) return true;
+        
         int mid = (left + right) / 2;
-                
         if(bin.charAt(mid) == '0'){
             int leftMid = (left + mid - 1) / 2;
             int rightMid = (right + mid + 1) / 2;
-            if(bin.charAt(leftMid) == '1' || bin.charAt(rightMid) == '1') return false;
+            
+            if(bin.charAt(leftMid) == '1' || bin.charAt(rightMid) == '1'){return false;}
         }
         
         return check(bin, left, mid - 1) && check(bin, mid + 1, right);
