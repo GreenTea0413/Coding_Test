bb8230ba45c015279b3b36896a1b4554cbbc3c2f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-23 10:17:19 +0900

    [level 3] Title: 광고 삽입, Time: 178.59 ms, Memory: 330 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,41 +1,46 @@
-import java.util.*;
-
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
-        int play = (int)toNum(play_time);
-        int adv = (int)toNum(adv_time);
-        long[] arr = new long[play + 1];
+        String answer = "";
+        int play = (int) toNum(play_time);
+        int adv = (int) toNum(adv_time);
         
-        for(String log : logs){
-            String[] s = log.split("-");
+        // 총 플레이 타임내에서 얼만큼 실행하고 그 실행한 횟수들의 합
+        // 그래서 첫시작과 끝 부분에 +와 -를 배치해서 누적합
+        long[] arr = new long[play + 1];
+        for(int i = 0; i < logs.length; i++){
+            String[] s = logs[i].split("-");
             int start = (int)toNum(s[0]);
             int end = (int)toNum(s[1]);
             
-            arr[start] ++;
-            arr[end] --;
+            arr[start] += 1;
+            arr[end] -= 1;
         }
         
-        // 누적합으로 시청하는 사람 구하기
+        // 여기서 광고 몇개가 실행됐는지 누적합으로 구하기
         for(int i = 1; i <= play; i++){
-            arr[i] += arr[i - 1];
-        }
-        // 누적합으로 시간 구하기
-        for(int i = 1; i <= play; i++){
-            arr[i] += arr[i - 1];
+            arr[i] += arr[i-1];
         }
         
-        long max = arr[adv - 1];
+        // 그리고 여기서 광고 갯수에다가 한번씩 더 더하면 총 광고 누적 시간
+        for(int i = 1; i<= play; i++){
+            arr[i] += arr[i-1];
+        }
+        
+        // 광고시작시간을 젤 앞에 두었을 경우를 먼저 고려
         long startTime = 0;
+        long max = arr[adv - 1];
         
-        for(int end = adv; end < play; end++){
-            long cur = arr[end]  - arr[end - adv];
+        // 그 다음 광고 끝난 시점부터 +1씩 하면서 젤 많이 시청시간이 누적되는 곳을 찾기
+        for(int end = adv; end < play; end ++){
+            // 이게 1415로 시작하면 1416 - 1415 해서 현재 구간에서 얼마나 많이 보는가를 구하기
+            long cur = arr[end] - arr[end - adv];
             
+            // 그리고 현 max인 총 누적시간의 합보다 현재값이 더크면 업데이트 후 시작시간 광고시작시간 넣어주기
             if(cur > max){
                 max = cur;
                 startTime = end - adv + 1;
             }
         }
-        
         int h = (int)(startTime / 3600);
         int m = (int)(startTime % 3600) / 60;
         int s = (int)(startTime % 60);
@@ -44,8 +49,8 @@ class Solution {
     }
     
     public long toNum(String s){
-        String[] st = s.split(":");
+        String[] arr = s.split(":");
+        return Long.parseLong(arr[0]) * 3600 + Long.parseLong(arr[1]) * 60 + Long.parseLong(arr[2]);
         
-        return Long.parseLong(st[0]) * 3600 + Long.parseLong(st[1]) * 60 + Long.parseLong(st[2]);
     }
 }
