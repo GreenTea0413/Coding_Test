9b6735a8005de94115822eafba6384c60f94c191
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-24 11:46:22 +0900

    [level 3] Title: 스타 수열, Time: 105.60 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -3,21 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] a) {
         int answer = 0;
-        int len = a.length;
-        if(len < 2) return answer;
         
-        // 이 문제는 해당 값을 얼마나 가지고 있고
-        // 그 길이를 계속해서 늘려가면서 확인하는 문제
+        // 맵을 통해서 해당 숫자가 몇개 있는지 알아야함
         Map<Integer, Integer> map = new HashMap<>();
-        for(int n : a){map.put(n, map.getOrDefault(n, 0) + 1);}
+        for(int n : a){
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
         
-        // 키 하나씩 꺼내서 몇개 들어있는지 확인하고 그게 answer보다 작으면 패스
-        // 그러지 않으면 for문을 돌려서 a배열 안에 값들이랑 
+        // 2개씩 묶어야함
         for(int key : map.keySet()){
             if(map.get(key) <= answer) continue;
             
             int count = 0;
-            for(int i = 0; i < len - 1; i++){
+            for(int i = 0; i < a.length - 1; i++){
                 if((a[i] == key || a[i + 1] == key) && (a[i] != a[i + 1])){
                     count++;
                     i++;
