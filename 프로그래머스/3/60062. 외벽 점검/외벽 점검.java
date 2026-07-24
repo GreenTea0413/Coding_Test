189637feab201de816d8975c7f55fb9ae9c0ec9d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-24 11:41:53 +0900

    [level 3] Title: 외벽 점검, Time: 31.34 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -1,57 +1,55 @@
 import java.util.*;
+
 class Solution {
     int answer = Integer.MAX_VALUE;
-    int w;
+    int wLen;
     public int solution(int n, int[] weak, int[] dist) {
-        // n미터
-        // 사람마다 1시간당 거리
-        w = weak.length;
-        int[] weak2 = new int[w * 2];
-        for(int i = 0; i < w; i++){
+        wLen = weak.length;
+        int[] weak2 = new int[n * 2];
+        for(int i = 0; i < wLen; i++){
             weak2[i] = weak[i];
-            weak2[i + w] = weak[i] + n;
+            weak2[i + wLen] = weak[i] + n;
         }
-
-        // 친구 순서를 고려해서 풀어야함
-        dfs(n, weak2, dist, new boolean[dist.length], new ArrayList<>());
-        return answer == Integer.MAX_VALUE ? -1 : answer;
+        
+        // 사람이 들어가는 순서를 다 고려해야함
+        dfs(n, weak2, dist, new ArrayList<>(), new boolean[dist.length]);
+        return answer == Integer.MAX_VALUE ? -1: answer;
     }
     
-    public void dfs(int n, int[] weak2, int[] dist, boolean[] v, List<Integer> friend){
+    public void dfs(int n, int[] weak2, int[] dist, List<Integer> friend, boolean[] v){
         if(dist.length == friend.size()){
-            // 같아지면 이제 여기서 확인
-            for(int i = 0; i < w; i++){
-                int count = check(weak2, i, friend) ;
-                if(count != -1){answer = Math.min(answer, count);}
+            for(int i = 0; i < wLen; i++){
+                int count = check(i, weak2, friend);
+                if(count != -1) answer= Math.min(answer, count);
             }
             return;
         }
         
         for(int i = 0; i < dist.length; i++){
             if(!v[i]){
-                friend.add(dist[i]);
                 v[i] = true;
+                friend.add(dist[i]);
                 
-                dfs(n, weak2, dist, v, friend);
+                dfs(n, weak2, dist, friend, v);
                 
-                friend.remove(friend.size() - 1);
                 v[i] = false;
+                friend.remove(friend.size() - 1);
             }
         }
     }
-    // friend가 4 3 2 1
-    public int check(int[] weak2, int start, List<Integer> friend){
+    
+    public int check(int start, int[] weak2, List<Integer> friend){
         int f = 0;
-        int cur = weak2[start] + friend.get(f); // 1
-        // start지점으로 부터 w개수 만큼만 진행
-        for(int i = start; i < start + w; i++){
-            // 이러면 1 5 6 10 / 5 6 10 11 / ... 가능
+        int cur = weak2[start] + friend.get(f);
+        
+        for(int i = start; i < start + wLen; i++){
             if(weak2[i] > cur){
                 f++;
-                if(f == friend.size()){return -1;}
+                if(f == friend.size()) return -1;
                 cur = weak2[i] + friend.get(f);
             }
         }
+        
         return f + 1;
     }
 }
