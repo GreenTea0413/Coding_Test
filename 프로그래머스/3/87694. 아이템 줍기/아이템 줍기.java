cc213720d760f5805efa12568a3e790b575f5e3b
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-25 19:36:52 +0900

    [level 3] Title: 아이템 줍기, Time: 1.61 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -4,59 +4,60 @@ class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
         int[][] board = new int[102][102];
+        int[][] d = {{0,1}, {0,-1},{1,0},{-1,0}};
         
-        // 테두리 만들기
         for(int[] rec : rectangle){
             int x1 = rec[0] * 2;
             int y1 = rec[1] * 2;
             int x2 = rec[2] * 2;
             int y2 = rec[3] * 2;
-            
+                
             for(int i = x1; i < x2 + 1; i++){
                 for(int j = y1; j < y2 + 1; j++){
                     board[i][j] = 1;
                 }
-            }
+            }    
         }
         
-        // 테두리 선만 남기고 안에 없애기
         for(int[] rec : rectangle){
             int x1 = rec[0] * 2;
             int y1 = rec[1] * 2;
             int x2 = rec[2] * 2;
             int y2 = rec[3] * 2;
-            
+                
             for(int i = x1 + 1; i < x2; i++){
                 for(int j = y1 + 1; j < y2; j++){
                     board[i][j] = 2;
                 }
-            }
+            }    
         }
         
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{characterX * 2, characterY * 2, 0});
         boolean[][] v = new boolean[102][102];
         
-        int[][] d = {{1,0}, {0,1}, {-1,0}, {0,-1}};
         while(!q.isEmpty()){
-            int[] cur = q.poll();
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+            int dist = now[2];
             
-            if(cur[0] == itemX * 2 && cur[1] == itemY * 2){
-                return cur[2] / 2;
+            if(x == itemX * 2 && y == itemY * 2){
+                return dist / 2;
             }
-            if(v[cur[0]][cur[1]]) continue;
-            v[cur[0]][cur[1]] = true;
             
             for(int i = 0; i < 4; i++){
-                int nx = cur[0] + d[i][0];
-                int ny = cur[1] + d[i][1];
+                int nx = x + d[i][0];
+                int ny = y + d[i][1];
+                
                 if(nx < 0 || nx > 101 || ny < 0 || ny > 101) continue;
+                
                 if(!v[nx][ny] && board[nx][ny] == 1){
-                    q.offer(new int[]{nx, ny, cur[2] + 1});
+                    q.offer(new int[]{nx, ny, dist + 1});
+                    v[nx][ny] = true;
                 }
             }
         }
-        
         
         return answer;
     }
