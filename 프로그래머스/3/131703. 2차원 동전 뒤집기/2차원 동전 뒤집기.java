d4d00b38c70d865abb9c043004c157642108ab55
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-07-26 12:07:18 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.06 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,62 +1,59 @@
 class Solution {
     public int solution(int[][] beginning, int[][] target) {
-        int n = beginning.length;
-        int m = beginning[0].length;
-
-        // diff 배열 생성
-        int[][] diff = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                // 이걸로 뒤집어야하는 부분들을 찾아서 diff에 설정 diff가 1이면 다르다는 뜻이니까 그걸 뒤집어야함
+        int answer = Integer.MAX_VALUE;
+        int n = target.length;
+        int m = target[0].length;
+        
+        int[][] diff = new int[n][m]; 
+        
+        // 뒤집어야 하는 칸들 고려
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 diff[i][j] = beginning[i][j] ^ target[i][j];
+                
             }
         }
-
-        int answer = Integer.MAX_VALUE;
-
-        // R[0] = 0 또는 1
-        for (int firstRow = 0; firstRow <= 1; firstRow++) {
+        
+        
+        // 행과 열을 만들어서 차이를 구하기
+        for(int i = 0; i <= 1; i++){
             int[] row = new int[n];
             int[] col = new int[m];
-        
-            row[0] = firstRow;
-
-            // diff[0][j] = row[0] ^ col[j]이여서 반대로 ^ row[0]를 옮겨서
-            // col에 대해서 뒤집어야하는지 안뒤집어도 되는지 값 넣기
-            for (int j = 0; j < m; j++) {
-                col[j] = diff[0][j] ^ row[0];
+            
+            // 행을 먼저 기준으로 하기 행을 안뒤집는다는 가정하에
+            row[0] = i;
+            
+            // 열은 차이와 row를 뒤집냐 안뒤집냐를 ^로 구하기
+            for(int y = 0; y < m; y++){
+                col[y] = diff[0][y] ^ row[0];
             }
-
-            // diff[i][0] = row[i] ^ col[0]
-            // 동일하게 하기
-            for (int i = 1; i < n; i++) {
-                row[i] = diff[i][0] ^ col[0];
+            
+            
+            for(int x = 1; x < n; x++){
+                row[x] = diff[x][0] ^ col[0];
             }
-
-            boolean possible = true;
-
-            // 모든 칸 검증
-            // 했을때 각 row랑 col이 한개만 1일때 뒤집었을 때 값이 달라짐
-            // 둘다 1이거나 0이면 그대로 원점이기때문에 안뒤집는걸로 쳐야함
-            for (int i = 0; i < n && possible; i++) {
-                for (int j = 0; j < m; j++) {
-                    if ((row[i] ^ col[j]) != diff[i][j]) {
-                        possible = false;
-                        break;
+            
+            boolean possible =true;
+            
+            for(int a = 0; a < n; a++){
+                if(possible){
+                    for(int b = 0; b < m; b++){
+                        if((row[a] ^ col[b]) != diff[a][b]){
+                            possible = false;
+                            break;
+                        }
                     }
                 }
             }
-
-            if (!possible) continue;
-
-            // row, col이 1이면 diff랑 다르기 때문에 뒤집어야한다는 뜻
+            
+            if(!possible) continue;
             int count = 0;
-            for (int r : row) {count += r;}
-            for (int c : col) {count += c;}
-
-            answer = Math.min(answer, count);
+            for(int r : row){count += r;}
+            for(int c : col){count += c;}
+            
+            answer= Math.min(answer, count);
         }
-
+        
         return answer == Integer.MAX_VALUE ? -1 : answer;
     }
 }
