97c7fa1cc13cb258d8d3e8936aad20541e347280
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-02 14:46:37 +0900

    [level 3] Title: 섬 연결하기, Time: 0.96 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,29 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] costs) {
-        int answer = 0;
         List<List<int[]>> graph = new ArrayList<>();
         for(int i = 0; i < n; i++){graph.add(new ArrayList<>());}
-        
-        for(int[] cost : costs){
-            graph.get(cost[0]).add(new int[]{cost[1] ,cost[2]});
-            graph.get(cost[1]).add(new int[]{cost[0] ,cost[2]});
+        for(int[] c : costs){
+            graph.get(c[0]).add(new int[]{c[1], c[2]});
+            graph.get(c[1]).add(new int[]{c[0], c[2]});
         }
-        Arrays.sort(costs, (a, b) -> (a[2] - b[2]));
         
-        // 장소, 길이 값
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
-        pq.offer(new int[]{0,0});
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (a[1] - b[1]));
+        pq.offer(new int[]{0, 0});
         boolean[] v = new boolean[n];
-        
+        int answer = 0;
         while(!pq.isEmpty()){
             int[] now = pq.poll();
-            
             if(v[now[0]]) continue;
+            
             v[now[0]] = true;
             answer += now[1];
-            
             for(int[] next : graph.get(now[0])){
                 if(!v[next[0]]){
                     pq.offer(next);
