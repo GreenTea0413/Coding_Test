846ca5b81c2b84e61766dc66b2ff3d5f6a11b0f8
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-03 12:29:38 +0900

    [level 3] Title: 여행경로, Time: 1.26 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,15 +3,17 @@ import java.util.*;
 class Solution {
     int l;
     String[] answer;
-    boolean find = false;
+    boolean found;
     public String[] solution(String[][] tickets) {
         l = tickets.length;
         answer = new String[l + 1];
-        boolean[] v = new boolean[l + 1];
-        // map으로 해당 티켓으로 다음 지역 가는거 확인
+        String[] path = new String[l + 1];
+        path[0] = "ICN";
+        
         Map<String, List<Integer>> map = new HashMap<>();
         for(int i = 0; i < l; i++){
             String start = tickets[i][0];
+            
             map.putIfAbsent(start, new ArrayList<>());
             map.get(start).add(i);
         }
@@ -20,29 +22,26 @@ class Solution {
             Collections.sort(list, (a, b) -> tickets[a][1].compareTo(tickets[b][1]));
         }
         
-        String[] path = new String[l + 1];
-        path[0] = "ICN";
-        dfs(tickets, v, path, "ICN", map, 0);
+        dfs(map, tickets, path, 0, new boolean[l + 1], "ICN");
         return answer;
     }
     
-    public void dfs(String[][] tickets, boolean[] v, String[] path, String start, Map<String, List<Integer>> map, int depth){
+    public void dfs(Map<String, List<Integer>> map, String[][] tickets, String[] path, int depth, boolean[] v, String start){
         if(depth == l){
             answer = path.clone();
-            find = true;
+            found = true;
             return;
         }
-        
         if(!map.containsKey(start)) return;
         
         for(int next : map.get(start)){
-            if(!v[next] && !find){
+            if(!v[next] && !found){
                 v[next] = true;
                 path[depth + 1] = tickets[next][1];
                 
-                dfs(tickets, v, path, tickets[next][1], map, depth + 1);
+                dfs(map, tickets, path, depth + 1, v, tickets[next][1]);
                 
-                v[next] = false;        
+                v[next] = false;
             }
         }
     }
