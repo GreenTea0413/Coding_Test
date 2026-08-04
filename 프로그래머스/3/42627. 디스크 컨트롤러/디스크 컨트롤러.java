1f55b347ef7afed002811729e3a81dcb8663c023
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-04 18:22:06 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.71 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,41 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        // 이러면 일단 요청 시간이 빠른 순으로 넣기
-        Arrays.sort(jobs, (a, b) -> (a[0] - b[0]));
-        
-        // 우선순위 -> 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if(a[2] == b[2]){
-                if(a[1] == b[1]) return a[0] - b[0];
-                else return a[1] - b[1];
+        int answer = 0;
+        int len = jobs.length;
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        // 정렬 순서 : 1. 소요시간 2. 시각 3. 번호
+        // 딱 하나만
+        // 종료한 시점에서 들어온게 겹치면 우선순위대로 먼저 작업 시키기
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
+            if(a[0] == b[0]){
+                if(a[1] == b[1]) return a[2] - b[2];
+                return a[1] - b[1];
             }
-            return a[2] - b[2];
+            return a[0] - b[0];
         });
         
         int time = 0;
-        int answer = 0;
-        int len = jobs.length;
-        int idx = 0;
         int count = 0;
-        // 작업의 번호, 요청시간, 소요시간
+        int idx = 0;
+        
         while(count < len){
-            // 일단 바로 넣고 여기서 time이랑 현재 들어온 값의 요청시간 call과 time이 맞는지 비교
-            // 비교해서 바로 넣기
             while(idx < len && jobs[idx][0] <= time){
-                pq.offer(new int[]{idx, jobs[idx][0], jobs[idx][1]});
+                // 정렬 순서 : 1. 소요시간 2. 시각 3. 번호
+                pq.offer(new int[]{jobs[idx][1], jobs[idx][0], idx});
                 idx++;
             }
             
-            // 그럼 현재 시간에 맞게 들어온 것들이 있을거임
-            // 0,0,3
-            // 2 3 5
-            // 1 1 9
             if(!pq.isEmpty()){
                 int[] now = pq.poll();
-                time += now[2];
-                answer += time - now[1];
                 count++;
+                time += now[0];
+                answer += time - now[1];
             }
             else{
                 time = jobs[idx][0];
