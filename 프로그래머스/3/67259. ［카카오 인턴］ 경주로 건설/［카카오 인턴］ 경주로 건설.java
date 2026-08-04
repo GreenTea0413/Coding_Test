ebd47fd9c6d625d11f42ba1d47171fc62234da83
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-04 16:28:04 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 7.66 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,13 +1,15 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public int solution(int[][] board) {
+        int answer = Integer.MAX_VALUE;
         int n = board.length;
         int[][][] dp = new int[n][n][4];
+        
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                for(int k = 0; k < 4; k++){
-                    dp[i][j][k] = Integer.MAX_VALUE;            
+            for(int j =0; j < n; j++){
+                for(int k =0; k < 4; k++){
+                    dp[i][j][k] = Integer.MAX_VALUE;
                 }
             }
         }
@@ -15,37 +17,39 @@ class Solution {
         for(int i = 0; i < 4; i++){
             dp[0][0][i] = 0;
         }
+        
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{0,0,-1,0});
+        int[][] d = {{0,1}, {0,-1},{1,0},{-1,0}};
         
-        int[][] d = {{0,1},{0,-1},{1,0},{-1,0}};
         while(!q.isEmpty()){
             int[] now = q.poll();
             int x = now[0]; int y = now[1]; int dir = now[2]; int cost = now[3];
-            if(dir != -1 && dp[x][y][dir] < cost) continue;
             
-            if(x == n - 1 && y == n - 1) {
-                dp[x][y][dir] = Math.min(cost, dp[x][y][dir]);
+            //if(dir != -1 && cost > dp[x][y][dir]) continue; 
+            
+            if(x == n - 1 && y == n - 1){
+                dp[x][y][dir] = Math.min(dp[x][y][dir], cost);
             }
             
-            for(int i = 0; i < 4; i++){
-                int nx = x + d[i][0]; int ny = y + d[i][1];
+            for(int i = 0 ; i < 4; i++){
+                int nx = x + d[i][0];
+                int ny = y + d[i][1];
                 
                 if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
                 int nextCost = cost;
-                if(dir == i || dir == -1) nextCost += 100;
+                if(dir == -1 || dir == i) nextCost += 100;
                 else nextCost += 600;
-                
-                if(dp[nx][ny][i] > nextCost && board[nx][ny] == 0){    
-                    q.offer(new int[]{nx,ny,i, nextCost});
+
+                if(board[nx][ny] == 0 && nextCost < dp[nx][ny][i]){
+                    q.offer(new int[]{nx, ny, i, nextCost});
                     dp[nx][ny][i] = nextCost;
                 }
             }
         }
         
-        int answer = Integer.MAX_VALUE;
         for(int i = 0; i < 4; i++){
-            answer = Math.min(dp[n - 1][n - 1][i], answer);
+            answer = Math.min(answer,dp[n - 1][n - 1][i]);
         }
         
         return answer;
