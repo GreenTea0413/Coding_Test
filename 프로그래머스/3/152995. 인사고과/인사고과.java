ca1116e29315783c789c66b7f3f9620ad103d09d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-06 18:09:55 +0900

    [level 3] Title: 인사고과, Time: 72.09 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,34 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-        //어떤 사원이 다른 임의의 사원보다 두 점수가 모두 낮은 경우가 한 번이라도 있다면 그 사원은 인센티브를 받지 못함
-        // 1등 2명이면 2등없이 3등
-        // 0번째가 완호의 석차임
+        int answer = 0;
+        // 앞의 숫자를 내림차순 뒤의 숫자를 오름차순
+        // [3,2][3,2][2,1][2,2][1,4] 로 한다면
+        // 왼쪽 max = 3 오른쪽 max = 2이다. 내라가면 자연스럽게 앞의 숫자는 줄어드니까 뒤에숫자까지 작다면?
+        // 그 사람은 보너스 못받는거임
+        int len = scores.length; 
         int[] wanho = scores[0];
-        int len = scores.length;
-        
-        // 앞에 배열이 큰 순서대로 놓고 그 다음 하나씩 비교할때
-        // 뒤의 점수가 만약에 최대값 보다 작을 때 완호랑 비교
+        int wSum = wanho[0] + wanho[1];
         Arrays.sort(scores, (a, b) -> {
-            if(a[0] == b[0]){
-                return a[1] - b[1];
-            }
-            return b[0] - a[0];
+            if(a[0] == b[0]) return a[1] - b[1];
+            else return b[0] - a[0];
         });
         
-        int maxSecond = 0;
-        int wanhoSum = wanho[0] + wanho[1];;
-        int rank = 1;
-        for(int[] score : scores){
-            if(score[1] < maxSecond){
-                if(score[0] == wanho[0] && score[1] == wanho[1]){
-                    return -1;
-                } continue;
+        // 2를 넣어주는거임
+        int rMax = scores[0][1];
+        for(int[] s : scores){
+            if(s[1] < rMax){
+                if(wanho[0] == s[0] && s[1] == wanho[1]) return -1;
+                else continue;
             }
             
-            maxSecond = Math.max(maxSecond, score[1]);
-            if(score[0] + score[1] > wanhoSum){rank++;}
+            rMax = Math.max(s[1], rMax);
+            if(s[0] + s[1] > wSum) answer++;
         }
-        return rank;
+        
+        return answer + 1;
     }
 }
