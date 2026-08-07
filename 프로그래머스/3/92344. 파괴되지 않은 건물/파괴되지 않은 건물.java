d32ec7a09380a2c381baf7c202c30c81385b2f1a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-07 12:24:04 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 30.74 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,59 +1,60 @@
 class Solution {
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
+        // 누적합으로 푸는 문제
         int n = board.length;
         int m = board[0].length;
-        int[][] map = new int[n + 1][m + 1];
         
-        // 이거 전에 무슨 점 끝 부분만 계산하기
-        // 1,0,0,3,4,4면 1은 공격 0,0 ~ 3,4 까지 4만큼 감소
-        // 각 점을 따지면 (0,0) (0,4) (3,0) (3,4)
-        // 누적합으로 이 문제를 풀어야 시간초과가 발생안함
+        // dp에는 skill로 인해서 부숴지거나 세우는거에 범위를 넣기
+        int[][] dp = new int[n + 1][m + 1];
         
         for(int[] s : skill){
-            if(s[0] == 1){
-                map[s[1]][s[2]] -= s[5];
-                map[s[1]][s[4] + 1] += s[5];
-                map[s[3] + 1][s[2]] += s[5];
-                map[s[3] + 1][s[4] + 1] -= s[5];
+            int type = s[0]; 
+            int r1 = s[1]; 
+            int c1 = s[2];
+            int r2 = s[3];
+            int c2 = s[4];
+            int degree = s[5];
+            
+            // 0,0 ~ 3,4 까지를 정한다면
+            // 4,0에는 -degree를 넣어야함
+            // 0,0 1,0 2,0 3,0 4,0에서
+            // -4 0 0 0 4 // -4 -4 -4 -4  0 // -4 -4 -4 -4 0
+            // 0 0 0 0 0  // -4  0  0  0  0 // -4 -4 -4 -4 0
+            // 0 0 0 0 0  // -4  0  0  0  0 // -4 -4 -4 -4 0
+            // 4 0 0 0 -4 //  0  0  0  0 -4 //  0  0  0  0 0
+            if(type == 1){
+                dp[r1][c1] -= degree;
+                dp[r1][c2 + 1] += degree;
+                dp[r2 + 1][c1] += degree;
+                dp[r2 + 1][c2 + 1] -= degree;
             }
             else{
-                map[s[1]][s[2]] += s[5];
-                map[s[1]][s[4] + 1] -= s[5];
-                map[s[3] + 1][s[2]] -= s[5];
-                map[s[3] + 1][s[4] + 1] += s[5];
+                dp[r1][c1] += degree;
+                dp[r1][c2 + 1] -= degree;
+                dp[r2 + 1][c1]  -= degree;
+                dp[r2 + 1][c2 + 1] += degree;
             }
         }
         
-        // 0 0 0 0 0 0
-        // 0 0 0 0 0 0
-        // 0 0 0 0 0 0
-        // 0 0 0 0 0 0
-        // 0 0 0 0 0 0
-        
-        // -4 0 0 0 0 4
-        // 0 0 0 0 0 0
-        // 0 0 0 0 0 0
-        // 0 0 0 0 0 0
-        // 4 0 0 0 0 -4
-        
-        // 왼쪽 부터 오른쪽으로 가면서 값 더해주기
+        // 누적합 범위 정했으니 이제 더하기하면서 값 정하기
+        // 왼쪽값을 오른쪽으로 넣기
         for(int i = 0; i < n + 1; i++){
             for(int j = 1; j < m + 1; j++){
-                map[i][j] += map[i][j - 1];   
+                dp[i][j] += dp[i][j-1];
             }
         }
         
-        // 위에서 아래로 값 더해주기
+        // 위에서 아래로 값 넣기
         for(int i = 0; i < m + 1; i++){
             for(int j = 1; j < n + 1; j++){
-                map[j][i] += map[j - 1][i];   
+                dp[j][i] += dp[j - 1][i];
             }
         }
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(board[i][j] + map[i][j] > 0)  answer++;
+                if(board[i][j] + dp[i][j] > 0) answer++;
             }
         }
         return answer;
