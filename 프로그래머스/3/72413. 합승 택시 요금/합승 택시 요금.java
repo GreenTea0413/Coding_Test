b8248235ffff2705de17a81eea429fa6e9850779
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-09 17:46:32 +0900

    [level 3] Title: 합승 택시 요금, Time: 22.46 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,51 +1,50 @@
 import java.util.*;
 
 class Solution {
+    List<List<int[]>> graph = new ArrayList<>();
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = Integer.MAX_VALUE;
-        List<List<int[]>> graph = new ArrayList<>();
-        for(int i = 0; i <= n; i++){graph.add(new ArrayList<>());}
+        for(int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        // 무방향 그래프 [출발] {도착, 비용}
         for(int[] f : fares){
             graph.get(f[0]).add(new int[]{f[1], f[2]});
             graph.get(f[1]).add(new int[]{f[0], f[2]});
         }
         
-        // 시작은 s
-        // 도착은 a, b 둘다 해야하는 상황
-        // 시작점 s, a, b에 대한 모든 거리의 최소를 구하기
-        int[] sn = dijkstra(s, graph, n);
-        int[] an = dijkstra(a, graph, n);
-        int[] bn = dijkstra(b, graph, n);
+        // 3개 다 한번씩 돌아가면서 합 구하기
+        // s에서 1 a에서 1 b에서 1가고 다 더한다음에 answer랑 최소 비교
+        int[] sn = dfs(s, n);
+        int[] an = dfs(a, n);
+        int[] bn = dfs(b, n);
         
+        // 마무리로 해당 목적지까지 길이 다 합쳐서 비교하기
         for(int i = 1; i <= n; i++){
-            answer = Math.min(answer, sn[i] + an[i] + bn[i]);
+            answer = Math.min(sn[i] + an[i] + bn[i], answer);
         }
-        
         return answer;
     }
     
-    
-    public int[] dijkstra(int start, List<List<int[]>> graph, int n){
+    public int[] dfs(int start, int n){
         int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
         
-        // 점, 거리
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
-        pq.offer(new int[]{start, 0});
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
         
-        while(!pq.isEmpty()){
-            int[] now = pq.poll();
-            int l = now[0];
-            int d = now[1];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{start, 0});
+        
+        while(!q.isEmpty()){
+            int[] now = q.poll();
             
-            if(dist[l] < d){continue;}
-            
-            for(int[] next : graph.get(l)){
-                int nextDist = next[1] + d;
-                if(dist[next[0]] > nextDist){
-                    dist[next[0]] = nextDist;
-                    pq.offer(new int[]{next[0], nextDist});
+            if(now[1] > dist[now[0]]) continue;
+            for(int[] next : graph.get(now[0])){
+                int p = next[0];
+                int d = next[1];
+                int nDist = d + now[1];
+                
+                if(nDist < dist[p]){
+                    q.offer(new int[]{p, nDist});
+                    dist[p] = nDist;
                 }
             }
         }
