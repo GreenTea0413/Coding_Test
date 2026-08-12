ae3138d25b00443e17e10fa2a0fd3be13c2a3272
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-12 11:47:03 +0900

    [level 3] Title: 길 찾기 게임, Time: 17.68 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -14,23 +14,21 @@ class Solution {
             this.y = y;
             this.idx = idx;
         }
-    }
+    };
     
     List<Integer> pre = new ArrayList<>();
     List<Integer> post = new ArrayList<>();
+    
     public int[][] solution(int[][] nodeinfo) {
         int n = nodeinfo.length;
         int[][] answer = new int[2][n];
         
         List<Node> nodes = new ArrayList<>();
-        
-        // node객체를 만들어서 해당 값을 넣어주기
-        for(int i = 0 ; i < n; i++){
+        for(int i = 0; i < n; i++){
             nodes.add(new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1));
         }
         
-        // y 좌표들을 기준으로 해서 높은 것을 부모로
-        nodes.sort((a,b) -> (b.y - a.y));
+        nodes.sort((a, b) -> (b.y - a.y));
         Node root = nodes.get(0);
         
         for(int i = 1; i < n; i++){
@@ -40,36 +38,28 @@ class Solution {
         preorder(root);
         postorder(root);
         
-        for(int i = 0; i < n; i++){answer[0][i] = pre.get(i);}
-        for(int i = 0; i < n; i++){answer[1][i] = post.get(i);}
+        for(int i = 0 ; i < n ; i++){
+            answer[0][i] = pre.get(i);
+            answer[1][i] = post.get(i);
+        }
         return answer;
     }
     
-    public void insert(Node parent, Node child){
-        if(parent.x > child.x){
-            if(parent.left != null){
-                insert(parent.left, child);
-            }
-            else{
-                parent.left = child;
-            }
+    public void insert(Node root, Node child){
+        if(root.x > child.x){
+            if(root.left == null) root.left = child;
+            else insert(root.left, child);
         }
         else{
-            if(parent.x < child.x){
-                if(parent.right != null){
-                    insert(parent.right, child);
-                }
-                else{
-                    parent.right = child;
-                }
-            }
+            if(root.right == null) root.right = child;
+            else insert(root.right, child);
         }
     }
     
     public void preorder(Node root){
+        // V L R
         if(root != null){
             pre.add(root.idx);
-            
             preorder(root.left);
             preorder(root.right);
         }
@@ -80,6 +70,8 @@ class Solution {
             postorder(root.left);
             postorder(root.right);
             post.add(root.idx);
+            
         }
+            
     }
 }
