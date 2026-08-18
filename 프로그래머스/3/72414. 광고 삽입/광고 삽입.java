b6a715ffd70842b6d8a40a11c4b69c7e598aaf23
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-18 23:36:47 +0900

    [level 3] Title: 광고 삽입, Time: 191.05 ms, Memory: 332 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,50 +1,50 @@
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
+        String answer = "";        
+        // 100 * 3600
+        // 누적합 문제
         int play = (int)toNum(play_time);
         int adv = (int)toNum(adv_time);
+        long[] dp = new long[play + 1];
         
-        long[] arr = new long[play + 1];
-        int len = logs.length;
-        
-        for(int i = 0; i < len; i++){
-            String[] l = logs[i].split("-");
-            int start = (int) toNum(l[0]);
-            int end = (int) toNum(l[1]);
+        // 이 문제는 int로 하면 오버플로우 발생함
+        for(String log : logs){
+            String[] l = log.split("-");
+            int start = (int)toNum(l[0]);
+            int end = (int)toNum(l[1]);
             
-            arr[start]++;
-            arr[end]--;
+            dp[start]++;
+            dp[end]--;
         }
         
         for(int i = 1; i <= play; i++){
-            arr[i] += arr[i-1];
+            dp[i] += dp[i - 1];
+        }
+        for(int i = 1; i <= play; i++){
+            dp[i] += dp[i - 1];
         }
         
-        for(int i = 1; i<= play; i++){
-            arr[i] += arr[i-1];
-        }
-        
-        long max = arr[adv - 1];
+        long max = dp[adv - 1];
         long startTime = 0;
         
         for(int i = adv; i < play; i++){
-            long cur = arr[i] - arr[i - adv];
+            long cur = dp[i] - dp[i - adv];
             
-            // 누적 시간이 더 크다!
             if(cur > max){
                 max = cur;
                 startTime = i - adv + 1;
             }
         }
         
-        // startTime = 6030
         int h = (int) startTime / 3600;
         int m = (int) startTime % 3600 / 60;
         int s = (int) startTime % 60;
         
         return String.format("%02d:%02d:%02d", h,m,s);
     }
-    long toNum(String s){
-        String[] arr = s.split(":");
-        return Long.parseLong(arr[0]) * 3600 + Long.parseLong(arr[1]) * 60 + Long.parseLong(arr[2]);
+    
+    long toNum(String str){
+        String[] s = str.split(":");
+        return Long.parseLong(s[0]) * 3600 + Long.parseLong(s[1]) * 60 + Long.parseLong(s[2]);
     }
 }
