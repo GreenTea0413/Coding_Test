353bee6c6994b6a54eaf512a654440c37fa27be5
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-19 00:06:32 +0900

    [level 3] Title: 110 옮기기, Time: 49.81 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -7,19 +7,16 @@ class Solution {
         
         for(int i = 0; i < len; i++){
             StringBuilder sb = new StringBuilder();
-            String str = s[i];
             int count = 0;
             
-            for(char c : str.toCharArray()){
+            for(char c : s[i].toCharArray()){
                 sb.append(c);
-                
+
                 if(sb.length() >= 3){
-                    char c1 = sb.charAt(sb.length() - 3);
-                    char c2 = sb.charAt(sb.length() - 2);
-                    char c3 = sb.charAt(sb.length() - 1);
-                    
-                    if(c1 == '1' && c2 == '1' && c3 == '0'){
-                        count++;
+                    if(sb.charAt(sb.length() - 1) == '0' &&
+                        sb.charAt(sb.length() - 2) == '1' &&
+                        sb.charAt(sb.length() - 3) == '1'){
+                        count ++;
                         sb.setLength(sb.length() - 3);
                     }
                 }
@@ -27,22 +24,22 @@ class Solution {
             
             StringBuilder result = new StringBuilder();
             int lastIdx = sb.lastIndexOf("0");
-            
-            if(lastIdx == -1){
-                for(int j = 0; j < count; j++){
-                    result.append("110");    
-                }
-                result.append(sb);
-            }
-            else{
-                result.append(sb.substring(0,lastIdx + 1));
-                for(int j = 0; j < count; j++){
-                    result.append("110");
-                }
+            // 0이 있으면 0뒤로 110 붙여야함
+            // 0이 없으면 젤 앞에 먼저 110 붙여야함
+            if(lastIdx != -1){
+                // 0이 아니면 result에 sb를먼저 붙이기 0 오기 전
+                result.append(sb.substring(0, lastIdx + 1));
+                for(int j = 0; j < count; j++) result.append("110");
                 result.append(sb.substring(lastIdx + 1));
             }
+            else{
+                for(int j = 0; j < count; j++) result.append("110");
+                result.append(sb);
+            }
+            
             answer[i] = result.toString();
         }
+         
         return answer;
     }
 }
