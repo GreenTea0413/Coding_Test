45da8f50400751bd6f1b32e30699154446cf622e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-20 15:51:03 +0900

    [level 3] Title: 스타 수열, Time: 117.65 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -3,26 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int[] a) {
         int answer = 0;
+        int len = a.length;
+        if(len == 1) return 0;
         
-        // 맵을 통해서 해당 숫자가 몇개 있는지 알아야함
+        // 여기에 Key값을 넣어서
         Map<Integer, Integer> map = new HashMap<>();
-        for(int n : a){
-            map.put(n, map.getOrDefault(n, 0) + 1);
-        }
+        for(int i : a) map.put(i, map.getOrDefault(i, 0) + 1);
         
-        // 2개씩 묶어야함
         for(int key : map.keySet()){
             if(map.get(key) <= answer) continue;
             
             int count = 0;
-            for(int i = 0; i < a.length - 1; i++){
-                if((a[i] == key || a[i + 1] == key) && (a[i] != a[i + 1])){
-                    count++;
-                    i++;
+            for(int i = 0; i < len - 1; i++){
+                if((a[i] == key || a[i + 1] == key) && a[i] != a[i + 1]){
+                    count ++;
+                    i ++;
                 }
             }
             answer = Math.max(answer, count);
         }
+        
         return answer * 2;
     }
 }
