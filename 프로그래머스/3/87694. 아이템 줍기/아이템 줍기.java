d440ddbd22add81517eae2ceed627513c2a689b0
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-20 16:55:27 +0900

    [level 3] Title: 아이템 줍기, Time: 1.54 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -2,56 +2,49 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int answer = 0;
+        // 일단 테두리를 따라가면 안되니까 2배로 만들고 안에 비우기
         int[][] board = new int[102][102];
-        int[][] d = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        
         for(int[] r : rectangle){
-            int x1 = r[0] * 2;
-            int y1 = r[1] * 2;
-            int x2 = r[2] * 2;
-            int y2 = r[3] * 2;
+            int x1 = r[0] * 2; int y1 = r[1] * 2; int x2 = r[2] * 2; int y2 = r[3] * 2;
             
-            for(int i = x1; i <= x2; i++){
-                for(int j = y1; j <= y2; j++){
+            for(int i = x1; i < x2 + 1; i++){
+                for(int j = y1; j < y2 + 1; j++){
+                    board[i][j] = 2;
+                }
+            }
+        }
+        for(int[] r : rectangle){
+            int x1 = r[0] * 2; int y1 = r[1] * 2; int x2 = r[2] * 2; int y2 = r[3] * 2;
+            
+            for(int i = x1 + 1; i < x2; i++){
+                for(int j = y1 + 1; j < y2; j++){
                     board[i][j] = 1;
                 }
             }
         }
         
-        for(int[] r : rectangle){
-            int x1 = r[0] * 2;
-            int y1 = r[1] * 2;
-            int x2 = r[2] * 2;
-            int y2 = r[3] * 2;
-            
-            for(int i = x1 + 1; i < x2; i++){
-                for(int j = y1 + 1; j < y2; j++){
-                    board[i][j] = 2;
-                }
-            }
-        }
         
         Queue<int[]> q = new LinkedList<>();
-        boolean[][] v = new boolean[102][102];
         q.offer(new int[]{characterX * 2, characterY * 2, 0});
+        boolean[][] v = new boolean[102][102];
+        
+        int answer = 0;
+        int[][] d = {{0,1}, {0,-1}, {1,0}, {-1,0}};
         
         while(!q.isEmpty()){
             int[] now = q.poll();
-            int x = now[0];
-            int y = now[1];
-            int dist = now[2];
             
-            if(x == itemX * 2 && y == itemY * 2){return dist/ 2;}
+            if(now[0] == itemX * 2 && now[1] == itemY * 2) return now[2] / 2;
             
-            for(int i = 0; i < 4; i++){
-                int nx = x + d[i][0];
-                int ny = y + d[i][1];
-                
+            for(int i = 0; i < 4; i ++){
+                int nx = now[0] + d[i][0];
+                int ny = now[1] + d[i][1];
+            
                 if(nx < 0 || nx > 101 || ny < 0 || ny > 101) continue;
-                
-                if(!v[nx][ny] && board[nx][ny] == 1){
+                if(board[nx][ny] == 2 && !v[nx][ny]){
                     v[nx][ny] = true;
-                    q.offer(new int[]{nx, ny, dist + 1});
+                    q.offer(new int[]{nx, ny, now[2] + 1});
                 }
             }
             
