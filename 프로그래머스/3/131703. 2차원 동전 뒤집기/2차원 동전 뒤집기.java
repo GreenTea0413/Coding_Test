c9e62241ff57e6f6b984134abc54fac19f8c4202
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-23 15:44:15 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 0.05 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,57 +1,57 @@
 class Solution {
     public int solution(int[][] beginning, int[][] target) {
         int answer = Integer.MAX_VALUE;
+        
+        // 하나 뒤집으면 한번에 그 줄 전체를 뒤집는거임
+        // 1,0을 선택했고 가로로 하겠다고 했으면 ------ 이렇게다 돌리는거임
         int n = target.length;
         int m = target[0].length;
+        int[][] arr = new int[n][m];
         
-        int[][] diff = new int[n][m]; 
-        
-        // 뒤집어야 하는 칸들 고려
+        // 값이 서로 다른지를 파악해야함
+        // XOR로 값이 다른 위치에 1을 대입 같으면 0
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                diff[i][j] = beginning[i][j] ^ target[i][j];
-                
+                arr[i][j] = beginning[i][j] ^ target[i][j];
             }
         }
         
-        
-        // 행과 열을 만들어서 차이를 구하기
+        // 처음 위치에 0 과 1 둘 중 값을 넣어서 뒤집을 행과 열을 구하기
         for(int i = 0; i <= 1; i++){
             int[] row = new int[n];
             int[] col = new int[m];
             
-            // 행을 먼저 기준으로 하기 행을 안뒤집는다는 가정하에
             row[0] = i;
             
-            // 열은 차이와 row를 뒤집냐 안뒤집냐를 ^로 구하기
-            for(int y = 0; y < m; y++){
-                col[y] = diff[0][y] ^ row[0];
+            // row[0] = 0일 때 첫가로줄은 안 뒤집는거니까 거기에 맞게 col도 뒤집어야하는지 구하기
+            for(int j = 0; j < m; j++){
+                col[j] = arr[0][j] ^ row[0];
             }
             
-            
-            for(int x = 1; x < n; x++){
-                row[x] = diff[x][0] ^ col[0];
+            for(int k = 0; k < n; k++){
+                row[k] = arr[k][0] ^ col[0];
             }
             
-            boolean possible =true;
+            boolean possible = true;
             
             for(int a = 0; a < n; a++){
                 if(possible){
+                    // 아직까지 가능하다면 돌려야한다...
                     for(int b = 0; b < m; b++){
-                        if((row[a] ^ col[b]) != diff[a][b]){
+                        // 그리고 row랑 col을 돌리지 말지에 대해서 arr랑 같이 다르면 땡
+                        if((row[a] ^ col[b]) != arr[a][b]){
                             possible = false;
                             break;
                         }
                     }
                 }
             }
-            
             if(!possible) continue;
             int count = 0;
-            for(int r : row){count += r;}
-            for(int c : col){count += c;}
+            for(int r : row) count += r;
+            for(int c : col) count += c;
             
-            answer= Math.min(answer, count);
+            answer = Math.min(answer, count);
         }
         
         return answer == Integer.MAX_VALUE ? -1 : answer;
