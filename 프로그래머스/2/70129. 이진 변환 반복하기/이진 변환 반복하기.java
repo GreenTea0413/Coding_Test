d0ce25b09721f649e911c78ba47117ec79ad048a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-25 16:18:50 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.20 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,29 +2,28 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
         
-        while(!s.equals("1")){
-            answer[0] += 1;
-            int len = 0;
+        // answer[0] 은 0 제거하고 난 문자열 길이를 2진수로 바꾼 횟수
+        // answer[1] 은 0 제거된 횟수
+        while(true){
+            if(s.equals("1")) break;
+            int count = 0;
             
-            for(int i = 0; i < s.length(); i++){
-                if (s.charAt(i) == '0') answer[1] += 1;
-                else len += 1;
+            for(char c : s.toCharArray()){
+                if(c == '0') answer[1]++;
+                if(c == '1') count++;
             }
             
-            s = toStr(len);
+            // 0을 제외한 1의 나머지 길이만큼 2진으로 다시 나타내야함
+            // 길이가 4
+            String next = "";
+            while(count > 0){
+                int rest = count % 2;
+                next = String.valueOf(rest) + next;
+                count = count / 2;
+            }
+            s = next;
+            answer[0] ++;
         }
         return answer;
-    }
-    
-    public String toStr(int n){
-        String temp = "";
-        
-        while(n > 0){
-            int r = n % 2;
-            temp += String.valueOf(r);
-            n /= 2;
-        }
-        
-        return temp;
     }
 }
