c5d26d050af0b52d544776c03f2463eae82fbf79
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-25 20:17:14 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -5,14 +5,17 @@ class Solution {
         for(int i = 1; i < arr.length; i++){
             answer = lcm(arr[i], answer);
         }
+           
         return answer;
     }
     
-    public int lcm(int a, int b){
-        int num = gcd(a, b);
-        return a / num * b;
+    // 최소 공배수
+    public int lcm (int a, int b){
+        return a / gcd(a, b) * b;
     }
     
+    
+    // 최대 공약수 먼저 구하기
     public int gcd(int a, int b){
         if(b == 0) return a;
         return gcd(b, a % b);
