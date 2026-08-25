b8f81d61343bfeabc61eecf9bc9f0bf4e12c2271
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-25 19:41:35 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.06 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -2,12 +2,12 @@ class Solution {
     public long solution(int n) {
         long answer = 0;
         long[] dp = new long[n + 1];
-        if (n == 1) return 1;
-        
-        dp[0] = 1;
+        dp[0] = 0;
         dp[1] = 1;
         
-        for(int i = 2; i <= n; i++){
+        if(n == 1) return 1;
+        dp[2] = 2;
+        for(int i = 3; i < n + 1; i++){
             dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
         }
         return dp[n];
