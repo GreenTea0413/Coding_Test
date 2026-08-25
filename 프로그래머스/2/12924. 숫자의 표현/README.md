fd27d38da7d2ff6320e4e2de7552a3fd03c87a94
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-25 16:33:42 +0900

    [level 2] Title: 숫자의 표현, Time: 1.11 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
 
 ### 성능 요약
 
-메모리: 53.5 MB, 시간: 1.65 ms
+메모리: 57.5 MB, 시간: 1.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 14일 17:42:29
+2026년 08월 25일 16:33:38
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,17 +1,19 @@
 class Solution {
-    public int solution(int n) {        
-        // 본인 포함하기때문에 무조건 하나 있음
-        int answer = 1;
-        
-        // 본인 중간 이상보다 넘어서 시작하면 절대 안됨
-        for(int i = 1; i <= n / 2; i++){
-            int sum = i;
-            for(int j = i + 1; j <= n / 2 + 1; j++){
-                sum += j;
-                if (sum > n) break;
-                else if(sum == n) {answer++; break;}
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++){
+            // 1부터 시작해서 쭈우우욱 더하기
+            int count = i;
+            int inc = i + 1;
+            while(true){
+                if(count == n) {answer ++; break;}
+                else if(count > n) break;
+                count += inc;
+                inc++;
             }
         }
+        
         return answer;
     }
 }
