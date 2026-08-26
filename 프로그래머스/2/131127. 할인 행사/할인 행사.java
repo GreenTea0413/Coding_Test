9377a2579f5c3e3134acbf18dba1f01a927bbb0a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-26 16:54:04 +0900

    [level 2] Title: 할인 행사, Time: 38.70 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,29 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
+        // map을 통해서 각 범위 안에서 몇개씩 들어있는지 비교
+        // 그리고 하나씩꺼내서 비교했을 떄 number를 만족하지 못하면 continue        
+        // 시작점을 0부터 하고 9까지 갈 수 있는 범위를 정하기
         int len = discount.length;
-        
-        for(int i = 0 ; i < len - 9; i++){
-            int[] copy = number.clone();
-            //10일 연속으로 가야하니까 이렇게 끊고
-            for(int j = i; j < i + 10; j++){
-                for(int k = 0; k < want.length; k++){
-                    if(discount[j].equals(want[k])){
-                        copy[k] -= 1;
-                    }
-                }
+        int answer = 0;
+        for(int start = 0; start < len - 9; start++){
+            Map<String, Integer> map = new HashMap<>();
+            for(int i = start; i < start + 10; i++){
+                String d = discount[i];
+                map.put(d, map.getOrDefault(d, 0) + 1);
             }
             
             boolean check = true;
-            
-            for(int j = 0; j < copy.length; j++){
-                if(copy[j] > 0){check = false; break;}
-                if(copy[j] < 0){check = false; break;}
+            for(int j = 0; j < want.length; j++){
+                String w = want[j];
+                int n = number[j];
+                if(map.get(w) == null || map.get(w) != n) { check = false; break; }
             }
             
             if(check) answer++;
         }
-        
         return answer;
     }
 }
