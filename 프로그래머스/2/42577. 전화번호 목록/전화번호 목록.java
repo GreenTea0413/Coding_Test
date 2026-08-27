98a93efac7ba04e47d66668c9a71947d16879853
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-27 11:51:32 +0900

    [level 2] Title: 전화번호 목록, Time: 189.39 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,12 +4,13 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         int len = phone_book.length;
-        Arrays.sort(phone_book);
+        Arrays.sort(phone_book, (a, b) -> (a.compareTo(b)));
         
         for(int i = 0; i < len - 1; i++){
-            if(phone_book[i + 1].startsWith(phone_book[i])){
-               return false; 
-            }
+            if(phone_book[i + 1].startsWith(phone_book[i])) {
+                    answer = false;
+                    break; 
+            }    
         }
         return answer;
     }
