aa62c91bace1dcb8cfb722c835d6b8493866e389
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-29 22:34:30 +0900

    [level 2] Title: 튜플, Time: 50.76 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -3,25 +3,28 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         Map<Integer, Integer> map = new HashMap<>();
-        String temp = s.replace("},{", "a");
-        String[] numbers = temp.substring(2, temp.length() - 2).split("a");
+        s = s.replace("{{", "");
+        s = s.replace("}}", "");
+        s = s.replace("},{", "a");
         
-        for(int i = 0; i < numbers.length; i++){
-            String[] num = numbers[i].split(",");
-            for(int j = 0; j < num.length; j++){
-                int n = Integer.parseInt(num[j]);
-                map.put(n, map.getOrDefault(n, 0) + 1);    
+        String[] arr1 = s.split("a");
+        
+        for(String a : arr1){
+            String[] arr2 = a.split(",");
+            for(String b : arr2){
+                int n = Integer.parseInt(b);
+                map.put(n, map.getOrDefault(n, 0) + 1);
             }
         }
-        
-        List<Integer> answer = new ArrayList<>();
-        // 1 2 3 4 키를 가지고 이제 정렬
-        List<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, (a, b) ->{return map.get(b) - map.get(a);});
-        
-        for(int l : list) {
-            answer.add(l);
+        List<int[]> list = new ArrayList<>();
+        for(int key : map.keySet()){
+            list.add(new int[]{key, map.get(key)});
         }
-        return answer.stream().mapToInt(i -> i).toArray();
+        
+        list.sort((a, b) -> (b[1] - a[1]));
+        int[] answer = new int[map.size()];
+        int idx = 0;
+        for(int[] l : list) { answer[idx] = l[0]; idx++; }
+        return answer;
     }
 }
