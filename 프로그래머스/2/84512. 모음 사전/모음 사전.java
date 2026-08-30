dbbb36d2bef99146d67d827f2092d1740bc4c9db
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-30 15:30:55 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,16 +1,14 @@
 class Solution {
     public int solution(String word) {
         int answer = 0;
-        // 5 * 5 * 5 * 5 * 5
-        // 1 5 25 125 625
-        int[] alpabet = {781,156,31,6,1};
         String moum = "AEIOU";
+        // 625 125 25 5 1
+        int[] count = {781, 156, 31, 6, 1};
         
         for(int i = 0; i < word.length(); i++){
             int idx = moum.indexOf(word.charAt(i));
-            answer += alpabet[i] * idx + 1;
+            answer += count[i] * idx + 1;
         }
-        
         return answer;
     }
 }
