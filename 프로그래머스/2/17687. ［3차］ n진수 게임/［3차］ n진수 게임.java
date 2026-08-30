19c0a729091b975bb3a6d09c30391a6573fe1ddc
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-08-30 17:09:40 +0900

    [level 2] Title: [3차] n진수 게임, Time: 297.21 ms, Memory: 396 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,32 +1,32 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
+        // 미친건가? n진수 게임으로 이걸하네
+        // 내차례때 말해야하는 값을 말해야함
+        // 그러면 예를 들어 4번 말해야하고 사람이 2명이면 4턴가야함
+        // n진수에 대해서 8글자까지 나오려면 0 1 10 11 100 여기까지 나와야함
+        int len = m * t;
+        int count = 1;
+        
         String answer = "";
-        String number = "";
-        int len = t * m;
+        String str = "0";
         
-        int num = 0;
-        while(number.length() < len){
-            number += toNum(num, n);
-            num++;
+        while(str.length() < len){
+            int num = count;
+            String temp = "";
+            while(num > 0){
+                int rest = num % n;
+                char c = (rest >= 10) ? (char)('A' + rest - 10) : (char) ('0' + rest);
+                temp = c + temp;
+                num = num / n;
+            }
+            str += temp;
+            count++;
         }
         
-        for(int i = p - 1; i < len; i += m){
-            answer += number.charAt(i);
+        // 이제 여기서 str 완성되면 p번째 마다 말하도록 하기
+        for(int i = p; i <= len; i += m){
+            answer += "" + str.charAt(i - 1);
         }
-        
         return answer;
-    }
-    
-    public String toNum(int num, int n){
-        if (num == 0) return "0";
-        
-        String str = "";
-        while(num > 0){
-            int rest = num % n;
-            char c = (rest >= 10) ? (char)('A' + rest - 10) : (char)('0' + rest);
-            str = c + str;
-            num /= n;
-        }
-        return str;
     }
 }
