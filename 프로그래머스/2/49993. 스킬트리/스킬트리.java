4cd61ed165375582c921492fbcb01dd6a1af6cf8
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-01 15:43:22 +0900

    [level 2] Title: 스킬트리, Time: 0.06 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -3,30 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        String[] sk = skill.split("");
-        int len = skill_trees.length;
         
-        for(int i = 0; i < len; i++){
-            String[] st = skill_trees[i].split("");
-            
-            Queue<String> q = new LinkedList<>();
-            for(String k : sk){q.offer(k);}
-            
+        // CBD
+        // 스킬트리를 BACDE로 한다면? 안된다
+        // 만약에 CBD가 일단 Set에 넣어 놓고 CBADF랑 비교한다면
+        // C가 set에 있고 지금 idx = 0이라면 ㅇㅋ
+        // B가 set에 있고 지금 idx = 1이라면 ㅇㅋ
+        // A는 set에 없으니까 배우던 말던
+        Set<Character> set = new HashSet<>();
+        for(char c : skill.toCharArray()) set.add(c);
+        
+        
+        for(int i = 0; i < skill_trees.length; i++){
+            int idx = 0;
+            String s = skill_trees[i];
             boolean check = true;
-            for(String t : st){
-                if(!q.contains(t)) continue;
-                else{
-                    if(q.peek().equals(t)){
-                        q.poll();
-                    }
-                    else{
-                        check = false;
-                        break;
-                    }
+            // BACDE
+            for(char c : s.toCharArray()){
+                if(set.contains(c)){
+                    if(skill.charAt(idx) == c) idx++;
+                    else { check = false; break; }
                 }
             }
             if(check) answer++;
         }
+        
+        
+        
         return answer;
     }
 }
