a774d097f6545c24c473043b1c2c0abf8f574210
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-02 12:05:57 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 11.30 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,45 +1,56 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, String[] board) {
         int answer = 0;
-        char[][] map = new char[m][n];
-        for(int i = 0; i < m; i++){
-            map[i] = board[i].toCharArray();
-        }
+        Character[][] arr = new Character[m][n];
         
+        int idx = 0;
+        for(int x = 0; x < m; x++){
+            for(int y = 0; y < n; y++){
+                arr[x][y] = board[x].charAt(y);
+            }
+        }
+
+        // 블록 위치 하나당 (0,0) 이면 좌표 4개 검사
+        // 같은거면 위치 기억
         while(true){
-            boolean [][] v = new boolean[m][n];
+            boolean[][] v = new boolean[m][n];
             int count = 0;
-            
-            for(int i = 0; i < m - 1; i++){
-                for(int j = 0; j < n - 1; j++){
-                    char now = map[i][j];
-                    if(now == ' ') continue;
-                    
-                    if(now == map[i + 1][j] && now == map[i][j+1] && now == map[i+1][j+1]){
-                        v[i][j] = true;
-                        v[i][j + 1] = true;
-                        v[i + 1][j] = true;
-                        v[i + 1][j + 1] = true;
+            for(int x = 0; x < m - 1; x++){
+                for(int y = 0; y < n - 1; y++){
+                    if(arr[x][y] != '.'
+                       && arr[x][y] == arr[x][y + 1] 
+                       && arr[x][y] == arr[x + 1][y] 
+                       && arr[x][y] == arr[x + 1][y + 1]){
+                        v[x][y] = true;
+                        v[x][y+1] = true;
+                        v[x+1][y] = true;
+                        v[x+1][y+1] = true;
                     }
                 }
             }
             
-            for(int i = 0; i < m; i++){
-                for(int j = 0; j < n; j++){
-                    if(v[i][j]){
-                        map[i][j] = ' ';
-                        count++;
-                    }
+            // v에 true들 체크해서 바꿔줘야함
+            for(int x = 0; x < m; x++){
+                for(int y = 0; y < n; y++){
+                    if(v[x][y]) {arr[x][y] = '.'; count++;}
                 }
             }
             
-            for(int j = 0; j < n; j++){
-                for(int i = m - 1; i >= 0; i--){
-                    if(map[i][j] == ' '){
-                        for(int k = i - 1; k >= 0; k--){
-                            if(map[k][j] != ' '){
-                                map[i][j] = map[k][j];
-                                map[k][j] = ' ';
+            if(count == 0) break;
+            answer += count;
+            
+            // 이제 .으로 바꿨으니까 어떻게 내려와야할까
+            // 제일 밑부분부터 .이면 x - 1에 있는걸 아래로 내리기
+            for(int y = 0; y < n; y++){
+                for(int x = m - 1; x >= 0; x--){
+                    if(arr[x][y] == '.'){
+                        // 여기서 단순하게 한칸만 내리면 안됨
+                        for(int z = x - 1; z >= 0; z--){
+                            if(arr[z][y] != '.'){
+                                arr[x][y] = arr[z][y];
+                                arr[z][y] = '.';
                                 break;
                             }
                         }
@@ -47,10 +58,8 @@ class Solution {
                 }
             }
             
-            if(count == 0) break;
-            answer += count;
+            
         }
-        
         
         return answer;
     }
