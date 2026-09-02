eddc1381048169b6e3094326cf125942dce5d94f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-02 12:45:48 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 75.50 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -3,43 +3,52 @@ import java.util.*;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         long answer = 0;
+        // 다 합치면 30이고
+        // 하나당 15가 되어야함
+        Queue<Long> q1 = new LinkedList<>();
+        Queue<Long> q2 = new LinkedList<>();
+        
         int len = queue1.length;
         long sum1 = 0;
         long sum2 = 0;
         long total = 0;
-        
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        for(int i = 0; i < len; i++){
-            q1.offer(queue1[i]);
-            q2.offer(queue2[i]);
-            sum1 += queue1[i];
-            sum2 += queue2[i];
-            total += queue1[i] + queue2[i];
+        for(int i = 0; i < len; i++) {
+            long n1 = queue1[i];
+            long n2 = queue2[i];
+            
+            q1.offer(n1);
+            q2.offer(n2);
+            
+            sum1 += n1;
+            sum2 += n2;
         }
+        total = sum1 + sum2;
         
         if(total % 2 != 0) return -1;
         if(sum1 == sum2) return 0;
-        
+            
         while(answer < len * 2){
+            // 큐1이 더 크면?
             while(!q1.isEmpty() && sum1 > sum2){
-                int n = q1.poll();
+                long n = q1.poll();
                 sum1 -= n;
                 sum2 += n;
                 q2.offer(n);
                 answer++;
             }
-            while(!q2.isEmpty() && sum1 < sum2){
-                int n = q2.poll();
+            while(!q2.isEmpty() && sum2 > sum1){
+                long n = q2.poll();
                 sum1 += n;
                 sum2 -= n;
                 q1.offer(n);
                 answer++;
             }
             
-            if(sum1 == sum2) {return (int) answer;}
+            if(sum1 == sum2) return (int) answer;
             
         }
-        return -1;
+        
+        
+        return (int) -1;
     }
 }
