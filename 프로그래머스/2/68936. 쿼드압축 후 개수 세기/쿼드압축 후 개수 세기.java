9ff480fddda8dd7503ec1f8c0be8633a3de3ec67
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-03 11:35:16 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 12.07 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,36 +1,37 @@
 class Solution {
+    int[] answer = new int[2];
     public int[] solution(int[][] arr) {
-        // 1 / 2 씩 가로 세로 잘라서 4등분 한 다음 0, 1비교
+        // 일단 전체를 더했을 때 칸 수와 같지않다면? 나눠야함
+        // 그게 아니라면 해당 값이 0이면 answer[0]++, 다시 칸수와 같다면? answer[1] ++;
         int len = arr.length;
-        return compress(arr, 0, 0, len);
-    }
-    
-    // 여기서 이제 네모가 오면 처음부터 시작점에서부터 len길이만큼 전체 합을 구하기
-    // - 0이면 0 max면 1
-    public int[] compress(int[][] arr, int x, int y, int len){
-        int max = len * len;
-        int sum = 0;
-        for(int i = x; i < x + len; i++){
-            for(int j = y; j < y + len; j++){
-                sum += arr[i][j];
-            }
-        }
-        
-        if(sum != 0 && sum != max) return qurt(arr, x, y, len / 2);
-        return (sum == max) ? new int[]{0, 1} : new int[]{1, 0};
-    }
-    
-    public int[] qurt(int[][] arr, int x, int y, int half){
-        int[] answer = new int[2];
-        
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 2; j++){
-                int[] result = compress(arr, x + half * i, y + half * j, half);
-                answer[0] += result[0];
-                answer[1] += result[1];
-            }
-        }
+        calculate(arr, len, 0, 0);
         
         return answer;
     }
+    
+    void calculate(int[][] arr, int len, int x, int y){
+        int sum = 0;
+        for(int i = x; i < x + len; i++){
+            for(int j = y; j < y + len; j++){
+                sum+= arr[i][j];
+            }
+        }
+        
+        if(sum == 0) answer[0]++;
+        else if(sum == len * len) answer[1]++;
+        else{
+            // 여기서는 이제 절반으로 나눠서 보내야함
+            // len / 2를 기점으로 다시 점검하도록 해야함
+            quater(arr, len / 2, x, y);
+        }
+    }
+    
+    void quater(int[][] arr, int half, int x, int y){
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                calculate(arr, half, x + half * i, y + half * j);
+            }
+        }
+    }
+    
 }
