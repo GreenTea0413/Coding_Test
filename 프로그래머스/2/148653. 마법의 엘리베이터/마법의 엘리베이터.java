37b5fe694447e198166ed564be86f0723a4a6a26
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-03 12:42:48 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -2,21 +2,28 @@ class Solution {
     public int solution(int storey) {
         int answer = 0;
         
+        // 1, -1, 10, -10 처럼 엘리베이터에 대해서 딱 나눠 떨어지면 버튼 횟수를 줄일 수 있음
         while(storey > 0){
             int rest = storey % 10;
-            
-            if(rest > 5){
+            // 나머지가 5보다 크면 10이랑 더 가까우니까 storey를 더해주기
+            // 그리고 다음 자릿수로 가야하니까 /10으로 나눠주면 됨
+            if(rest > 5) {
                 answer += 10 - rest;
                 storey = (storey / 10) + 1;
             }
-            else if(rest < 5){
+            else if(rest < 5) {
                 answer += rest;
                 storey = (storey / 10);
             }
-            // 5일때는 앞자리 보고 내리던지 올리던지 해야함
+            // 예외로 만약에 5랑 같다면? 
             else{
+                // 그럴 경우 95면 100으로 만드는게 훨씬 이득임
+                // 그래서 앞자리를 알아야함
+                // 95 / 10 % 10 이여서 나머지는 9;
                 int front = (storey / 10) % 10;
-                if(front >= 5){
+                
+                // 50이상이면 무조건 5 더하는게 이득
+                if(front >= 5) {
                     answer += 5;
                     storey = (storey / 10) + 1;
                 }
@@ -26,7 +33,6 @@ class Solution {
                 }
             }
         }
-         
         return answer;
     }
 }
