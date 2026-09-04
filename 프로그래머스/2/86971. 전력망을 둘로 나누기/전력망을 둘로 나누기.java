be29f362d15732dd81c6dfdba0872b9cbe470b5e
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-04 16:12:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,53 +4,51 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i <= n; i++) graph.add(new ArrayList<>());
         
-        // 그래프 만들기
-        for(int i = 0; i <= n; i++){graph.add(new ArrayList<>());}
         for(int[] w : wires){
             graph.get(w[0]).add(w[1]);
             graph.get(w[1]).add(w[0]);
         }
         
-        // 그래프 선 하나씩 끊기
+        // 그래프를 만들었으면 이제 wires에 있는거 하나씩 없애고 나서 bfs로 갯수 구할거임
         for(int[] w : wires){
-            int a = w[0];
-            int b = w[1];
+            int a = w[0]; int b = w[1];
             
             graph.get(a).remove(Integer.valueOf(b));
             graph.get(b).remove(Integer.valueOf(a));
             
-            int countA = bfs(graph, a, new boolean[n + 1]);
-            int countB = bfs(graph, b, new boolean[n + 1]);
-            int gap = Math.abs(countA - countB);
+            // 이렇게 없애고 나서
+            int num1 = bfs(graph, new boolean[n + 1] , a);
+            int num2 = n - num1;
+            int gap = Math.abs(num2 - num1);
+            
+            answer = Math.min(answer, gap);
             
             graph.get(a).add(b);
             graph.get(b).add(a);
-               
-            answer = Math.min(answer, gap);
         }
         
         return answer;
     }
     
-    public int bfs(List<List<Integer>> graph, int start, boolean[] v){
+    int bfs(List<List<Integer>> graph, boolean[] v, int start){
         Queue<Integer> q = new LinkedList<>();
         q.offer(start);
         v[start] = true;
-        int count = 1;
         
+        int count = 1;
         while(!q.isEmpty()){
             int now = q.poll();
             
             for(int next : graph.get(now)){
                 if(!v[next]){
                     v[next] = true;
-                    count++;
                     q.offer(next);
+                    count++;
                 }
             }
         }
-        
         return count;
     }
 }
