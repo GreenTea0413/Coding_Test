5a9619c27ffa1e9490058db8f5e50b63436046ea
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-10 11:02:18 +0900

    [level 2] Title: 124 나라의 숫자, Time: 1.84 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,11 +1,15 @@
+import java.util.*;
+
 class Solution {
     public String solution(int n) {
         String answer = "";
-        // 11 42 12 44 13 111
+        
+        // 1 2 4 밖에 못씀
+        // 1 2 4 11 12 14 -> 나눴을 때 나머지가 0 1 2에 따라 4 1 2 부여해서 계속 쌓아가면 될듯
         while(n > 0){
             int rest = n % 3;
             
-            if (rest == 0){
+            if(rest == 0){
                 answer = "4" + answer;
                 n = n / 3 - 1;
             }
@@ -13,7 +17,7 @@ class Solution {
                 answer = "1" + answer;
                 n /= 3;
             }
-            else {
+            else{
                 answer = "2" + answer;
                 n /= 3;
             }
