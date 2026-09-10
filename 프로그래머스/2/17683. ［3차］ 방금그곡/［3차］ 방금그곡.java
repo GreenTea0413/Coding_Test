7905a64d99d90c757689ed9f680b6aa7f66645ad
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-10 14:24:38 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.57 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,58 +1,65 @@
 import java.util.*;
+
 class Solution {
     public String solution(String m, String[] musicinfos) {
         String answer = "(None)";
-        int maxTime = -1;
-        int len = musicinfos.length;
-        String[][] music = new String[len][4];
-        // # 붙은거 소문자로 처리
+        // C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+        // #들어가있으면 문자 비교하기 어려우니까 소문자로 바꾸기
+
+        // #까지 잘 봐야하는 문제
         m = convert(m);
         
-        for(int i = 0; i < len; i++){
-            String[] temp = musicinfos[i].split(",");
-            for(int j = 0; j < 4; j++){
-                music[i][j] = temp[j];
-            }
-        }
-        
-        Arrays.sort(music, (a, b) -> a[0].compareTo(b[0]));
-        
-        for(String[] arr : music){
-            int start = toNum(arr[0]);
-            int end = toNum(arr[1]);
-            String name = arr[2];
-            String melody = arr[3];
-            melody = convert(melody);
+        int max = -1;
+        for(String mu : musicinfos){
+            // 시작, 끝, 제목, 코드
+            String[] s = mu.split(",");
+            int start = toMin(s[0]);
+            int end = toMin(s[1]);
+            String title = s[2];
+            String melody = convert(s[3]);
             
-            int gap = end - start;
-            int ml = melody.length();
-
             StringBuilder sb = new StringBuilder();
-
-            for(int i = 0; i < gap; i++){
-                sb.append(melody.charAt(i % ml));
-            }
-
-            String tm = sb.toString();
-            if(maxTime < gap && tm.contains(m)){
-                maxTime = gap;
-                answer = name;
+            for(int i = 0; i < end - start; i++) sb.append(melody.charAt(i % melody.length()));
+            
+            // 비교를 처음부터 해도 되는가?
+            int len = m.length();
+            if(len > sb.length()) continue;
+            
+            // 이제 여기서 #비교를 해야함
+            if(sb.toString().contains(m)) {
+                if(end - start > max) {
+                    max = end - start;
+                    answer = title;
+                }   
             }
         }
-        
         return answer;
     }
     
-    public int toNum(String time){
-        String[] arr = time.split(":");
-        return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
+    int toMin(String str){
+        String[] s = str.split(":");
+        return Integer.parseInt(s[0]) * 60 + Integer.parseInt(s[1]);
     }
     
-    public String convert(String s){
-        return s.replace("C#", "c")
-                .replace("D#", "d")
-                .replace("F#", "f")
-                .replace("G#", "g")
-                .replace("A#", "a");
+    String convert(String str){
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < str.length() - 1; i++){
+            if(str.charAt(i) == '#') continue;
+            
+            if(str.charAt(i + 1) == '#'){
+                if(str.charAt(i) == 'A') sb.append('a');
+                if(str.charAt(i) == 'C') sb.append('c');
+                if(str.charAt(i) == 'D') sb.append('d');
+                if(str.charAt(i) == 'E') sb.append('e');
+                if(str.charAt(i) == 'F') sb.append('f');
+                if(str.charAt(i) == 'G') sb.append('g');
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+        if(str.charAt(str.length() - 1) != '#') sb.append(str.charAt(str.length() - 1));
+        return sb.toString();
     }
 }
