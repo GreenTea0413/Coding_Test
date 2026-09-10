c24418c9a0bbfbfaab16ce67e32879ffc6fc627a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-10 11:50:45 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 11.94 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,47 +1,47 @@
 import java.util.*;
 
 class Solution {
-    public Map<String, Integer> map;
+    Map<String, Integer> map = new HashMap<>();
+    
     public String[] solution(String[] orders, int[] course) {
         List<String> answer = new ArrayList<>();
-        map = new HashMap<>();
-        // 메뉴가 2개 이상이여야함
-        // 문자 길이가 같다면 횟수가 더 많은 걸로 하기
-        // 이걸로 코스 길이만큼 문자열 만들어 내기
-        for(String order : orders){
-            char[] arr = order.toCharArray();
-            Arrays.sort(arr);
+        
+        // 이걸로 coruse 길이에 맞는 세트들만 만듬
+        for(String ord : orders) {
+            char[] o = ord.toCharArray();
+            Arrays.sort(o);
+            
             for(int c : course){
-                combine(arr, "", 0, c);
-            } 
+                dfs(o, 0, "", c);   
+            }
+        }
+    
+        // map에서 이제 value로 길이마다 가장 큰 값 찾기 course랑 같은 애들만 answer에 담기
+        Map<Integer, Integer> max = new HashMap<>();
+        
+        // 이러면 이제 2개 이상 팔린 것 중에서 길이에 대해 최대값들을 넣음
+        for(String key : map.keySet()){
+            if(map.get(key) >= 2) {
+                max.put(key.length(), Math.max(max.getOrDefault(key.length(), 0), map.get(key)));
+            }
         }
         
-        for(int c : course){
-            int max = 1;
-            for(Map.Entry<String, Integer> entry : map.entrySet()){
-                if (entry.getKey().length() == c){
-                    max = Math.max(max, entry.getValue());
-                }
-            }
+        for(String key : map.keySet()){
+            int len = key.length();
+            int count = map.get(key);
             
-            for(Map.Entry<String, Integer> entry : map.entrySet()){
-                if(entry.getKey().length() == c && max == entry.getValue() && max >= 2){
-                    answer.add(entry.getKey());
-                }
-            }
+            if(count >= 2 && max.get(len) == count) answer.add(key);
         }
         
         Collections.sort(answer);
         return answer.toArray(new String[0]);
     }
-    
-    public void combine(char[] arr, String now, int start, int depth){
-        if(depth == 0){
+    void dfs(char[] c, int start, String now, int depth){
+        if(depth == 0) {
             map.put(now, map.getOrDefault(now, 0) + 1);
             return;
         }
-        for(int i = start; i < arr.length; i++){
-            combine(arr, now + arr[i], i + 1, depth -1);
-        }
+        
+        for(int i = start; i < c.length; i++) dfs(c, i + 1, now + c[i], depth - 1);
     }
 }
