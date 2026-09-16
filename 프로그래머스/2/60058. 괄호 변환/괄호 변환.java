3b1f6f51cf7234f58f467c9e87d0375b404fcd55
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-16 15:31:40 +0900

    [level 2] Title: 괄호 변환, Time: 5.04 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,49 +1,59 @@
+import java.util.*;
+
 class Solution {
     public String solution(String p) {
         if(p.equals("")) return "";
-        
         String answer = "";
+        
+        // '(' 와 ')' 의 개수는 항상 같습니다.
+        // 그러면 처음에 ( 과 )의 수를 세서 같으면 짤라내기? 어차피 얘네는 뭘 해도 상관없으니까
+        // 그리고 일단 갯수만 먼저 맞추면 올바르다고 판단하는듯
         int left = 0;
         int right = 0;
         int idx = 0;
         for(int i = 0; i < p.length(); i++){
             if(p.charAt(i) == '(') left++;
             else right++;
-            if(left == right){
+            
+            if(left == right) {
                 idx = i;
                 break;
             }
         }
         
+        // 여기서 이제 ( ) 이거 서로 수 맞춰줬으니까 잘라
         String u = p.substring(0, idx + 1);
         String v = p.substring(idx + 1);
         
-        // 앞에 자른게 올바른거면 이렇게 다시 시작
-        if(isCheck(u)){
-            return u + solution(v);
-        }
+        // u 가 이제 올바른지 판단해야함
+        // 맞음녀 v만 다시 돌리라고 함
+        // 근데 아니면 앞에 ( + V +)로 돌려야함
+        if(check(u)) return u + solution(v);
         
-        // 아니면
-        String temp = "(" + solution(v) + ")";
+        String next = "(" + solution(v) + ")";
         
-        // u 앞뒤 제거 + 뒤집기
         StringBuilder sb = new StringBuilder();
+        // 그리고 u의 첫번째와 마지막을 제거하고 나머지는 다 뒤집어서 넣고
+        // 그거를 next뒤에 붙이기
         for(int i = 1; i < u.length() - 1; i++){
             if(u.charAt(i) == '(') sb.append(')');
             else sb.append('(');
         }
-
-        return temp + sb.toString();
+        
+        return next + sb.toString();
     }
     
-    public boolean isCheck(String u){
-        int count = 0;
-        for(char c : u.toCharArray()){
-            if(c == '(') count++;
-            else count--;
-            if(count < 0) return false;
-        }
+    boolean check(String u){
+        // 여기서 확인하기
+        Stack<Character> stack = new Stack<>();
         
-        return count == 0;
+        for(char c : u.toCharArray()){
+            if(c == ')'){
+                if(stack.isEmpty()) return false;
+                else if(!stack.isEmpty() && stack.peek() == '(') stack.pop();
+            }
+            else stack.push('(');
+        }
+        return true;
     }
 }
