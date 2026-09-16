1cc5ad9ca2572da2f6d241fc7233425f401f1410
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-16 15:08:30 +0900

    [level 2] Title: 무인도 여행, Time: 8.29 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,43 +1,59 @@
 import java.util.*;
+
 class Solution {
+    int[][] d = {{0,1}, {0,-1}, {1,0},{-1,0}};
+    int[][] board;
+    boolean[][] v;
+    int xLen;
+    int yLen;
     public int[] solution(String[] maps) {
-        int lenX = maps.length;
-        int lenY = maps[0].length();
-        List<Integer> answer = new ArrayList<>();
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] v = new boolean[lenX][lenY];
-        int[][] d = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        xLen = maps.length;
+        yLen = maps[0].length();
+        board = new int[xLen][yLen];
+        v = new boolean[xLen][yLen];
         
-        
-        for(int i = 0; i < lenX; i++){
-            for(int j = 0; j < lenY; j++){
-                if (maps[i].charAt(j) != 'X' && !v[i][j]){
-                    q.offer(new int[]{i, j});
-                    v[i][j] = true;
-                    int sum = 0;
-                    while(!q.isEmpty()){
-                        int[] now = q.poll();
-                        int x = now[0];
-                        int y = now[1];
-                        sum += maps[x].charAt(y) - '0';
-                        
-                        for(int k = 0; k < 4; k ++){
-                            int nx = x + d[k][0];
-                            int ny = y + d[k][1];
-                            
-                            if(0<= nx && nx< lenX && 0 <= ny && ny < lenY && !v[nx][ny] && maps[nx].charAt(ny) != 'X'){
-                                v[nx][ny] = true;
-                                q.offer(new int[]{nx, ny});
-                            }
-                        }
-                    }
-                    answer.add(sum);
-                }
+        int count = 0;
+        // X는 0으로 유지하고 나머지는 숫자로 넣기
+        for(int i = 0; i < xLen; i++){
+            for(int j = 0; j < yLen; j++){
+                char m = maps[i].charAt(j);
+                
+                if(m == 'X') {count++; continue;}
+                board[i][j] = Integer.parseInt(String.valueOf(m));
             }
         }
         
-        if (answer.size() == 0) return new int[]{-1};
+        if(count == xLen * yLen) return new int[]{-1};
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        
+        for(int i = 0; i < xLen; i++){
+            for(int j = 0; j < yLen; j++){
+                if(board[i][j] != 0 && !v[i][j]){
+                    answer.add(dfs(i, j));
+                }
+            }
+        }
         Collections.sort(answer);
         return answer.stream().mapToInt(i -> i).toArray();
+    }
+    
+    int dfs(int x, int y){
+        int sum = board[x][y];
+        v[x][y] = true;
+    
+        for(int i = 0; i < 4; i++){
+            int nx = x + d[i][0];
+            int ny = y + d[i][1];
+            
+            if(nx < 0 || nx >= xLen || ny < 0 || ny >= yLen) continue;
+            if(!v[nx][ny] && board[nx][ny] != 0){
+                v[nx][ny] = true;
+                sum += dfs(nx, ny);
+            }
+        }
+        
+        return sum;
     }
 }
