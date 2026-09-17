76938f60464d33cedee583f6b03121c2942e68ef
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-17 17:15:31 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,74 +1,71 @@
 class Solution {
+    int[][] d = {{0, 1}, {0,-1}, {1,0},{-1,0}};
+    int[][] dt = {{1,1}, {-1,-1},{1,-1},{-1,1}};
+    
     public int[] solution(String[][] places) {
-        int[] answer = {1,1,1,1,1};
+        int len = places.length;
+        int[] answer = new int[len];
         
-        for(int i = 0; i < 5; i++){
-            // 여기서 하나씩 시작
-            String[] place = places[i];
+        // 5 * 5
+        // 좌표 비교했을 때 |r1 - r2| + |c1 - c2|
+        // 기존의 상하좌우로 거르고
+        // 대각선들은 있으면 거기에 맞게 x, y -> x + 1, y + 1에 사람있으면 (x ,y + 1) ,(x + 1, y)가 X인지
+        for(int i = 0; i < len; i++){
             boolean check = true;
-            for(int j = 0; j < 5; j++){
-                for(int k = 0; k < 5; k++){
-                    // 여기서 이제 탐색을 하는데 탐색했을 때 P 8방향으로 비교
-                    // 비교 했을 때 하나라도 P 걸리는거 있으면 false 반환
-                    if(place[j].charAt(k) == 'P'){
-                        if (!isCheck(place, j, k)){
-                            check = false;
-                            break;
-                        }  
-                    }
-                }
-                if (!check) {
-                    answer[i] = 0;
-                    break;
-                }
-            }
-        }
-        return answer;
-    }
-    int[][] d = {{1,0}, {-1,0}, {0,1}, {0,-1},{1,1},{1,-1},{-1,1},{-1,-1}, {2,0}, {0,2}, {-2,0}, {0,-2}};
-    public boolean isCheck(String[] place, int x, int y){
-        for(int i = 0; i < 12; i++){
-            int nx = x + d[i][0];
-            int ny = y + d[i][1];
-            if(0 <= nx && nx < 5 && 0 <= ny && ny < 5){
-                if(place[nx].charAt(ny) == 'P'){
-                    // 대각선이 아닌 곳에서 P가 나오면 바로 false
-                    if(i < 4) return false;
+            char[][] arr = new char[5][5];
+            
+            for(int j = 0; j < 5; j++) arr[j] = places[i][j].toCharArray();
+            
+            // arr에 이제 다 담았으면 시작하기
+            for(int x = 0; x < 5; x++){
+                if(!check) break;
+                for(int y = 0; y < 5; y++){
+                    // 여기서 이제 주변 검사하는거임
+                    if(!check) break;
                     
-                    // 대각선이라면 테스트 해봐야함
-                    // 오른쪽 아래
-                    if (i == 4) {
-                        if (place[nx - 1].charAt(ny) != 'X' || place[nx].charAt(ny - 1) != 'X'){
-                            return false;
+                    if(arr[x][y] == 'P'){
+                        for(int k = 0; k < 4; k++){
+                            int nx = x + d[k][0];
+                            int ny = y + d[k][1];
+                            
+                            if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+                            
+                            if(arr[nx][ny] == 'P') {
+                                check = false;
+                                break;
+                            }
+                            
+                            int dx = nx + d[k][0];
+                            int dy = ny + d[k][1];
+                            
+                            if(dx < 0 || dx >= 5 || dy < 0 || dy >= 5) continue;
+                            
+                            if(arr[dx][dy] == 'P' && arr[nx][ny] != 'X') {
+                                check = false;
+                                break;
+                            }
                         }
-                    }
-                    // 왼쪽 아래
-                    if (i == 5) {
-                        if (place[nx - 1].charAt(ny) != 'X' || place[nx].charAt(ny + 1) != 'X'){
-                            return false;
+                        
+                        for(int k = 0; k < 4; k++){
+                            int nx = x + dt[k][0];
+                            int ny = y + dt[k][1];
+                            
+                            if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+                            
+                            if(arr[nx][ny] == 'P'){
+                                if(arr[nx][y] != 'X' || arr[x][ny] != 'X'){
+                                    check = false;
+                                    break;
+                                }
+                            }
                         }
-                    }
-                    // 오른쪽 위
-                    if(i == 6){
-                        if (place[nx].charAt(ny - 1) != 'X' || place[nx + 1].charAt(ny) != 'X'){
-                            return false;
-                        }
-                    }
-                    // 왼쪽 위
-                    if(i == 7){
-                        if (place[nx + 1].charAt(ny) != 'X' || place[nx].charAt(ny + 1) != 'X'){
-                            return false;
-                        }
-                    }
-                    // 직선 거리 2
-                    if(i > 7){
-                        int tx = x + d[i][0] / 2;
-                        int ty = y + d[i][1] / 2;
-                        if(place[tx].charAt(ty) != 'X') return false;
                     }
                 }
             }
+            if(check) answer[i] = 1;
+            else answer[i] = 0;
         }
-        return true;
+        
+        return answer;
     }
 }
