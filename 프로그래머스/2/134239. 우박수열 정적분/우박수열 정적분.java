153cad2d21e47c0eebc9a763505908917f6e44cb
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-17 18:54:01 +0900

    [level 2] Title: 우박수열 정적분, Time: 1.99 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -1,40 +1,47 @@
 import java.util.*;
+
 class Solution {
     public double[] solution(int k, int[][] ranges) {
-        // (0, 5), (1, 16), (2, 8), (3, 4), (4, 2), (5, 1)
-        List<Integer> list = new ArrayList<>();
-        list.add(k);
+        int n = 0;
+        int len = ranges.length;
+        double[] answer = new double[len];
+        
+        List<int[]> dots = new ArrayList<>();
+        
         while(k > 1){
-            if (k % 2 == 0) { k /= 2;}
-            else {k = k * 3 + 1;}
-            list.add(k);
+            dots.add(new int[]{n, k});
+            n++;
+            
+            if(k % 2 == 0) k =  k / 2;
+            else k = 3 * k + 1;
+        }
+        dots.add(new int[]{n, 1});
+        
+        // x에 대한 어떤 범위 [a, -b]
+        // ranges -> [0,0] -> [0, 0] ~ [0, n - 0]까지의 넓이
+        // 매번 계산하기 어려우니 미리 구하기
+        // 0,0 ~ 해당 좌표들까지의 너비 구하기 가로 한칸씩 늘려가면서 하기
+        // 0,5 0,0 1,0 1,16 / 1,0 1,16 2,0 2,5
+        double[] space = new double[n + 1];
+        for(int i = 1; i <= n; i++){
+            double y = dots.get(i - 1)[1];
+            double ny = dots.get(i)[1];
+            
+            space[i] = (ny + y) / 2;
         }
         
-        // 미리 모든 한칸한칸 구간의 넓이를 구하기
-        // 0 ~ 1, 1 ~ 2, 2 ~ 3, 3 ~ 4, 4 ~ 5
-        int n = list.size() - 1;
-        double[] sqr = new double[n];
-        for(int i = 0; i < n; i++){
-            sqr[i] = (list.get(i)+ list.get(i + 1)) / 2.0;
-        }
-        
-        // 그 다음 범위에 대한 넓이 구하기
-        // 1 -> 0 ~ 1 2 -> 0 ~ 2...
-        double[] total_sqr = new double[n + 1];
-        for(int i = 0; i < n; i++){
-            total_sqr[i + 1] = total_sqr[i] + sqr[i];
-        }
-        
-        // x = a, x = n - b, y = 0
-        double[] answer = new double[ranges.length];
-        for(int i = 0; i < ranges.length; i++){
+        // 그 다음 이제 범위 내에서 값 구하기
+        for(int i = 0; i < len; i++){
+            double sum = 0;
             int a = ranges[i][0];
             int b = n + ranges[i][1];
             
             if(a > b) answer[i] = -1;
-            else {answer[i] = total_sqr[b] - total_sqr[a];}
+            else{
+                for(int j = a + 1; j <= b; j++) sum += space[j];
+                answer[i] = sum;
+            }
         }
-        
         return answer;
     }
 }
