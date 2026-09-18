896282b64ef214ae803f89fa385faef5d6e50f8a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-19 02:06:20 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,15 +1,8 @@
 -- 코드를 입력하세요
--- 중고 거래 게시물을 3건 이상 등록한
-SELECT user_id, nickname, CONCAT(city, ' ', street_address1, ' ', street_address2)  as '전체주소', CONCAT(
-    SUBSTRING(TLNO, 1, 3), '-',
-    SUBSTRING(TLNO, 4, 4), '-',
-    SUBSTRING(TLNO, 8, 4)
-)  as '전화번호'
-from USED_GOODS_USER
-where user_id in (
-    select WRITER_ID
-    from USED_GOODS_BOARD
-    group by WRITER_ID
-    having count(*) >= 3
-)
-order by user_id DESC;
+SELECT b.WRITER_ID as USER_ID, u.NICKNAME,	
+concat(u.CITY,' ',u.STREET_ADDRESS1,' ',u.STREET_ADDRESS2) as 전체주소, 
+concat(substring(u.TLNO,1,3),'-',substring(u.TLNO, 4,4),'-',substring(u.TLNO, 8,4)) as 전화번호
+from USED_GOODS_BOARD b join USED_GOODS_USER u on b.writer_id = u.user_id
+group by b.WRITER_ID
+having count(*) >= 3
+order by b.WRITER_ID desc;
