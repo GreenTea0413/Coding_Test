ca84a79b834eca277b51e0249d21520d1e197a39
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-19 01:08:59 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,9 +1,6 @@
 -- 코드를 입력하세요
-
-SELECT i.animal_id, i.name
-from animal_ins i
-join animal_outs o
-on i.animal_id = o.animal_id
-order by o.datetime - i.datetime desc
+SELECT i.ANIMAL_ID, i.NAME
+from ANIMAL_INS i left join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
+where o.ANIMAL_ID is not null
+order by o.DATETIME - i.DATETIME DESC
 limit 2;
-
