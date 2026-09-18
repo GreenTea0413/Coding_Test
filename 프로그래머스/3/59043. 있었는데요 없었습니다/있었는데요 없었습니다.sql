2a75a80e94c7bc617a6ad232494ba111c6ec459d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-19 00:42:12 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
-SELECT i.ANIMAL_ID, i.NAME
-from animal_ins i
-join animal_outs o
-on i.animal_id = o.animal_id
-where i.datetime >= o.datetime
-order by i.datetime;
+-- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문
+SELECT o.ANIMAL_ID, o.NAME
+from ANIMAL_OUTS o join ANIMAL_INS i on o.ANIMAL_ID = i.ANIMAL_ID
+where o.DATETIME < i.DATETIME
+order by i.DATETIME;
+
