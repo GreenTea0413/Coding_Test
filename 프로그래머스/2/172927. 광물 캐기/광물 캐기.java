e726cb94f26a5f29458c4afb03d52705ba4cfbd2
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-18 16:21:27 +0900

    [level 2] Title: 광물 캐기, Time: 5.18 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,44 +1,44 @@
 class Solution {
-    int answer = 1250;
+    int answer = Integer.MAX_VALUE;
     public int solution(int[] picks, String[] minerals) {
         mine(picks, minerals, 0, 0);
+        
         return answer;
     }
     
-    // 곡괭이로 땅파기
-    // 현재 곡괭이 값에 따른 minerals * 피로도 해서 다음으로 넘기기
-    
-    public void mine(int[] picks, String[] minerals, int total, int now){
-        if (picks[0] + picks[1] + picks[2] == 0 || now >= minerals.length){
-            answer = Math.min(total, answer);
+    void mine(int[] picks, String[] minerals, int now, int total){
+        if(picks[0] + picks[1] + picks[2] == 0 || now >= minerals.length){
+            answer = Math.min(answer, total); 
             return;
         }
+        // picks 처음부터 돌려서 minerals를 하나씩 캐면됨
+        // minerals의 순서를 바꾸는건 아닌거 같
+        // i 0번째 다이아, 1번째 철, 2번째     
         
         for(int i = 0; i < 3; i++){
             if(picks[i] != 0){
-                picks[i]--;
-                int cost = 0;
+                int sum = 0;
+                picks[i] --;
                 int size = Math.min(now + 5, minerals.length);
                 
                 for(int j = now; j < size; j++){
-                    // 곡괭이
-                    if (i == 0) {
-                        cost += 1;
-                    }
+                    // 다이아로 캘때
+                    if(i == 0) sum++;
+                    // 철로
                     else if(i == 1){
-                        // 광물
-                        if (minerals[j].equals("diamond")) cost += 5;
-                        else cost += 1;
+                        if(minerals[j].equals("diamond")) sum += 5;
+                        else sum++;
                     }
+                    //돌로
                     else{
-                        if (minerals[j].equals("diamond")) cost += 25;
-                        else if (minerals[j].equals("iron")) cost += 5;
-                        else cost += 1;
+                        if(minerals[j].equals("diamond")) sum += 25;
+                        else if(minerals[j].equals("iron")) sum += 5;
+                        else sum++;
                     }
                 }
                 
-                mine(picks, minerals, total+ cost, now + 5);
-                picks[i]++;
+                mine(picks, minerals, size, total + sum);
+                picks[i] ++;
             }
         }
     }
