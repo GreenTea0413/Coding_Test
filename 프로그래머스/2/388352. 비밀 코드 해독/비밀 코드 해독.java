12f5b2b5da263b3520d54fc8f11c3cf88c89727f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-18 17:06:19 +0900

    [level 2] Title: 비밀 코드 해독, Time: 46.70 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,39 +2,37 @@ import java.util.*;
 
 class Solution {
     int answer = 0;
-    public int solution(int n, int[][] q, int[] ans) {
-        // 30개 중 5개 조합으로 다 비교하기
-        comb(1, n, q, ans, new ArrayList<>());
+    public int solution(int n, int[][] q, int[] ans) {        
+        // 1 ~ n까지 5개씩 조합 만들어서 다 비교해보면 되는거임!
+        combine(1, n, q, ans, new ArrayList<>());
         return answer;
     }
     
-    public void comb(int start, int end, int[][] q, int[] ans, List<Integer> list){
+    public void combine(int start, int end, int[][] q, int[] ans, List<Integer> list){
+        // 5개씩 채우면 비교하러가기
         if(list.size() == 5){
-            if(isValid(list, q, ans)){
-                answer++;
-                return;
-            }
+            if(isCheck(list, q, ans)) answer++;
+            return;
         }
         
         for(int i = start; i <= end; i++){
             list.add(i);
-            comb(i + 1, end, q, ans, list);
+            combine(i + 1, end, q, ans, list);
             list.remove(list.size() - 1);
         }
     }
     
-    public boolean isValid(List<Integer> list, int[][] q, int[] ans){
+    boolean isCheck(List<Integer> list, int[][] q, int[] ans){
+        // 리스트로 만든 숫자 조합들로 ans에 정답 갯수랑 맞는지 비교하기
         for(int i = 0; i < q.length; i++){
             int count = 0;
-            for(int num : list){
-                for(int x : q[i]){
-                    if(num == x) count++;
-                }
+            for(int num : q[i]){
+                for(int l : list){
+                    if(num == l) {count ++; break;}
+                }   
             }
-            
             if(count != ans[i]) return false;
         }
-        
         return true;
     }
 }
