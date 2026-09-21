cb69fb547e0814ed06ad1d2aa1b986a5caeb2a1b
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-21 17:40:46 +0900

    [level 3] Title: 스티커 모으기(2), Time: 3.84 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,28 +1,26 @@
 class Solution {
+    int len;
     public int solution(int sticker[]) {
-        int len = sticker.length;
+        len = sticker.length;
         if(len == 1) return sticker[0];
         
-        // 시작점을 기준으로 어차피 0이면 len - 2까지 밖에 못씀
-        // 반대로 1이면 len - 1까지 밖에 못씀
-        int num1 = dp(sticker, 0, len - 2);
-        int num2 = dp(sticker, 1, len - 1);
-    
-        return Math.max(num1, num2);
+        return Math.max(dp(0, len - 2, sticker), dp(1, len - 1, sticker));
     }
     
-    public int dp (int[] sticker, int start, int end){
+    int dp (int start, int end, int[] sticker){
+        // 만약에 같으면 이렇게 종료 시켜줘야함
         if(start == end) return sticker[start];
         
-        int len = end - start + 1;
-        int[] arr = new int[len];
+        int gap = end - start + 1;
+        int[] arr = new int[gap];
+        
         arr[0] = sticker[start];
         arr[1] = Math.max(arr[0], sticker[start + 1]);
         
-        for(int i = 2; i < len; i ++){
-            arr[i] = Math.max(arr[i - 2] + sticker[start + i], arr[i - 1]);
+        for(int i = 2; i < gap; i++){
+            arr[i] = Math.max(arr[i - 1], arr[i - 2] + sticker[start + i]);
         }
         
-        return arr[len - 1];
+        return arr[gap - 1];
     }
 }
