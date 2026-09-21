6c985e0ac4f5dd0769730339f0444e4f40f8f8e7
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-21 18:21:45 +0900

    [level 3] Title: 불량 사용자, Time: 64.89 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,46 +1,46 @@
 import java.util.*;
 
 class Solution {
-    Set<Set<String>> answer = new HashSet<>();
-    
+    Set<Set<String>> set = new HashSet<>();
+    String[] user;
+    String[] ban;
+    int uLen;
+    int bLen;
     public int solution(String[] user_id, String[] banned_id) {
-        // fr*d*에 대해서 몇개가 되는지
-        // abc1**에 대해서 몇개가 되는지를 찾으면 됨
-        boolean[] v = new boolean[user_id.length];
-        // banned_id를 하나씩 내려가면서 Set에 담아서 보관하기
-        dfs(user_id, banned_id, v, 0, new HashSet<>());
+        user = user_id; ban = banned_id;  uLen = user_id.length; bLen = banned_id.length;
+        // dfs 백트래킹 활용해서 전체 경우의 수 만들기
+        dfs(new boolean[uLen], 0, new HashSet<>());
         
-        return answer.size();
+        // 이제 set에 모은걸로 값 구하기
+        return set.size();
     }
     
-    public void dfs(String[] user_id, String[] banned_id, boolean[] v, int depth, Set<String> set){
-        if(depth == banned_id.length){
-            answer.add(new HashSet<>(set));
+    void dfs(boolean[] v, int depth, Set<String> cur){
+        if(depth == bLen){
+            set.add(new HashSet<>(cur));
             return;
         }
         
-        for(int i = 0; i < user_id.length; i++){
-            if(!v[i]){
-                if(check(user_id[i], banned_id[depth])){
-                    v[i] = true;
-                    set.add(user_id[i]);
-                    
-                    dfs(user_id, banned_id, v, depth + 1, set);
-                    
-                    v[i] = false;
-                    set.remove(user_id[i]);
-                }
+        for(int i = 0; i < uLen; i++){
+            if(!v[i] && check(user[i], ban[depth])){
+                cur.add(user[i]);
+                v[i] = true;
+
+                dfs(v, depth + 1, cur);
+
+                v[i] = false;
+                cur.remove(user[i]);
             }
         }
     }
     
-    public boolean check(String u, String b){
+    boolean check(String u, String b){
         if(u.length() != b.length()) return false;
-        for(int i = 0; i < u.length(); i++){
-            if(b.charAt(i) == '*') continue;
+        
+        for(int i = 0; i < b.length(); i++){
+            if(b.charAt(i) =='*') continue;
             else if(u.charAt(i) != b.charAt(i)) return false;
         }
-        
         return true;
     }
 }
