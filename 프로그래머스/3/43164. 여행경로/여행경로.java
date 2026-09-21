c93abb1f2244ec0e5b49279cad00ba8d593c93b4
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-21 18:54:33 +0900

    [level 3] Title: 여행경로, Time: 0.51 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,48 +1,51 @@
 import java.util.*;
 
 class Solution {
-    int l;
-    String[] answer;
-    boolean found;
+    Map<String, List<Integer>> map = new HashMap<>();
+    List<String> answer = new ArrayList<>();
+    boolean found = false;
+    int len;
     public String[] solution(String[][] tickets) {
-        l = tickets.length;
-        answer = new String[l + 1];
-        String[] path = new String[l + 1];
-        path[0] = "ICN";
+        len = tickets.length;
+        Arrays.sort(tickets, (a, b) -> {
+            if(a[0].equals(b[0])) return a[1].compareTo(b[1]);
+            return a[0].compareTo(b[0]);
+        });
         
-        Map<String, List<Integer>> map = new HashMap<>();
-        for(int i = 0; i < l; i++){
-            String start = tickets[i][0];
+        for(int i = 0; i < len; i++){
+            String[] t = tickets[i];
             
-            map.putIfAbsent(start, new ArrayList<>());
-            map.get(start).add(i);
-        }
+            map.putIfAbsent(t[0], new ArrayList<>());
+            map.get(t[0]).add(i);
+        }        
+        List<String> path = new ArrayList<>();
+        path.add("ICN");
         
-        for(List<Integer> list : map.values()){
-            Collections.sort(list, (a, b) -> tickets[a][1].compareTo(tickets[b][1]));
-        }
-        
-        dfs(map, tickets, path, 0, new boolean[l + 1], "ICN");
-        return answer;
+        dfs(tickets, "ICN", path, 0, new boolean[len + 1]);
+        return answer.toArray(new String[0]);
     }
     
-    public void dfs(Map<String, List<Integer>> map, String[][] tickets, String[] path, int depth, boolean[] v, String start){
-        if(depth == l){
-            answer = path.clone();
+    void dfs(String[][] tickets, String start, List<String> path, int depth, boolean[] v){
+        if(depth == len){
+            answer = new ArrayList<>(path);
             found = true;
             return;
         }
         if(!map.containsKey(start)) return;
         
-        for(int next : map.get(start)){
-            if(!v[next] && !found){
-                v[next] = true;
-                path[depth + 1] = tickets[next][1];
+        if(!found){
+            for(int next : map.get(start)){
+                if(!v[next]){
+                    path.add(tickets[next][1]);
+                    v[next] = true;
+                    
+                    dfs(tickets, tickets[next][1], path, depth + 1, v);
                 
-                dfs(map, tickets, path, depth + 1, v, tickets[next][1]);
-                
-                v[next] = false;
+                    v[next] = false;
+                    path.remove(path.size() - 1);
+                }
             }
         }
+        
     }
 }
