5a612d48695facebdfa027f01b4d5df624a64168
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-21 18:31:45 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 28.69 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -3,38 +3,31 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
         int[] answer = new int[2];
-        answer[1] = gems.length;
         
-        Set<String> set = new HashSet<>();
-        for(int i = 0; i < gems.length; i++){set.add(gems[i]);}
-        
-        if(set.size() == 1) return new int[]{1, 1};
-        int kind = set.size();
         int left = 0;
         int right = 0;
+        int len = gems.length;
+        int gap = len;
+        Set<String> set = new HashSet<>();
+        for(String g : gems) set.add(g);
+
         Map<String, Integer> map = new HashMap<>();
-        
-        while(right < gems.length){
+        while(right < len){
             String g = gems[right];
-            map.put(g, map.getOrDefault(g, 0) +1);
+            map.put(g, map.getOrDefault(g, 0) + 1);
             
-            // 만약에 map크기가 이제 kind랑 맞으면
-            while(map.size() == kind){
-                // 그리고 가장 짧은 구간이니까
-                if((answer[1] - answer[0]) > (right - left)){
-                    answer[0] = left;
-                    answer[1] = right;
+            while(map.size() == set.size()){
+                if(gap > right - left) {
+                    gap = right - left;
+                    answer[0] = left + 1; answer[1] = right + 1;
                 }
-                 String l = gems[left];
-                map.put(l, map.get(l) - 1);
-                if(map.get(l) == 0) map.remove(l);
+                String lGem = gems[left];
+                map.put(lGem, map.get(lGem) - 1);
+                if(map.get(lGem) == 0) map.remove(lGem);
                 left++;
             }
-            
             right++;
         }
-        answer[0] ++;
-        answer[1] ++;
         return answer;
     }
 }
