ec9bc837f578d4debf2a249d01000fad5d05091d
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-22 14:36:56 +0900

    [level 3] Title: 징검다리 건너기, Time: 18.68 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,31 +1,33 @@
 class Solution {
     public int solution(int[] stones, int k) {
-        int answer = 0;
-        int left = 1;
+        int left = 0;
         int right = Integer.MAX_VALUE;
         
-        // 몇명이 뛰어갈 수 있을까
-        // mid가 인원수라고 생각하고 못건너는 경우가 k 점프 이상이면 점점 줄여가야함
         while(left <= right){
             int mid = (left + right) / 2;
             
-            if(!check(mid, k, stones)){right = mid - 1; }
-            else{left = mid + 1;}
+            if(!check(mid, stones, k)) {
+                right = mid - 1;
+            }
+            else left = mid + 1;
         }
-        
         return right;
     }
     
-    boolean check(int mid, int k, int[] stones){
+    boolean check(int n, int[] stones, int k){
         int count = 0;
-        for(int s : stones){
-            if(s - mid < 0){
+        // count를 0으로 두고 
+        for(int i = 0; i < stones.length; i++){
+            if(stones[i] < n){
                 count++;
-                if(count >= k){
-                    return false;
-                }
             }
-            else{count = 0;}
+            else{
+                count = 0;
+            }
+            
+            if(count >= k){
+                return false;
+            }
         }
         return true;
     }
