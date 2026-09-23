80935b5b7f616f62a3a2e47faee6d3afdad6de8f
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-23 16:16:43 +0900

    [level 3] Title: 풍선 터트리기, Time: 21.15 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -2,28 +2,22 @@ class Solution {
     public int solution(int[] a) {
         int answer = 0;
         int len = a.length;
-    
+        
+        //  2 1 3 2 면 3 양옆에는 둘 다 작기 때문에 한번만 터트렸을 때 3은 1 터트리면 결국 못터트림
+        // 그러면 왼쪽에서 쭈욱 오면서 최솟값을 구하기
+        // 오른쪽에서도 쭈욱 오면서 최솟값 구하기
+        // 그러고 이제 양쪽에 대해서 둘 다 최솟값이면 그 풍선은 못터트림
+        
         int[] left = new int[len];
         int[] right = new int[len];
         left[0] = a[0];
         right[len - 1] = a[len - 1];
         
-        // 이렇게 왼쪽 오른쪽해서 현 위치보다 작으면
-        // [9, 0, 0]인데 [9, -1, -5]
-        for(int i = 1; i < len; i++){
-            left[i] = Math.min(left[i - 1], a[i]);
-        }
-        
-        // 이건 오른쪽에서 왼쪽으로
-        // [-5, -5, -5]
-        for(int i = len - 2; i >= 0; i--){
-            right[i] = Math.min(a[i], right[i + 1]);
-        }
+        for(int i = 1; i < len; i++) left[i] = Math.min(left[i - 1], a[i]);
+        for(int i = len - 2; i >= 0; i--) right[i] = Math.min(right[i + 1], a[i]);
         
         for(int i = 0; i < len; i++){
-            if(a[i] == left[i] || a[i] == right[i]){
-                answer++;
-            }
+            if(a[i] == left[i] || a[i] == right[i]) answer++;
         }
         return answer;
     }
