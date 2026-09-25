f97079b393c2d9e6e23d1fa45145aa6b14ae294a
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-26 01:31:32 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 13.07 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,58 +1,63 @@
 class Solution {
     public boolean solution(int[][] key, int[][] lock) {
-        int n = lock.length;
-        int[][] lock3 = new int[n * 3][n * 3];
-        
-        for(int i = n; i < n * 2; i++){
-            for(int j = n; j < n * 2; j++){
-                lock3[i][j] = lock[i - n][j - n];
+        int M = key.length;
+        int N = lock.length;
+        int[][] arr = new int[N * 3][N * 3];
+        // 자물쇠를 3 * 3 -> 9*9로 만든다음에 가운데에 배치해서 열쇠 다 돌려보게끔하기
+        for(int i = N; i < 2 * N; i++){
+            for(int j = N; j < 2 * N; j++){
+                arr[i][j] = lock[i - N][j - N];
             }
         }
         
-        int m = key.length;
-        // 시작점으로부터 이제 4방향으로 돌리면서 키가 다 맞는지 확인
-        for(int i = 0; i < 4; i++){
+        
+        // 0,0 부터 시작하게 되면 거기서 부터 열쇠 크기 만큼 비교해서 0을 1로 바꿨을 때 열쇠가 다 1이라면?
+        // 시작점에 대해서 이렇게 돌리면 되긴한데 열쇠 돌려가면서 대치하기
+        for(int k = 0; k < 4; k++){
+            // 90도 4번 돌릴거니까 4번하고
             key = rotate(key);
-            for(int x = 0; x < n * 2; x++){
-                for(int y = 0 ; y < n * 2; y++){
-                    // 여기서 이제 x,y 기준으로 열쇠 크기에 맞게 값 넣기
-                    for(int a = 0; a < m; a++){
-                        for(int b = 0; b < m; b++){
-                            lock3[x + a][y + b] += key[a][b];
+
+            for(int x = 0; x < 2 * N; x++){
+                for(int y = 0; y < 2 * N; y++){
+                    // 시작점을 기준으로 이제 M만큼 열쇠 값들 더하기
+                    for(int a = 0; a < M; a++){
+                        for(int b = 0; b < M; b++){
+                            arr[x + a][y + b] += key[a][b];
                         }
                     }
                     
-                    if(check(lock3, n)) return true;
+                    if(check(arr, N)) return true;
                     
-                    for(int a = 0; a < m; a++){
-                        for(int b = 0; b < m; b++){
-                            lock3[x + a][y + b] -= key[a][b];
+                    // 그리고 약간 백트래킹 느낌으로 계산해준거 다 빼고 다음으로 넘어가기
+                    for(int a = 0; a < M; a++){
+                        for(int b = 0; b < M; b++){
+                            arr[x + a][y + b] -= key[a][b];
                         }
                     }
                 }
             }
         }
-        
+ 
         return false;
     }
     
-    public int[][] rotate(int[][] key){
-        int n = key.length;
-        int[][] result = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                result[i][j] = key[n - j - 1][i];
-            }
-        }
-        return result;
-    }
-    
-    public boolean check(int[][] map, int n){
-        for(int i = n; i < n * 2; i++){
-            for(int j = n; j < n * 2; j++){
-                if(map[i][j] != 1) return false;
+    boolean check (int[][] arr, int N){
+        for(int i = N; i < 2 * N; i++){
+            for(int j = N; j < 2 * N; j++){
+                if(arr[i][j] != 1) return false;
             }
         }
         return true;
+    }
+    int[][] rotate(int[][] arr){
+        int M = arr.length;
+        int[][] temp = new int[M][M];
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < M; j++){
+                temp[i][j] = arr[j][M - i - 1];
+            }
+        }
+        
+        return temp;
     }
 }
