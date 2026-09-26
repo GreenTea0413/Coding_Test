d4a8b00170da181c939e2139669e6cf8de99a8ec
BoSeong <124684536+GreenTea0413@users.noreply.github.com>
2026-09-26 14:54:48 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.50 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,30 +1,36 @@
+import java.util.*;
+
 class Solution {
-    int[][] d = {{1,0}, {0,-1},{0,1},{-1,0}};
-    String[] dir = {"d", "l", "r", "u"};
-    public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        int min = Math.abs(x - r) + Math.abs(y - c);
-        int[][] map = new int[n + 1][m + 1];
+    // 사전순으로 먼저 움직이도록 해야함
+    // d l r u
+    int[][] dir = {{1,0},{0,-1},{0,1},{-1,0}};
+    String[] alp = {"d","l","r","u"};
     
-        if(min > k) return "impossible";
-        if((k - min) % 2 != 0) return "impossible";
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {        
+        int min = Math.abs(x - r) + Math.abs(y - c);
+        
+        if(min > k) return "impossible"; // 최소 이동길이보다 k가 작으면 못감
+        if((k - min) % 2 != 0) return "impossible"; // k까지 갈때 min을 빼고 홀수면 못감
         
         StringBuilder sb = new StringBuilder();
+        // 좌표 1,1부터 시작해서 n, m까지
         while(k > 0){
             for(int i = 0; i < 4; i++){
-                int nx = x + d[i][0];
-                int ny = y + d[i][1];
+                int nx = x + dir[i][0];
+                int ny = y + dir[i][1];
                 
                 if(nx < 1 || nx > n || ny < 1 || ny > m) continue;
                 
                 int dist = Math.abs(r - nx) + Math.abs(c - ny);
                 int remain = k - 1;
-
-                if((k - dist) % 2 == 0 || remain >= dist) {
-                    x = nx; y = ny;
-                    sb.append(dir[i]);
-                    k--;
-                    break;
-                }
+                
+                if(remain < dist) continue;
+                if((remain - dist) % 2 != 0) continue;
+                
+                x = nx; y = ny;
+                sb.append(alp[i]);
+                k--;
+                break;
             }
         }
         
